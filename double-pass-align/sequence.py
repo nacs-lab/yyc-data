d103bf04e3b0b73f3b5373f64246837d7ca86b00
Yichao Yu <yyc1992@gmail.com>
2014-09-16 21:43:16 -0400

    [align] wider and slower....

--- double-pass-align/sequence.py
+++ double-pass-align/sequence.py
@@ -3,14 +3,14 @@
 from numpy import *
 
 def sequence_gen(mod_freq, mod_amp, cent_freq, dt=100e-6, T=1):
-    t = -dt
+    t = 0
     while t < T:
+        yield cent_freq + mod_amp * cos(mod_freq * 2 * pi * t)
         t += dt
-        yield cent_freq + mod_amp * sin(mod_freq * 2 * pi * t)
 
 def main():
     print('dt = 10 us, TTL(all) = 0')
-    for freq in sequence_gen(2, 20e6, 80e6, dt=1e-3, T=.5):
+    for freq in sequence_gen(1, 60e6, 80e6, dt=1e-3, T=.5):
         print('dt = 1000 us, freq(2) = %.0f Hz' % freq)
 
 if __name__ == '__main__':
