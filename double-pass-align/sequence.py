75760429bdd51480ae4a8b0c59f9d18936152982
Yichao Yu <yyc1992@gmail.com>
2014-09-18 09:57:36 -0400

    [align] original range....

--- double-pass-align/sequence.py
+++ double-pass-align/sequence.py
@@ -10,7 +10,7 @@ def sequence_gen(mod_freq, mod_amp, cent_freq, dt=100e-6, T=1):
 
 def main():
     print('dt = 10 us, TTL(all) = 0')
-    for freq in sequence_gen(1, 60e6, 80e6, dt=1e-3, T=.5):
+    for freq in sequence_gen(1, 20e6, 80e6, dt=1e-3, T=.5):
         print('dt = 1000 us, freq(2) = %.0f Hz' % freq)
 
 if __name__ == '__main__':
