77c5ba3e5f115a2e21800ce387abb31870e25d4d
Yichao Yu <yyc1992@gmail.com>
2015-06-12 11:41:24 -0400

    [experiments/waveplate] plotting the new (final?) data. Update scripts

--- experiments/waveplate/waveplate.jl
+++ experiments/waveplate/waveplate.jl
@@ -15,16 +15,18 @@ function getFileInfo(name)
     return ("$name.txt", Any[tryParseNumber(v) for v in split(bname, '_')])
 end
 
-function getData(finfo)
+function getData(finfo, unc, offset)
+    # 15% + 0.02μW uncertainty by default
     data = readdlm(finfo[1], ' ', Float64)
     idxs = sortperm(data[:, 1])
     res = Array{Float64, 2}(size(data, 1), 3)
     res[:, 1:2] = data[idxs, :]
     for i in 1:size(data, 1)
-        res[i, 3] = res[i, 2] * 0.01 + 0.02e-3 # 15% + 0.02μW
+        res[i, 3] = res[i, 2] * unc + offset
     end
     return res
 end
+
 function plotData(finfo, data)
     errorbar(data[:, 1], data[:, 2], data[:, 3])
     lamb, waveplate = finfo[2][1:2]
@@ -46,21 +48,11 @@ function plotData(finfo, data)
     title("\$$(Int(lamb))\$nm, $nplate$angl_str")
 end
 
-function plotFile(finfo)
-    data = getData(finfo)
-    plotData(finfo, data)
-    data
-end
-
 function plotSingleData(finfo, data)
     figure()
     plotData(finfo, data)
     xlim([minimum(data[:, 1]) - 5, maximum(data[:, 1]) + 5])
     ylim([0, maximum(data[:, 2]) * 1.1])
-end
-
-function plotSingleFile(finfo)
-    plotSingleData(finfo, getData(finfo))
 end
 
 model(x, p) = begin
@@ -87,4 +79,5 @@ function plotDataFit(finfo, data)
     plot(xs, ys)
 end
 
-plotFileFit(finfo) = plotDataFit(finfo, getData(finfo))
+plotFileFit(finfo, unc=0.01, offset=0.02e-3) =
+    plotDataFit(finfo, getData(finfo, unc, offset))
