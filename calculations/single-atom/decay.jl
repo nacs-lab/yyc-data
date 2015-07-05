e53e62ca404674ff96e880e937e51c4bd5752cda
Yichao Yu <yyc1992@gmail.com>
2015-07-05 17:21:24 -0400

    [calculations/single-atom] decay optimization

--- calculations/single-atom/decay.jl
+++ calculations/single-atom/decay.jl
@@ -10,40 +10,49 @@ immutable StationaryAtom
     δ::Float64
 end
 
-function propagate(H::StationaryAtom, ψ0, dt, ψs::Matrix{Complex128})
-    @assert length(ψ0) == 2
+immutable StationaryPropagator
+    H::StationaryAtom
+    dt::Float64
+    T11::Complex128
+    T12::Complex128
+    T21::Complex128
+    T22::Complex128
+    function StationaryPropagator(H::StationaryAtom, dt)
+        # Hamiltonian is
+        # H = [δ / 2, Ω
+        #      Ω, -δ / 2] = (δ / 2) σ_z + Ω σ_x = c_σ * (c_σz σ_z + c_σx σ_x)
 
+        c_σ = √(H.δ^2 / 4 + H.Ω^2)
+        c_σx = H.Ω / c_σ
+        c_σz = H.δ / 2 / c_σ
+
+        # Propagator is
+        # exp(im H Δt) = exp(im * c_σ * (c_σz σ_z + c_σx σ_x) * Δt)
+        #              = cos(c_σ Δt) + im * (c_σz σ_z + c_σx σ_x) * sin(c_σ Δt)
+        #              = cos_dt + (im c_σz sin_dt) σ_z + (im c_σx sin_dt) σ_x
+        #              = [cos_dt + im c_σz sin_dt, im c_σx sin_dt
+        #                 im c_σx sin_dt         , cos_dt - im c_σz sin_dt]
+
+        cos_dt = cos(c_σ * dt)
+        sin_dt = sin(c_σ * dt)
+
+        T11 = cos_dt + im * c_σz * sin_dt
+        T22 = cos_dt - im * c_σz * sin_dt
+        T21 = T12 = im * c_σx * sin_dt
+        new(H, dt, T11, T12, T21, T22)
+    end
+end
+
+@inline function propagate(P::StationaryPropagator, ψ0, ψs::Matrix{Complex128})
     nstep = size(ψs, 2)
     @inbounds ψs[:, 1] = ψ0
 
-    # Hamiltonian is
-    # H = [δ / 2, Ω
-    #      Ω, -δ / 2] = (δ / 2) σ_z + Ω σ_x = c_σ * (c_σz σ_z + c_σx σ_x)
-
-    c_σ = √(H.δ^2 / 4 + H.Ω^2)
-    c_σx = H.Ω / c_σ
-    c_σz = H.δ / 2 / c_σ
-
-    # Propagator is
-    # exp(im H Δt) = exp(im * c_σ * (c_σz σ_z + c_σx σ_x) * Δt)
-    #              = cos(c_σ Δt) + im * (c_σz σ_z + c_σx σ_x) * sin(c_σ Δt)
-    #              = cos_dt + (im c_σz sin_dt) σ_z + (im c_σx sin_dt) σ_x
-    #              = [cos_dt + im c_σz sin_dt, im c_σx sin_dt
-    #                 im c_σx sin_dt         , cos_dt - im c_σz sin_dt]
-
-    cos_dt = cos(c_σ * dt)
-    sin_dt = sin(c_σ * dt)
-
-    T11 = cos_dt + im * c_σz * sin_dt
-    T22 = cos_dt - im * c_σz * sin_dt
-    T21 = T12 = im * c_σx * sin_dt
-
-    next_decay = -log(rand()) / H.Γ
+    next_decay = -log(rand()) / P.H.Γ
 
     @inbounds for i in 2:nstep
-        t = i * dt
+        t = i * P.dt
         if t > next_decay
-            next_decay += -log(rand()) / H.Γ
+            next_decay += -log(rand()) / P.H.Γ
             ψ_e = Complex128(0)
             ψ_g = Complex128(1)
         else
@@ -51,13 +60,18 @@ function propagate(H::StationaryAtom, ψ0, dt, ψs::Matrix{Complex128})
             ψ_g = ψs[1, i - 1]
         end
 
-        ψs[2, i] = T11 * ψ_e + T12 * ψ_g
-        ψs[1, i] = T22 * ψ_g + T21 * ψ_e
+        ψs[2, i] = P.T11 * ψ_e + P.T12 * ψ_g
+        ψs[1, i] = P.T22 * ψ_g + P.T21 * ψ_e
     end
     ψs
 end
 
-function propagate(H::StationaryAtom, ψ0, dt, nstep::Integer)
+@inline function propagate(H::StationaryAtom, ψ0, dt, ψs::Matrix{Complex128})
+    P = StationaryPropagator(H, dt)
+    propagate(P, ψ0, ψs)
+end
+
+@inline function propagate(H::StationaryAtom, ψ0, dt, nstep::Integer)
     # Pre-allocate result and temporary arrays
     ψs = Array{Complex{Float64}, 2}(2, nstep)
     propagate(H, ψ0, dt, ψs)
@@ -67,8 +81,9 @@ function propagate_average(H, dt, nstep, ntime)
     ψ_init = Complex{Float64}[0, 1]
     ψs = Matrix{Complex{Float64}}(2, nstep)
     ps = zeros(Float64, (2, nstep))
+    P = StationaryPropagator(H, dt)
     @inbounds for i in 1:ntime
-        propagate(H, ψ_init, dt, ψs)
+        propagate(P, ψ_init, ψs)
         for j in 1:nstep
             ps[1, j] += abs2(ψs[1, j])
             ps[2, j] += abs2(ψs[2, j])
