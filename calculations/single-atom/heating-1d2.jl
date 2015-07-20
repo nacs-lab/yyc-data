5f42e4eeb40f0c187d6048bc6ed6fd475c12d4d9
Yichao Yu <yyc1992@gmail.com>
2015-07-20 17:29:25 -0400

    [calculations/single-atom] more realistic Na parameters

--- calculations/single-atom/heating-1d2.jl
+++ calculations/single-atom/heating-1d2.jl
@@ -13,19 +13,19 @@ m_Na = 22.98977e-3 / 6.02214129e23 / (1.0545717253362894e-34 * 1e6)
 h_trap = HTrap(m_Na, (ω_g, ω_e))
 
 # k, Γ
-o_decay = OpticalDecay(2π / 0.589, 2π * 1.0)
+o_decay = OpticalDecay(2π / 0.589, 2π * 10.0)
 
 # k, Ω, δ, τ_θ
-δ = -2π * 0.5
-Ω = 2π * 0.1
+δ = 2π * 5.0
+Ω = 2π * 2.5
 o_drive1 = OpticalDrive(2π / 0.589, Ω, δ, 1000.0)
 o_drive2 = OpticalDrive(-2π / 0.589, Ω, δ, 1000.0)
 
 h_system = HSystem(h_trap, o_decay, (o_drive1, o_drive2))
 
-grid_size = 256
-grid_space = 0.01
-p_sys = SystemPropagator(h_system, 0.1, grid_space, 100000, grid_size)
+grid_size = 1024
+grid_space = 0.005
+p_sys = SystemPropagator(h_system, 0.005, grid_space, 40000, grid_size)
 
 
 function gen_ψ0(grid_size, grid_space)
@@ -33,7 +33,7 @@ function gen_ψ0(grid_size, grid_space)
     ψ0 = Array{Complex128}(2, grid_size)
     sum = 0.0
     @inbounds for i in 1:grid_size
-        ψ = exp(-((i * grid_space - x_center + 0.3) / 0.2)^2)
+        ψ = exp(-((i * grid_space - x_center + 1.0) / 0.2)^2)
         sum += abs2(ψ)
         ψ0[1, i] = ψ
         ψ0[2, i] = 0
