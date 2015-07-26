b0b2272f9d3127a8ab490497adf85a762e49166d
Yichao Yu <yyc1992@gmail.com>
2015-07-26 14:44:58 -0400

    [calculations/single-atom] Amplitude2D

--- calculations/single-atom/src/atomic.jl
+++ calculations/single-atom/src/atomic.jl
@@ -5,7 +5,11 @@
 
 @enum Polarization Pol_σplus Pol_σminus Pol_π
 
-immutable Amplitude3D{T}
+abstract Amplitude
+
+Base.abs(amp::Amplitude) = sqrt(abs2(amp))
+
+immutable Amplitude3D{T<:Real} <: Amplitude
     x::Complex{T}
     y::Complex{T}
     z::Complex{T}
@@ -24,7 +28,45 @@ immutable Amplitude3D{T}
     end
 end
 
-@inline call{T}(::Type{Amplitude3D}, pol::Polarization, amp::T) =
+@inline call{T<:Real}(::Type{Amplitude3D}, pol::Polarization, amp::T) =
     Amplitude3D{T}(pol, amp)
-@inline call{T}(::Type{Amplitude3D}, pol::Polarization, amp::Complex{T}) =
-    Amplitude3D{T}(pol, amp)
+
+@inline call{T<:Real}(::Type{Amplitude3D}, pol::Polarization,
+                      amp::Complex{T}) = Amplitude3D{T}(pol, amp)
+
+Base.abs2(amp::Amplitude3D) = abs2(amp.x) + abs2(amp.y) + abs2(amp.z)
+
+immutable LinearPol
+end
+immutable CircularPol{Dir}
+end
+
+const linearPol = LinearPol()
+const rightCirPol = CircularPol{1}()
+const leftCirPol = CircularPol{-1}()
+
+immutable Amplitude2D{T<:Real} <: Amplitude
+    x::Complex{T}
+    y::Complex{T}
+    @inline Amplitude2D(x, y) = new(x, y)
+    @inline Amplitude2D(::LinearPol, amp, angl=T(0)) =
+        new(amp * cos(angl), amp * sin(angl))
+    @inline function Amplitude2D{Dir}(::CircularPol{Dir}, amp)
+        amp2 = sqrt(T(0.5)) * amp
+        new(amp2, Dir * im * amp2)
+    end
+end
+
+@inline call{T}(::Type{Amplitude2D}, p::LinearPol, amp::T, angl=T(0)) =
+    Amplitude2D{T}(p, amp, angl)
+
+@inline call{T}(::Type{Amplitude2D}, p::LinearPol, amp::Complex{T},
+                angl=T(0)) = Amplitude2D{T}(p, amp, angl)
+
+@inline call{T}(::Type{Amplitude2D}, p::CircularPol, amp::T) =
+    Amplitude2D{T}(p, amp)
+
+@inline call{T}(::Type{Amplitude2D}, p::CircularPol, amp::Complex{T}) =
+    Amplitude2D{T}(p, amp)
+
+Base.abs2(amp::Amplitude2D) = abs2(amp.x) + abs2(amp.y)
