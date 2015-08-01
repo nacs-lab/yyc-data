3f784678581f6fed4f2df4d90421a57250d6895a
Yichao Yu <yyc1992@gmail.com>
2015-08-01 14:07:51 -0400

    [calculations/single-atom] Export and move comments

--- calculations/single-atom/SingleAtom/src/atomic.jl
+++ calculations/single-atom/SingleAtom/src/atomic.jl
@@ -6,6 +6,8 @@ using ..Utils
 import ..Optical
 import Base: *
 
+export TransitionType, Trans_σ⁺, Trans_σ⁻, Trans_π
+
 @enum TransitionType Trans_σ⁺ Trans_σ⁻ Trans_π
 
 """
@@ -19,27 +21,24 @@ Overlap of a drive with a dipole transition
 """
 function *(ax_trans::Tuple{Vec3D,TransitionType}, amp::Vec3D)
     # A = a * π + b * σ⁺ + c * σ⁻
-
-    # ax ⋅ π = 1
-    # ax ⋅ σ⁺ = 0
-    # ax ⋅ σ⁻ = 0
-
-    # ax × π = 0
-    # ax × σ⁺ = i σ⁺
-    # ax × σ⁻ = -i σ⁻
-
-    # ax × (ax × π) = 0
-    # ax × (ax × σ⁺) = -σ⁺
-    # ax × (ax × σ⁻) = -σ⁻
     ax = ax_trans[1]
     trans = ax_trans[2]
     ax_scale = 1 / abs(ax)
     norm_ax = ax * ax_scale
     if trans == Trans_π
+        # ax ⋅ π = 1
+        # ax ⋅ σ⁺ = 0
+        # ax ⋅ σ⁻ = 0
         return abs(norm_ax * amp)
     end
     # TODO figure out the sign of σ⁺ and σ⁻
+    # ax × π = 0
+    # ax × σ⁺ = i σ⁺
+    # ax × σ⁻ = -i σ⁻
     trans_diff = norm_ax × amp # i (σ⁺ - σ⁻)
+    # ax × (ax × π) = 0
+    # ax × (ax × σ⁺) = -σ⁺
+    # ax × (ax × σ⁻) = -σ⁻
     trans_amp = norm_ax × trans_diff # -(σ⁺ + σ⁻)
     σ_sign = ifelse(trans == Trans_σ⁺, -1im, 1im)
     return abs(trans_diff + σ_sign * trans_amp) / 2
