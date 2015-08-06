df79c4f4d63baed0890230e9ece2f3342c7cede1
Yichao Yu <yyc1992@gmail.com>
2015-08-06 12:00:17 -0400

    [calculations/single-atom] fancier output for performance tests. record output

--- calculations/single-atom/SingleAtom/test/perf/soa_copy.jl
+++ calculations/single-atom/SingleAtom/test/perf/soa_copy.jl
@@ -2,11 +2,9 @@
 
 using SingleAtom
 
-@generated function test_scale{N,T}(nele, factors::NTuple{N,Complex{T}},
-                                    nloop=10_000)
+@generated function test_scale{N,T}(nele, factors::NTuple{N,Complex{T}}, nloop)
     splat_factors = [gensym(:splat) for i in 1:N]
     init_ex = quote
-        println("nele: $nele, ndim: $($N)")
         ary = Matrix{Complex{T}}(nele, $N)
         soary = convert(StructOfArrays, ary)
         factor2 = exp(rand(T, nele) * im)
@@ -42,39 +40,63 @@ using SingleAtom
     end
     quote
         $init_ex
-        println("AoS")
         $rand_ex
-        @time for n in 1:nloop
+        t1 = @elapsed for n in 1:nloop
             $aos_run
         end
-        println("SoA")
         $rand_ex
-        @time for n in 1:nloop
+        t2 = @elapsed for n in 1:nloop
             $soa_run
+        end
+        t1 / nloop, t2 / nloop
+    end
+end
+
+get_factors(n) = if n == 1
+    (exp(im * 1f0),)
+else
+    (map(x->exp(im * x), linspace(0f0, 10f0, n))...)
+end
+
+function print_strs_aligned(strs, max_len)
+    len = length(strs)
+    for i in 1:len
+        str = strs[i]
+        str_len = length(str)
+        print(" " ^ (max_len - str_len), str)
+        if i != len
+            print("| ")
         end
     end
 end
 
-get_factors(n) = (map(x->exp(im * x), linspace(0f0, 10f0, n))...)
+function test_nele(nele, _ndims, nloop=10_000)
+    ts = [test_scale(nele, get_factors(ndim), nloop) for ndim in _ndims]
+    ndims_str = [@sprintf("%d", ndim) for ndim in _ndims]
+    taos_str = [@sprintf("%.2e", ts[i][1]) for i in 1:length(_ndims)]
+    tsoa_str = [@sprintf("%.2e", ts[i][2]) for i in 1:length(_ndims)]
+    max_len = maximum(length, [ndims_str; taos_str; tsoa_str])
+    println("* `nele=$nele`")
+    println()
+    print("|      ndim| ")
+    print_strs_aligned(ndims_str, max_len)
+    println("|")
+    println("|----------|", ("-" ^ (max_len + 1) * "|") ^ length(_ndims))
 
-# test_scale(64, get_factors(5))
-# test_scale(64, get_factors(10))
-test_scale(64, get_factors(24))
-println()
-# test_scale(128, get_factors(5))
-# test_scale(128, get_factors(10))
-test_scale(128, get_factors(24))
-println()
-# test_scale(256, get_factors(5))
-# test_scale(256, get_factors(10))
-test_scale(256, get_factors(24))
-println()
-# test_scale(512, get_factors(5))
-# test_scale(512, get_factors(10))
-test_scale(512, get_factors(24))
-println()
-# test_scale(1024, get_factors(5))
-# test_scale(1024, get_factors(10))
-test_scale(1024, get_factors(24))
+    print("|Time (AoS)| ")
+    print_strs_aligned(taos_str, max_len)
+    println("|")
+
+    print("|Time (SoA)| ")
+    print_strs_aligned(tsoa_str, max_len)
+    println("|")
+    println()
+end
+
+test_nele(64, [1, 3, 10, 30], 200_000)
+test_nele(128, [1, 3, 10, 30], 100_000)
+test_nele(256, [1, 3, 10, 30], 50_000)
+test_nele(512, [1, 3, 10, 30], 25_000)
+test_nele(1024, [1, 3, 10, 30], 12_500)
 
 # @code_llvm test_scale(128, get_factors(2), 100000)
