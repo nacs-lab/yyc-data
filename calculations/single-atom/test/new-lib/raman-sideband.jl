3a32250064fa5a4db5210407ac078a4091cf1563
Yichao Yu <yyc1992@gmail.com>
2015-08-11 11:17:56 -0400

    [calculations/single-atom] clean up, benchmark

--- calculations/single-atom/test/new-lib/raman-sideband.jl
+++ calculations/single-atom/test/new-lib/raman-sideband.jl
@@ -26,16 +26,23 @@ add_transition!(builder, :G1, :E, Transition{Trans_σ⁺}(k_res, Γ_Na, 1f0))
 add_transition!(builder, :G2, :E, Transition{Trans_σ⁺}(k_res, Γ_Na / 2, 1f0))
 add_transition!(builder, :G1, :G2, Transition{Trans_σ⁺}(k_res, 0f0, 1f0))
 
-δ₁ = -2π * 40.0
-δ₂ = -2π * 0.0
 Ω = 2π * 0.5
 
 add_drive!(builder, Drive{Vec3D{Complex64}(0, 2π * 0.05, 0)}(k_res, -ω_g, NaN,
                                                               1000.0),
            :G1, :G2)
 
-add_drive!(builder, Drive{Vec3D{Complex64}(0, Ω, 0)}(-k_res, δ₂, NaN, 1000.0),
+add_drive!(builder, Drive{Vec3D{Complex64}(0, Ω, 0)}(-k_res, 0, NaN, 1000.0),
            :G2, :E)
+
+add_drive!(builder, Drive{Vec3D{Complex64}(0, 0.3Ω, 0)}(-k_res, 0, NaN, 1000.0),
+           :G1, :E)
+
+# add_drive!(builder, Drive{Vec3D{Complex64}(0, Ω, 0)}(0, 0, NaN, 1000.0),
+#            :G2, :E)
+
+# add_drive!(builder, Drive{Vec3D{Complex64}(0, 0.3Ω, 0)}(0, 0, NaN, 1000.0),
+#            :G1, :E)
 
 sys = MotionSystem(Vec3D(1f0, 0f0, 0f0), m_Na, builder)
 
@@ -93,7 +100,11 @@ println("start")
 @time precompile(propagate, tuple(Base.typesof(P, setup,
                                                _measure).parameters...))
 gc()
+# @profile propagate(P, setup, _measure)
+# Profile.print(C=true)
 @time propagate(P, setup, _measure)
+
+# exit()
 
 plot_measure(_measure)
 
