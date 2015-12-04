89ebeb02ca2de8cf2a7e1c0f04f2f61bbf911ad4
Yichao Yu <yyc1992@gmail.com>
2015-12-04 11:50:41 -0500

    [calculations/single-atom] Cs parameters

--- calculations/single-atom/test/heating-tests/param-plot.jl
+++ calculations/single-atom/test/heating-tests/param-plot.jl
@@ -21,23 +21,31 @@
         ψ0
     end
 
-    function run(ratio)
-        println("Running $ratio")
+    function run(det_hz)
+        println("Running $det_hz")
         # m here is actually m / ħ
-        m_Na = Float32(22.98977e-3 / 6.02214129e23 /
+        # m_Na = Float32(22.98977e-3 / 6.02214129e23 /
+        #                (1.0545717253362894e-34 * 1e6))
+        m_Cs = Float32(132.905451933e-3 / 6.02214129e23 /
                        (1.0545717253362894e-34 * 1e6))
-        ω_g = Float32(2π * 0.2) # f = 200kHz
-        ω_e = ω_g * ratio
-        h_trap = HTrap{Float32}(m_Na, (ω_g, ω_e))
+        ω_g = Float32(2π * 0.07) # f = 70kHz
+        ω_e = ω_g * √(0.6)
+        h_trap = HTrap{Float32}(m_Cs, (ω_g, ω_e))
 
-        λ_res = Float32(0.589)
+        λ_res = Float32(0.852)
 
         # k, Γ
-        o_decay = OpticalDecay{Float32}(2π / λ_res, 2π * 10.0)
+        o_decay = OpticalDecay{Float32}(2π / λ_res, 2π * 5.234)
+
+        trap_depth = 0.7 * 2π * 20 # 0.7mK
+        e_trap_depth = trap_depth * ω_e^2 / ω_g^2
+        δ_offset = trap_depth - e_trap_depth
+
+        δ_0 = 2π * det_hz
 
         # k, Ω, δ, τ_θ
-        δ = 2π * 5.0
-        Ω = 2π * 5.0
+        δ = δ_0 + δ_offset
+        Ω = 2π * 5.234 * 0.05
         o_drive1 = OpticalDrive{Float32}(2π / λ_res, Ω, δ, 1000.0)
         o_drive2 = OpticalDrive{Float32}(-2π / λ_res, Ω, δ, 1000.0)
 
@@ -47,9 +55,9 @@
 
         grid_size = 512
         grid_space = 0.005f0
-        p_sys = SystemPropagator(h_system, 0.005f0, grid_space,
-                                 50000, grid_size)
-        e_thresh = (maximum(p_sys.E_k) + maximum(p_sys.E_x[1])) / 4
+        p_sys = SystemPropagator(h_system, 0.01f0, grid_space,
+                                 100_000, grid_size)
+        e_thresh = trap_depth
         ψ0 = gen_ψ0(grid_size, grid_space)
         _accum = EnergyRecorder(p_sys, e_thresh)
         monte_carlo = 100
@@ -61,11 +69,11 @@ end
 
 println("start")
 
-ratios = linspace(0, 7, 8)
-xax_name = "Trapping Depth ratio"
-@time accums = pmap(run, ratios)
+params = linspace(-25, 10, 8)
+xax_name = "Free space detuning"
+@time accums = pmap(run, params)
 
-exit()
+# exit()
 
 using PyPlot
 
@@ -76,7 +84,7 @@ end
 figure()
 final_e = Float32[_accum.Es[end] for _accum in accums]
 final_e2 = Float32[_accum.Es2[end] for _accum in accums]
-errorbar(ratios, final_e, final_e2)
+errorbar(params, final_e, final_e2)
 xlabel(xax_name)
 ylabel("Final energy")
 grid()
@@ -85,7 +93,7 @@ ylim(0, ylim()[2])
 figure()
 t_esc = Float32[_accum.t_esc for _accum in accums]
 t_esc2 = Float32[_accum.t_esc2 for _accum in accums]
-errorbar(ratios, t_esc, t_esc2)
+errorbar(params, t_esc, t_esc2)
 xlabel(xax_name)
 ylabel("Escape time (us)")
 grid()
@@ -94,7 +102,7 @@ ylim(0, ylim()[2])
 figure()
 pcount = Float32[_accum.pcount for _accum in accums]
 pcount2 = Float32[_accum.pcount2 for _accum in accums]
-errorbar(ratios, pcount, pcount2)
+errorbar(params, pcount, pcount2)
 xlabel(xax_name)
 ylabel("Photon count")
 grid()
