a828acdffeda5b03a0c58308f79943a22060c220
Yichao Yu <yyc1992@gmail.com>
2015-12-16 16:39:20 -0500

    [calculations/practical-lz] optimization

--- calculations/practical-lz/sims/opt_sweep.jl
+++ calculations/practical-lz/sims/opt_sweep.jl
@@ -131,3 +131,50 @@ function gen_scan_plot{T}(opt::OptParams{T})
 end
 
 const opt_params = OptParams(2f-1, 500, 8, 0.15f-1, 1f-1, 8f-1)
+
+function run_scan{T}(opt::OptParams{T})
+    δ1 = opt.δ1
+    δ2 = opt.δ2
+    δ3 = opt.δ3
+    np = 20 # number of samples in each interval
+    mid_min, mid_max, mid_avg = run_range(opt, linspace(-δ1, δ1, np))
+    side1_min, side1_max, side1_avg = run_range(opt, linspace(-δ3, -δ2, np))
+    side2_min, side2_max, side2_avg = run_range(opt, linspace(δ2, δ3, np))
+    side_min = min(side1_min, side2_min)
+    side_max = max(side1_max, side2_max)
+    side_avg = (side1_avg + side2_avg) / 2
+    (1 - mid_min) * 2 + side_max * 10 + side_avg / mid_avg
+end
+function optim_model(params)
+    # println(params)
+    set_params(opt_params, params)
+    res = run_scan(opt_params)
+    println(res)
+    Float64(res)
+end
+
+params = [-0.11083115669054208,-0.005421677040109914,-0.05139793646080384,-0.0021625185024753964,-0.01105432107412546,-0.0031723844585940237,-0.013821859115642522,-0.0037941483426334292,-0.0016658166228669658,0.0795912629232008,0.008355797655672724,-0.02129744092973063,-0.008622724582925952,0.008365347665137718,0.005627545243400751,-0.0025042245300960437,-0.0020916691271710345]
+do_opt = true
+
+if do_opt
+    # opt_res = optimize(optim_model, params, method=:cg,
+    #                    grtol=1e-6, ftol=1e-15, xtol=1e-15)
+    opt_res = optimize(optim_model, params, method=:bfgs,
+                       grtol=1e-6, ftol=1e-15, xtol=1e-15)
+    # opt_res = optimize(optim_model, params, method=:l_bfgs,
+    #                    grtol=1e-4, ftol=1e-8, xtol=1e-8)
+
+    @show opt_res
+    params = opt_res.minimum
+    println(params)
+end
+
+# params: [Ω0; Ω1; Δδ; cδ[sinN]; cΩ[sinN]]
+set_params(opt_params, params)
+println(run_scan(opt_params))
+x, y = gen_scan_plot(opt_params)
+using PyPlot
+
+plot(x, y)
+grid()
+show()
