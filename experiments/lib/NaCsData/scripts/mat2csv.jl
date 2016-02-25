94c9ebcaf8d6b3e122a69e88b008c36dfe1df02f
Yichao Yu <yyc1992@gmail.com>
2016-02-25 14:32:02 -0500

    [experiments/lib] script to convert mat files

---
+++ experiments/lib/NaCsData/scripts/mat2csv.jl
@@ -0,0 +1,26 @@
+#!/usr/bin/julia -f
+
+using NaCsData
+
+iname = ARGS[1]
+oname = ARGS[2]
+param_name = length(ARGS) >= 3 ? ARGS[3] : ""
+
+_param_name, params, data = NaCsData.load_matscan(iname)
+isempty(param_name) && (param_name = _param_name)
+
+open(oname, "w") do fd
+    print(fd, param_name)
+    for j in 1:size(data, 1)
+        write(fd, ",Image$j")
+    end
+    println(fd)
+    for i in 1:length(params)
+        print(fd, params[i])
+        for j in 1:size(data, 1)
+            write(fd, ",")
+            print(fd, data[j, i])
+        end
+        println(fd)
+    end
+end
