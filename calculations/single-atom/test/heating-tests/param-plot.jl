1287c910fc6e717f43a253b0dd3dc08f60838227
Yichao Yu <yyc1992@gmail.com>
2016-05-09 00:40:46 -0400

    [calculations/single-atom] try magic wavelength

--- calculations/single-atom/test/heating-tests/param-plot.jl
+++ calculations/single-atom/test/heating-tests/param-plot.jl
@@ -9,7 +9,7 @@
         ψ0 = Array{Complex64}(2, grid_size)
         sum = 0.0
         @inbounds for i in 1:grid_size
-            ψ = exp(-((i * grid_space - x_center + 0.8f0 * 0) / 0.2f0)^2)
+            ψ = exp(-((i * grid_space - x_center + 0f0 * 0) / 0.2f0)^2)
             sum += abs2(ψ)
             ψ0[1, i] = ψ
             ψ0[2, i] = 0
@@ -28,16 +28,16 @@
         #                (1.0545717253362894e-34 * 1e6))
         m_Cs = Float32(132.905451933e-3 / 6.02214129e23 /
                        (1.0545717253362894e-34 * 1e6))
-        ω_g = Float32(2π * 0.07) # f = 70kHz
-        ω_e = ω_g * √(0.6)
+        ω_g = Float32(2π * 0.16) # f = 160kHz
+        ω_e = ω_g # * 0 # 0.5
         h_trap = HTrap{Float32}(m_Cs, (ω_g, ω_e))
 
         λ_res = Float32(0.852)
 
         # k, Γ
-        o_decay = OpticalDecay{Float32}(2π / λ_res, 2π * 5.234)
+        o_decay = OpticalDecay{Float32}(2π / λ_res, 2π * 1)
 
-        trap_depth = 0.7 * 2π * 20 # 0.7mK
+        trap_depth = 1.2 * 2π * 20 # 2mK
         e_trap_depth = trap_depth * ω_e^2 / ω_g^2
         δ_offset = trap_depth - e_trap_depth
 
@@ -45,7 +45,7 @@
 
         # k, Ω, δ, τ_θ
         δ = δ_0 - δ_offset
-        Ω = 2π * 5.234 * √(0.1 / 2)
+        Ω = 2π * 1 / 2
         o_drive1 = OpticalDrive{Float32}(2π / λ_res, Ω, δ, 1000.0)
         o_drive2 = OpticalDrive{Float32}(-2π / λ_res, Ω, δ, 1000.0)
         # o_drive3 = OpticalDrive{Float32}(0, Ω * 2, δ, 1000.0)
@@ -55,9 +55,9 @@
         # h_system = HSystem(h_trap, o_decay, (o_drive2,))
 
         grid_size = 512
-        grid_space = 0.005f0
-        p_sys = SystemPropagator(h_system, 0.008f0, grid_space,
-                                 6_250_000, grid_size)
+        grid_space = 0.0025f0
+        p_sys = SystemPropagator(h_system, 0.02f0, grid_space,
+                                 500_000, grid_size)
         e_thresh = trap_depth
         ψ0 = gen_ψ0(grid_size, grid_space)
         _accum = EnergyRecorder(p_sys, e_thresh)
@@ -70,7 +70,7 @@ end
 
 println("start")
 
-params = linspace(-25, 10, 8)
+params = [-4, -3, -2, -1, -0.75, -0.5, -0.25, 0, 1, 2]
 xax_name = "Free space detuning"
 @time accums = pmap(run, params)
 
