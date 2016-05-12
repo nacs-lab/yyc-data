19f594aa633b1a79b8687b0ede9ea2319467ec79
Yichao Yu <yyc1992@gmail.com>
2016-05-12 09:28:38 -0400

    [calculations/single-atom] result

---
+++ calculations/single-atom/results/imaging_line/gamma_5_3.jl
@@ -0,0 +1,174 @@
+脚本启动于 2016年05月11日 星期三 23时26分57秒
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+start
+	From worker 2:	Running β=1.67, δ=-30.0, t=10000.0
+	From worker 5:	Running β=1.67, δ=-20.0, t=10000.0
+	From worker 3:	Running β=1.67, δ=-10.0, t=10000.0
+	From worker 4:	Running β=1.67, δ=-25.0, t=10000.0
+	From worker 7:	Running β=1.67, δ=0.0, t=10000.0
+	From worker 9:	Running β=1.67, δ=-15.0, t=10000.0
+	From worker 6:	Running β=1.67, δ=-17.5, t=10000.0
+	From worker 8:	Running β=1.67, δ=-5.0, t=10000.0
+	From worker 3:	Running β=1.0, δ=-15.0, t=10000.0
+	From worker 7:	Running β=1.0, δ=-10.0, t=10000.0
+	From worker 9:	Running β=1.0, δ=-7.5, t=10000.0
+	From worker 4:	Running β=1.0, δ=-6.25, t=10000.0
+	From worker 5:	Running β=1.0, δ=-5.0, t=10000.0
+	From worker 6:	Running β=1.0, δ=0.0, t=10000.0
+	From worker 8:	Running β=1.0, δ=5.0, t=10000.0
+	From worker 2:	Running β=1.0, δ=10.0, t=10000.0
+	From worker 9:	Running β=1.67, δ=-30.0, t=3000.0
+	From worker 3:	Running β=1.67, δ=-25.0, t=3000.0
+	From worker 7:	Running β=1.67, δ=-20.0, t=3000.0
+	From worker 4:	Running β=1.67, δ=-17.5, t=3000.0
+	From worker 6:	Running β=1.67, δ=-15.0, t=3000.0
+	From worker 8:	Running β=1.67, δ=-10.0, t=3000.0
+	From worker 5:	Running β=1.67, δ=-5.0, t=3000.0
+	From worker 2:	Running β=1.67, δ=0.0, t=3000.0
+	From worker 9:	Running β=1.0, δ=-15.0, t=3000.0
+	From worker 3:	Running β=1.0, δ=-10.0, t=3000.0
+	From worker 7:	Running β=1.0, δ=-7.5, t=3000.0
+	From worker 4:	Running β=1.0, δ=-6.25, t=3000.0
+	From worker 8:	Running β=1.0, δ=-5.0, t=3000.0
+	From worker 6:	Running β=1.0, δ=0.0, t=3000.0
+	From worker 5:	Running β=1.0, δ=5.0, t=3000.0
+	From worker 2:	Running β=1.0, δ=10.0, t=3000.0
+24290.044015 seconds (2.33 M allocations: 2.475 GB, 0.00% gc time)
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/PyPlot.ji for module PyPlot.
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/PyCall.ji for module PyCall.
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/conversions.jl:61
+WARNING: symbol is deprecated, use Symbol instead.
+ [inlined code] from ./error.jl:26
+ in depwarn(::String, ::Symbol) at ./deprecated.jl:64
+ [inlined code] from ./strings/io.jl:51
+ in symbol(::String, ::Vararg{String,N}) at ./deprecated.jl:30
+ [inlined code] from ./strings/io.jl:51
+ in anonymous at ./<no file>:4294967295
+ in include_from_node1(::String) at ./loading.jl:426 (repeats 3 times)
+ [inlined code] from ./boot.jl:230
+ in anonymous at ./<no file>:4294967295
+ in eval(::Module, ::Any) at ./boot.jl:230
+ [inlined code] from ./sysimg.jl:11
+ in process_options(::Base.JLOptions) at ./client.jl:240
+ in _start() at ./client.jl:319
+while loading /usr/share/julia/site/v0.5/PyCall/src/pydates.jl, in expression starting on line 100
+WARNING: symbol is deprecated, use Symbol instead.
+ [inlined code] from ./error.jl:26
+ in depwarn(::String, ::Symbol) at ./deprecated.jl:64
+ [inlined code] from ./strings/io.jl:51
+ in symbol(::String, ::Vararg{String,N}) at ./deprecated.jl:30
+ [inlined code] from ./strings/io.jl:51
+ in anonymous at ./<no file>:4294967295
+ in include_from_node1(::String) at ./loading.jl:426 (repeats 3 times)
+ [inlined code] from ./boot.jl:230
+ in anonymous at ./<no file>:4294967295
+ in eval(::Module, ::Any) at ./boot.jl:230
+ [inlined code] from ./sysimg.jl:11
+ in process_options(::Base.JLOptions) at ./client.jl:240
+ in _start() at ./client.jl:319
+while loading /usr/share/julia/site/v0.5/PyCall/src/pydates.jl, in expression starting on line 100
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/pytype.jl:180
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/MacroTools.ji for module MacroTools.
+WARNING: symbol is deprecated, use Symbol instead.
+ [inlined code] from ./error.jl:26
+ in depwarn(::String, ::Symbol) at ./deprecated.jl:64
+ [inlined code] from ./strings/io.jl:51
+ in symbol(::Symbol, ::Vararg{Symbol,N}) at ./deprecated.jl:30
+ in parse_pydef(::Expr) at /usr/share/julia/site/v0.5/PyCall/src/pyclass.jl:108
+ in @pydef_object(::Any) at /usr/share/julia/site/v0.5/PyCall/src/pyclass.jl:174
+ in include_from_node1(::String) at ./loading.jl:426 (repeats 2 times)
+ [inlined code] from ./boot.jl:230
+ in anonymous at ./<no file>:4294967295
+ in eval(::Module, ::Any) at ./boot.jl:230
+ [inlined code] from ./sysimg.jl:11
+ in process_options(::Base.JLOptions) at ./client.jl:240
+ in _start() at ./client.jl:319
+while loading /usr/share/julia/site/v0.5/PyCall/src/io.jl, in expression starting on line 73
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:289
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:610
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:610
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.ASCIIString is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyCall/src/PyCall.jl:614
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /usr/share/julia/site/v0.5/PyPlot/src/PyPlot.jl:5
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/FixedPointNumbers.ji for module FixedPointNumbers.
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/Colors.ji for module Colors.
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/ColorTypes.ji for module ColorTypes.
+INFO: Recompiling stale cache file /home/yuyichao/.julia/lib/v0.5/LaTeXStrings.ji for module LaTeXStrings.
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /home/yuyichao/projects/nacs-lab/yyc-data/calculations/single-atom/test/heating-tests/param-plot.jl:118
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: No working GUI backend found for matplotlib.
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+("final_energy",[(1.67f0,-30.0f0,10000.0f0),(1.67f0,-25.0f0,10000.0f0),(1.67f0,-20.0f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.67f0,-10.0f0,10000.0f0),(1.67f0,-5.0f0,10000.0f0),(1.67f0,0.0f0,10000.0f0),(1.0f0,-15.0f0,10000.0f0),(1.0f0,-10.0f0,10000.0f0),(1.0f0,-7.5f0,10000.0f0),(1.0f0,-6.25f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.0f0,5.0f0,10000.0f0),(1.0f0,10.0f0,10000.0f0),(1.67f0,-30.0f0,3000.0f0),(1.67f0,-25.0f0,3000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.67f0,-10.0f0,3000.0f0),(1.67f0,-5.0f0,3000.0f0),(1.67f0,0.0f0,3000.0f0),(1.0f0,-15.0f0,3000.0f0),(1.0f0,-10.0f0,3000.0f0),(1.0f0,-7.5f0,3000.0f0),(1.0f0,-6.25f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,0.0f0,3000.0f0),(1.0f0,5.0f0,3000.0f0),(1.0f0,10.0f0,3000.0f0)],Float32[45.343998,33.6609,38.838894,49.787422,102.609375,320.58466,350.54446,448.2815,33.696617,33.43877,39.683098,36.842464,45.108887,379.9523,979.18115,967.5203,44.807796,42.324165,30.686281,50.966217,124.566086,293.40555,363.00916,399.4789,35.96722,34.51195,42.350704,36.532963,56.045357,369.17288,968.26025,909.57654],Float32[7.3221498,3.7935207,4.921088,6.7501864,13.159109,20.011242,24.08121,21.892368,5.0105042,4.0156207,6.1272187,3.8748188,6.963016,30.133827,20.220594,23.358809,6.2586417,5.8918257,3.8214915,5.95404,14.396354,19.914257,22.893288,25.077883,4.602651,4.0269876,4.7014194,5.4499807,6.6950617,36.402744,21.074606,34.205822])
+("escape_time",[(1.67f0,-30.0f0,10000.0f0),(1.67f0,-25.0f0,10000.0f0),(1.67f0,-20.0f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.67f0,-10.0f0,10000.0f0),(1.67f0,-5.0f0,10000.0f0),(1.67f0,0.0f0,10000.0f0),(1.0f0,-15.0f0,10000.0f0),(1.0f0,-10.0f0,10000.0f0),(1.0f0,-7.5f0,10000.0f0),(1.0f0,-6.25f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.0f0,5.0f0,10000.0f0),(1.0f0,10.0f0,10000.0f0),(1.67f0,-30.0f0,3000.0f0),(1.67f0,-25.0f0,3000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.67f0,-10.0f0,3000.0f0),(1.67f0,-5.0f0,3000.0f0),(1.67f0,0.0f0,3000.0f0),(1.0f0,-15.0f0,3000.0f0),(1.0f0,-10.0f0,3000.0f0),(1.0f0,-7.5f0,3000.0f0),(1.0f0,-6.25f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,0.0f0,3000.0f0),(1.0f0,5.0f0,3000.0f0),(1.0f0,10.0f0,3000.0f0)],Float32[8106.308,7999.48,5719.43,1395.9064,291.5258,255.96687,636.3448,1476.1797,7971.063,6991.0,4879.03,4914.2236,2846.9453,171.92862,333.07092,1133.312,2891.0237,2876.0623,2426.0833,1285.9412,290.19934,236.0043,620.62213,1448.5632,2986.9265,2840.385,2609.364,2177.185,1985.9075,188.88115,311.34277,1024.861],Float32[380.35645,434.27725,555.9168,236.0835,34.21212,19.420412,54.484177,108.09668,416.51666,484.48322,516.0299,463.35287,314.5145,21.422823,29.367577,103.212906,63.601757,46.32461,134.83054,138.148,35.968285,18.874767,79.30697,108.04411,9.772932,66.89895,101.37624,146.16576,135.29887,19.163082,29.790369,85.503624])
+("photon_count",[(1.67f0,-30.0f0,10000.0f0),(1.67f0,-25.0f0,10000.0f0),(1.67f0,-20.0f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.67f0,-10.0f0,10000.0f0),(1.67f0,-5.0f0,10000.0f0),(1.67f0,0.0f0,10000.0f0),(1.0f0,-15.0f0,10000.0f0),(1.0f0,-10.0f0,10000.0f0),(1.0f0,-7.5f0,10000.0f0),(1.0f0,-6.25f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.0f0,5.0f0,10000.0f0),(1.0f0,10.0f0,10000.0f0),(1.67f0,-30.0f0,3000.0f0),(1.67f0,-25.0f0,3000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.67f0,-10.0f0,3000.0f0),(1.67f0,-5.0f0,3000.0f0),(1.67f0,0.0f0,3000.0f0),(1.0f0,-15.0f0,3000.0f0),(1.0f0,-10.0f0,3000.0f0),(1.0f0,-7.5f0,3000.0f0),(1.0f0,-6.25f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,0.0f0,3000.0f0),(1.0f0,5.0f0,3000.0f0),(1.0f0,10.0f0,3000.0f0)],Float32[12179.56,23483.22,33550.04,11055.4,2723.46,1747.64,2008.14,2634.02,12496.48,20781.38,20664.4,25083.64,17546.14,1860.4,2060.3,3355.38,4694.52,8791.64,14180.16,10252.74,2689.84,1563.64,2014.46,2385.5,4782.14,8231.76,11093.28,11040.42,12125.48,2075.02,1921.5,3000.62],Float32[655.8723,1367.6328,3267.7046,1861.6699,325.68094,136.48013,178.85815,166.1471,676.61053,1533.6466,2247.1208,2358.7603,1922.897,238.24498,191.80394,335.19553,164.34409,331.65582,859.49396,1073.0687,341.694,121.31882,260.69458,171.95853,118.09854,234.55699,458.13943,756.4539,846.2342,217.14746,193.17949,273.01556])
+
+脚本完成，于 2016年05月12日 星期四 06时12分13秒
