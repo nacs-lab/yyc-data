00a9ba05e67eddddd30d273c27b493dcb2cfc3c7
Yichao Yu <yyc1992@gmail.com>
2016-05-13 20:14:18 -0400

    [calculations/single-atom] results

---
+++ calculations/single-atom/results/imaging_line/gamma_5_9.jl
@@ -0,0 +1,83 @@
+Script started on Fri 13 May 2016 05:55:40 PM EDT
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+start
+	From worker 2:	Running β=1.67, δ=-20.0, t=10000.0
+	From worker 5:	Running β=1.67, δ=-15.0, t=10000.0
+	From worker 3:	Running β=1.67, δ=-17.5, t=10000.0
+	From worker 7:	Running β=1.67, δ=-16.875, t=10000.0
+	From worker 4:	Running β=1.67, δ=-18.75, t=10000.0
+	From worker 8:	Running β=1.67, δ=-17.1875, t=10000.0
+	From worker 9:	Running β=1.67, δ=-16.5625, t=10000.0
+	From worker 6:	Running β=1.67, δ=-16.25, t=10000.0
+	From worker 7:	Running β=1.0, δ=-5.0, t=10000.0
+	From worker 4:	Running β=1.0, δ=-3.75, t=10000.0
+	From worker 6:	Running β=1.0, δ=-2.5, t=10000.0
+	From worker 9:	Running β=1.0, δ=-2.1875, t=10000.0
+	From worker 5:	Running β=1.0, δ=-1.875, t=10000.0
+	From worker 3:	Running β=1.0, δ=-1.5625, t=10000.0
+	From worker 2:	Running β=1.0, δ=-1.25, t=10000.0
+	From worker 8:	Running β=1.0, δ=0.0, t=10000.0
+	From worker 7:	Running β=1.67, δ=-20.0, t=3000.0
+	From worker 6:	Running β=1.67, δ=-18.75, t=3000.0
+	From worker 4:	Running β=1.67, δ=-17.5, t=3000.0
+	From worker 9:	Running β=1.67, δ=-17.1875, t=3000.0
+	From worker 5:	Running β=1.67, δ=-16.875, t=3000.0
+	From worker 2:	Running β=1.67, δ=-16.5625, t=3000.0
+	From worker 3:	Running β=1.67, δ=-16.25, t=3000.0
+	From worker 8:	Running β=1.67, δ=-15.0, t=3000.0
+	From worker 7:	Running β=1.0, δ=-5.0, t=3000.0
+	From worker 6:	Running β=1.0, δ=-3.75, t=3000.0
+	From worker 4:	Running β=1.0, δ=-2.5, t=3000.0
+	From worker 9:	Running β=1.0, δ=-2.1875, t=3000.0
+	From worker 5:	Running β=1.0, δ=-1.875, t=3000.0
+	From worker 2:	Running β=1.0, δ=-1.5625, t=3000.0
+	From worker 3:	Running β=1.0, δ=-1.25, t=3000.0
+	From worker 8:	Running β=1.0, δ=0.0, t=3000.0
+8124.743628 seconds (2.27 M allocations: 1.313 GB, 0.00% gc time)
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near /home/yuyichao/projects/nacs-lab/yyc-data/calculations/single-atom/test/heating-tests/param-plot.jl:118
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+WARNING: Base.UTF8String is deprecated, use String instead.
+  likely near no file:0
+in pystring_query at /usr/share/julia/site/v0.5/PyCall/src/conversions.jl
+("final_energy",[(1.67f0,-20.0f0,10000.0f0),(1.67f0,-18.75f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-17.1875f0,10000.0f0),(1.67f0,-16.875f0,10000.0f0),(1.67f0,-16.5625f0,10000.0f0),(1.67f0,-16.25f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,-3.75f0,10000.0f0),(1.0f0,-2.5f0,10000.0f0),(1.0f0,-2.1875f0,10000.0f0),(1.0f0,-1.875f0,10000.0f0),(1.0f0,-1.5625f0,10000.0f0),(1.0f0,-1.25f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-18.75f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-17.1875f0,3000.0f0),(1.67f0,-16.875f0,3000.0f0),(1.67f0,-16.5625f0,3000.0f0),(1.67f0,-16.25f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,-3.75f0,3000.0f0),(1.0f0,-2.5f0,3000.0f0),(1.0f0,-2.1875f0,3000.0f0),(1.0f0,-1.875f0,3000.0f0),(1.0f0,-1.5625f0,3000.0f0),(1.0f0,-1.25f0,3000.0f0),(1.0f0,0.0f0,3000.0f0)],Float32[13.156621,10.521515,11.057123,14.196241,19.051733,15.177591,21.48321,43.058014,16.802595,18.237106,17.249554,17.817823,17.141228,20.593359,26.47658,280.58878,10.496332,18.455534,15.336549,15.652274,13.941606,17.283405,17.06235,61.483814,14.7759495,18.47074,17.678324,23.251368,22.158342,24.035,25.094082,267.46237],Float32[1.9811285,1.4761163,1.483685,1.7986987,2.089697,2.0230923,2.7095578,4.1575694,2.0280302,2.3235793,2.1116388,2.180526,1.9478017,3.07505,2.7709367,22.199606,1.3881079,2.9324155,2.1201508,2.1414201,2.1401608,2.0951324,2.4317575,7.1698775,2.0740027,2.1317596,2.5661743,2.3935795,3.2980893,3.4742558,3.0193126,22.06188])
+("escape_time",[(1.67f0,-20.0f0,10000.0f0),(1.67f0,-18.75f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-17.1875f0,10000.0f0),(1.67f0,-16.875f0,10000.0f0),(1.67f0,-16.5625f0,10000.0f0),(1.67f0,-16.25f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,-3.75f0,10000.0f0),(1.0f0,-2.5f0,10000.0f0),(1.0f0,-2.1875f0,10000.0f0),(1.0f0,-1.875f0,10000.0f0),(1.0f0,-1.5625f0,10000.0f0),(1.0f0,-1.25f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-18.75f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-17.1875f0,3000.0f0),(1.67f0,-16.875f0,3000.0f0),(1.67f0,-16.5625f0,3000.0f0),(1.67f0,-16.25f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,-3.75f0,3000.0f0),(1.0f0,-2.5f0,3000.0f0),(1.0f0,-2.1875f0,3000.0f0),(1.0f0,-1.875f0,3000.0f0),(1.0f0,-1.5625f0,3000.0f0),(1.0f0,-1.25f0,3000.0f0),(1.0f0,0.0f0,3000.0f0)],Float32[9001.501,7603.578,9200.24,7485.5806,8000.4487,6855.5244,7821.4727,1941.1722,7858.605,9000.347,7914.6646,8397.869,7041.611,8681.897,5533.7983,757.099,2402.3542,2342.3901,2520.6006,2406.1167,2460.4705,2471.007,2391.8904,1483.5739,2458.0642,2585.1277,2460.8428,2608.195,2587.0793,2531.8928,1871.9995,500.5663],Float32[427.9281,609.2124,387.44437,611.5474,571.3002,644.7658,567.1444,352.9742,568.10364,428.42224,556.24695,523.47455,636.83856,410.85556,620.2274,127.12749,170.75615,176.89185,156.91968,169.73253,164.50777,160.2837,169.38472,173.06796,157.07138,146.95602,164.39487,141.16035,145.88838,148.35486,194.45438,88.27813])
+("photon_count",[(1.67f0,-20.0f0,10000.0f0),(1.67f0,-18.75f0,10000.0f0),(1.67f0,-17.5f0,10000.0f0),(1.67f0,-17.1875f0,10000.0f0),(1.67f0,-16.875f0,10000.0f0),(1.67f0,-16.5625f0,10000.0f0),(1.67f0,-16.25f0,10000.0f0),(1.67f0,-15.0f0,10000.0f0),(1.0f0,-5.0f0,10000.0f0),(1.0f0,-3.75f0,10000.0f0),(1.0f0,-2.5f0,10000.0f0),(1.0f0,-2.1875f0,10000.0f0),(1.0f0,-1.875f0,10000.0f0),(1.0f0,-1.5625f0,10000.0f0),(1.0f0,-1.25f0,10000.0f0),(1.0f0,0.0f0,10000.0f0),(1.67f0,-20.0f0,3000.0f0),(1.67f0,-18.75f0,3000.0f0),(1.67f0,-17.5f0,3000.0f0),(1.67f0,-17.1875f0,3000.0f0),(1.67f0,-16.875f0,3000.0f0),(1.67f0,-16.5625f0,3000.0f0),(1.67f0,-16.25f0,3000.0f0),(1.67f0,-15.0f0,3000.0f0),(1.0f0,-5.0f0,3000.0f0),(1.0f0,-3.75f0,3000.0f0),(1.0f0,-2.5f0,3000.0f0),(1.0f0,-2.1875f0,3000.0f0),(1.0f0,-1.875f0,3000.0f0),(1.0f0,-1.5625f0,3000.0f0),(1.0f0,-1.25f0,3000.0f0),(1.0f0,0.0f0,3000.0f0)],Float32[18765.08,22772.82,36961.88,32587.68,35020.22,31506.1,36960.8,8809.44,14037.78,23141.98,29027.06,33174.3,30908.06,39779.74,27239.46,4455.86,5228.14,6923.74,9726.68,10962.22,10556.5,11446.36,11204.3,6798.4,4288.62,6549.88,8311.22,10232.54,10828.68,12012.6,9284.78,2833.5],Float32[1038.0585,1942.6053,1739.9796,2793.2056,2621.9434,3052.62,2791.7344,1652.5746,1061.8622,1163.3789,2138.1401,2133.6074,2845.194,1979.3943,3056.2278,765.5818,451.6921,617.24066,701.8534,870.786,789.4047,824.3389,855.7688,843.0482,295.76306,417.6063,600.4004,607.2298,655.91016,729.953,998.4106,504.1652])
+
+Script done on Fri 13 May 2016 08:11:17 PM EDT
