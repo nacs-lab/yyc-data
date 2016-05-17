681fd9363e6828de144afd972a58d78999362746
Yichao Yu <yyc1992@gmail.com>
2016-05-16 21:42:58 -0400

    [calculations/single-atom] results

---
+++ calculations/single-atom/results/imaging_line/gamma_5_12.jl
@@ -0,0 +1,49 @@
+脚本启动于 2016年05月16日 星期一 21时43分43秒
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module StructsOfArrays
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+WARNING: replacing module Compat
+start
+	From worker 9:	Running β=4.0, δ=-55.0, t=8000.0
+	From worker 3:	Running β=4.0, δ=-65.0, t=8000.0
+	From worker 2:	Running β=4.0, δ=-90.0, t=8000.0
+	From worker 8:	Running β=4.0, δ=-75.0, t=8000.0
+	From worker 7:	Running β=4.0, δ=-60.0, t=8000.0
+	From worker 4:	Running β=4.0, δ=-70.0, t=8000.0
+	From worker 6:	Running β=4.0, δ=-80.0, t=8000.0
+	From worker 5:	Running β=4.0, δ=-85.0, t=8000.0
+	From worker 6:	Running β=4.0, δ=-50.0, t=8000.0
+	From worker 9:	Running β=4.0, δ=-45.0, t=8000.0
+	From worker 8:	Running β=2.5, δ=-40.0, t=8000.0
+	From worker 3:	Running β=2.5, δ=-38.75, t=8000.0
+	From worker 7:	Running β=2.5, δ=-37.5, t=8000.0
+	From worker 2:	Running β=2.5, δ=-36.25, t=8000.0
+	From worker 4:	Running β=2.5, δ=-35.0, t=8000.0
+	From worker 5:	Running β=2.5, δ=-30.0, t=8000.0
+9002.517075 seconds (2.28 M allocations: 1.586 GB, 0.00% gc time)
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.
+WARNING: No working GUI backend found for matplotlib.WARNING: No working GUI backend found for matplotlib.
+
+WARNING: No working GUI backend found for matplotlib.
+("final_energy",[(4.0f0,-90.0f0,8000.0f0),(4.0f0,-85.0f0,8000.0f0),(4.0f0,-80.0f0,8000.0f0),(4.0f0,-75.0f0,8000.0f0),(4.0f0,-70.0f0,8000.0f0),(4.0f0,-65.0f0,8000.0f0),(4.0f0,-60.0f0,8000.0f0),(4.0f0,-55.0f0,8000.0f0),(4.0f0,-50.0f0,8000.0f0),(4.0f0,-45.0f0,8000.0f0),(2.5f0,-40.0f0,8000.0f0),(2.5f0,-38.75f0,8000.0f0),(2.5f0,-37.5f0,8000.0f0),(2.5f0,-36.25f0,8000.0f0),(2.5f0,-35.0f0,8000.0f0),(2.5f0,-30.0f0,8000.0f0)],Float32[76.52301,54.461582,14.0501175,10.975319,29.16741,111.87988,168.17157,171.71336,187.3574,181.75175,13.979293,11.025696,10.596816,15.700609,33.751225,126.14548],Float32[8.494044,7.916846,1.8949484,1.563882,4.383173,9.976129,13.722815,12.694484,14.071041,12.899759,1.7639608,1.2091087,1.4129683,2.268564,4.0295963,11.358149])
+("escape_time",[(4.0f0,-90.0f0,8000.0f0),(4.0f0,-85.0f0,8000.0f0),(4.0f0,-80.0f0,8000.0f0),(4.0f0,-75.0f0,8000.0f0),(4.0f0,-70.0f0,8000.0f0),(4.0f0,-65.0f0,8000.0f0),(4.0f0,-60.0f0,8000.0f0),(4.0f0,-55.0f0,8000.0f0),(4.0f0,-50.0f0,8000.0f0),(4.0f0,-45.0f0,8000.0f0),(2.5f0,-40.0f0,8000.0f0),(2.5f0,-38.75f0,8000.0f0),(2.5f0,-37.5f0,8000.0f0),(2.5f0,-36.25f0,8000.0f0),(2.5f0,-35.0f0,8000.0f0),(2.5f0,-30.0f0,8000.0f0)],Float32[5319.2124,5541.4595,6426.072,6882.4937,4616.022,368.88202,183.04106,276.124,382.81866,961.59827,6727.042,6561.642,7042.78,6241.333,5537.561,264.75934],Float32[501.35864,501.41412,450.02576,395.67386,482.26434,61.17854,34.499218,83.70943,164.98222,300.85236,416.68448,438.57068,370.31293,473.0659,482.44406,41.864807])
+("photon_count",[(4.0f0,-90.0f0,8000.0f0),(4.0f0,-85.0f0,8000.0f0),(4.0f0,-80.0f0,8000.0f0),(4.0f0,-75.0f0,8000.0f0),(4.0f0,-70.0f0,8000.0f0),(4.0f0,-65.0f0,8000.0f0),(4.0f0,-60.0f0,8000.0f0),(4.0f0,-55.0f0,8000.0f0),(4.0f0,-50.0f0,8000.0f0),(4.0f0,-45.0f0,8000.0f0),(2.5f0,-40.0f0,8000.0f0),(2.5f0,-38.75f0,8000.0f0),(2.5f0,-37.5f0,8000.0f0),(2.5f0,-36.25f0,8000.0f0),(2.5f0,-35.0f0,8000.0f0),(2.5f0,-30.0f0,8000.0f0)],Float32[548.2,1049.16,3255.62,14066.4,15947.44,652.24,212.34,191.24,147.14,184.48,11028.56,16141.02,24101.8,26353.62,22057.72,551.22],Float32[64.18132,114.875465,282.39005,980.7279,1772.1604,114.981544,39.314075,41.6213,31.659128,34.385906,818.5587,1216.304,1497.2123,2156.0305,2060.8271,96.10487])
+
+脚本完成，于 2016年05月17日 星期二 00时13分56秒
