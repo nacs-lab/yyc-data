ce09507fa61cf2eecc5a9876b756f0fc3cf5ec37
Yichao Yu <yyc1992@gmail.com>
2016-05-18 23:13:21 -0400

    [presentations/damop-2016] fix plot

--- presentations/damop-2016/plot.jl
+++ presentations/damop-2016/plot.jl
@@ -8,7 +8,7 @@ data2 = readdlm("CsPhotonsVsDetuning-7.txt")
 errorbar(data[:, 1], data[:, 2], data[:, 3],
          label="\$\\lambda=970nm\$", linewidth=2)
 errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-         label="\$\\lambda=930nm\$", linewidth=2)
+         label="\$\\lambda=935nm\$", linewidth=2)
 ylim([0, 25000])
 legend()
 grid()
