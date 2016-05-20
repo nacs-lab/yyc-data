027355ff452a5cc9c5fbaa2527c8d28cf2790abe
Yichao Yu <yyc1992@gmail.com>
2016-05-19 23:25:23 -0400

    [presentations/damop-2016] tweak photon count plot

--- presentations/damop-2016/plot.jl
+++ presentations/damop-2016/plot.jl
@@ -6,36 +6,41 @@ data = readdlm("CsPhotonsVsDetuning-4.txt")
 data2 = readdlm("CsPhotonsVsDetuning-7.txt")
 data3 = readdlm("CsPhotonsVsDetuning-3.txt")
 
-# errorbar(data[:, 1], data[:, 2], data[:, 3],
-#          label="\$\\lambda=970nm\$", linewidth=2)
-# errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-#          label="\$\\lambda=935nm\$", linewidth=2)
-# ylim([0, 25000])
-# legend()
-# grid()
-# xlabel("Detuning (MHz)", size=20)
-# ylabel("Photon count", size=20)
-# # show()
-# savefig("det-vs-photon-dc.png", bbox_inches="tight")
-
-# errorbar(data[:, 1], data[:, 2], data[:, 3],
-#          label="DC,\$\\lambda=970nm\$", linewidth=2)
-# errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-#          label="DC,\$\\lambda=935nm\$", linewidth=2)
-# ylim([0, 30000])
-# legend()
-# grid()
-# xlabel("Detuning (MHz)", size=20)
-# ylabel("Photon count", size=20)
-# # show()
-# savefig("det-vs-photon-dc2.png", bbox_inches="tight")
-
+figure()
 errorbar(data[:, 1], data[:, 2], data[:, 3],
-         label="DC,\$\\lambda=970nm\$", linewidth=2)
+         label="\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
 errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-         label="DC,\$\\lambda=935nm\$", linewidth=2)
+         label="\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
+ylim([0, 25000])
+legend()
+grid()
+xlabel("Detuning (MHz)", size=20)
+ylabel("Photon count", size=20)
+# show()
+savefig("det-vs-photon-dc.png", bbox_inches="tight")
+close()
+
+figure()
+errorbar(data[:, 1], data[:, 2], data[:, 3],
+         label="DC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
+errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
+         label="DC,\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
+ylim([0, 30000])
+legend()
+grid()
+xlabel("Detuning (MHz)", size=20)
+ylabel("Photon count", size=20)
+# show()
+savefig("det-vs-photon-dc2.png", bbox_inches="tight")
+close()
+
+figure()
+errorbar(data[:, 1], data[:, 2], data[:, 3],
+         label="DC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
+errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
+         label="DC,\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
 errorbar(data3[:, 1], data3[:, 2], data3[:, 3],
-         label="AC,\$\\lambda=970nm\$", linewidth=2)
+         label="AC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
 ylim([0, 30000])
 legend()
 grid()
@@ -43,3 +48,4 @@ xlabel("Detuning (MHz)", size=20)
 ylabel("Photon count", size=20)
 # show()
 savefig("det-vs-photon-ac.png", bbox_inches="tight")
+close()
