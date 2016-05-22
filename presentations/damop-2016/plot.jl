a3246e5895d8fe1a0077410470945b248399dd12
Yichao Yu <yyc1992@gmail.com>
2016-05-22 18:19:34 -0400

    [presentations/damop-2016] AC -> modulated

--- presentations/damop-2016/plot.jl
+++ presentations/damop-2016/plot.jl
@@ -22,9 +22,9 @@ close()
 
 figure()
 errorbar(data[:, 1], data[:, 2], data[:, 3],
-         label="DC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
+         label="\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
 errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-         label="DC,\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
+         label="\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
 ylim([0, 30000])
 legend()
 grid()
@@ -36,11 +36,11 @@ close()
 
 figure()
 errorbar(data[:, 1], data[:, 2], data[:, 3],
-         label="DC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
+         label="\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
 errorbar(data2[:, 1], data2[:, 2], data2[:, 3],
-         label="DC,\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
+         label="\$\\lambda=935nm\$", fmt="o-", linewidth=3, markersize=8)
 errorbar(data3[:, 1], data3[:, 2], data3[:, 3],
-         label="AC,\$\\lambda=970nm\$", fmt="o-", linewidth=3, markersize=8)
+         label="\$\\lambda=970nm\$\n(modulated)", fmt="o-", linewidth=3, markersize=8)
 ylim([0, 30000])
 legend()
 grid()
