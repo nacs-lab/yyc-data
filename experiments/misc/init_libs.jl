11043788af10dfd43fc10e7841225046d812f609
Yichao Yu <yyc1992@gmail.com>
2017-01-01 14:31:39 +0800

    [experiments/misc] Use NaCsCalc in the fitting notebook

--- experiments/misc/init_libs.jl
+++ experiments/misc/init_libs.jl
@@ -1,6 +1,7 @@
 push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
 
 using NaCsData
+import NaCsCalc: Trap
 using PyPlot
 matplotlib["rcParams"][:update](Dict("font.size" => 20,
                                      "font.weight" => "bold"))
