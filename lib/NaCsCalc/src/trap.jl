493ff230f82a01cf28936a77f51e1e3d44f93f91
Yichao Yu <yyc1992@gmail.com>
2017-01-01 09:56:06 +0800

    [lib/NaCsCalc] Thermal sideband

--- lib/NaCsCalc/src/trap.jl
+++ lib/NaCsCalc/src/trap.jl
@@ -38,4 +38,93 @@ function sideband{T1,T2,T3}(n1::T1, n2::T2,
     lag * exp(lpre)
 end
 
+@noinline function resize_caches_thread(tid, Ωcache, pcache)
+    old_len = size(Ωcache, 1)
+    resize!(Ωcache, tid)
+    resize!(pcache, tid)
+    for i in (old_len + 1):tid
+        Ωcache[i] = Vector{Float64}(1024)
+        pcache[i] = Vector{Float64}(1024)
+    end
+end
+
+@generated function thermal_sideband{N}(nbar::NTuple{N}, t, η::NTuple{N},
+                                        Δn::NTuple{N})
+    factor = ceil(Int, log(N * 1e3))
+    init = quote
+        nmax = ($((:(ceil(Int, nbar[$i] * $factor)) for i in 1:N)...),)
+        nbarp1 = ($((:(1 / (nbar[$i] + 1)) for i in 1:N)...),)
+        α = ($((:(nbar[$i] * nbarp1[$i]) for i in 1:N)...),)
+        s = 0.0
+    end
+    if N >= 2
+        push!(init.args, :(tid = Threads.threadid()))
+    end
+
+    @gensym p
+    @gensym Ω
+    body = :(s += $p * cos($Ω)^2)
+
+    for i in N:-1:2
+        @gensym j
+        @gensym p0
+        @gensym Ωlocal
+        @gensym plocal
+        Ωcache = [Vector{Float64}(1024)]
+        pcache = [Vector{Float64}(1024)]
+        setup = quote
+            if tid < size($Ωcache, 1)
+                resize_caches_thread(tid, $Ωcache, $pcache)
+            end
+            @inbounds begin
+                $Ωlocal = $Ωcache[tid]
+                $plocal = $pcache[tid]
+            end
+            if size($Ωlocal, 1) < nmax[$i] + 1
+                resize!($Ωlocal, nmax[$i] + 1)
+                resize!($plocal, nmax[$i] + 1)
+            end
+            local $p0 = nbarp1[$i]
+            @inbounds for $j in 0:nmax[$i]
+                local $j
+                $Ωlocal[$j + 1] = sideband($j, $j + Δn[$i], η[$i])
+                $plocal[$j + 1] = $p0
+                $p0 = $p0 * α[$i]
+            end
+        end
+        push!(init.args, setup)
+
+        prev_p = p
+        prev_Ω = Ω
+        @gensym p
+        @gensym Ω
+        body = quote
+            for $j = 0:nmax[$i]
+                local $j
+                $prev_p = $p * $plocal[$j + 1]
+                $prev_Ω = $Ω * $Ωlocal[$j + 1]
+                $body
+            end
+        end
+    end
+
+    @gensym j
+    @gensym p0
+    body = quote
+        local $p0 = nbarp1[1]
+        @inbounds @fastmath for $j = 0:nmax[1]
+            local $j
+            $Ω = sideband($j, $j + Δn[1], η[1]) * t
+            $p = $p0
+            $p0 = $p0 * α[1]
+            $body
+        end
+    end
+    return quote
+        $init
+        $body
+        return s
+    end
+end
+
 end
