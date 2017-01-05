2a49c93cbaf8e4fa324bb0323fcbd9f3d70dea00
Yichao Yu <yyc1992@gmail.com>
2017-01-05 19:00:53 +0800

    [lib/NaCsSim] Thermal init

--- lib/NaCsSim/src/system.jl
+++ lib/NaCsSim/src/system.jl
@@ -12,10 +12,29 @@ import ..Utils: HybridArray
 typealias State{T<:AbstractFloat,N} NTuple{N,HybridArray{T,3}}
 
 function (::Type{State{T,N}}){T,N}(nx, ny, nz)
-    ntuple(x->HybridArray{T,3}(nx, ny, nz), Val{N})
+    ntuple(x->HybridArray{T,3}(nx + 1, ny + 1, nz + 1), Val{N})
 end
 
 # Initial condition
+
+immutable ThermalInit{Idx,T<:AbstractFloat}
+    nx::T
+    ny::T
+    nz::T
+end
+
+function (init::ThermalInit{Idx,T}){Idx,T}(atomic_state::State{T})
+    for ary in atomic_state
+        Utils.zero!(ary)
+    end
+    ary = atomic_state[Idx]
+    nmax = size(ary.full)
+    nx = Utils.sample_thermal(init.nx, nmax[1] - 1)
+    ny = Utils.sample_thermal(init.ny, nmax[2] - 1)
+    nz = Utils.sample_thermal(init.nz, nmax[3] - 1)
+    push!(ary.sparse, ((nx + 1, ny + 1, nz + 1), Complex{T}(1)))
+    return
+end
 
 # Pulses
 
