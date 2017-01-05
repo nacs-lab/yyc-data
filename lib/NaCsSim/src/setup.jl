ba00d6652ada12f69dfdc69b84caed1460512005
Yichao Yu <yyc1992@gmail.com>
2017-01-05 18:26:15 +0800

    [lib/NaCsSim] Add init_atom callback

--- lib/NaCsSim/src/setup.jl
+++ lib/NaCsSim/src/setup.jl
@@ -4,13 +4,15 @@ module Setup
 
 import FunctionWrappers: FunctionWrapper
 
-immutable Sequence{AS,ES,I,M}
+immutable Sequence{AS,ES,IA,I,M}
+    init_atom::IA
     init::I
     pulses::Vector{FunctionWrapper{Bool,Tuple{AS,ES}}}
     measure::M
 end
 
 function run{AS,ES}(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES)
+    seq.init_atom(atomic_state)
     seq.init(atomic_state, extern_state)
     @inbounds for p in seq.pulses
         if !p(atomic_state, extern_state)
