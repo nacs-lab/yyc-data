5aeed32680b2f9d85f47cd44ea61d1b87bbb9313
Yichao Yu <yyc1992@gmail.com>
2017-01-06 17:18:55 +0800

    [lib/NaCsSim] Sequence builder

--- lib/NaCsSim/src/setup.jl
+++ lib/NaCsSim/src/setup.jl
@@ -22,4 +22,27 @@ function run{AS,ES}(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES)
     return seq.measure(atomic_state, extern_state)
 end
 
+immutable SeqBuilder{AS,ES,IA,I,M}
+    seq::Sequence{AS,ES,IA,I,M}
+    pulsemap::Dict{Any,Any}
+end
+function (::Type{SeqBuilder{AS,ES}}){AS,ES,IA,I,M}(init_atom::IA, init::I,
+                                                   measure::M)
+    seq = Sequence{AS,ES,IA,I,M}(init_atom, init,
+                                 FunctionWrapper{Bool,Tuple{AS,ES}}[],
+                                 measure)
+    return SeqBuilder(seq, Dict{Any,Any}())
+end
+
+compile_pulse(pulse) = pulse
+
+function add_pulse(builder::SeqBuilder, pulse)
+    real_pulse = get!(builder.pulsemap, pulse) do
+        compile_pulse(pulse)
+    end
+    pulses = builder.seq.pulses
+    push!(pulses, eltype(pulses)(real_pulse))
+    return
+end
+
 end
