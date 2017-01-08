619e469bca7146b21362c279076ae0cd96993ff6
Yichao Yu <yyc1992@gmail.com>
2017-01-08 15:19:55 +0800

    [lib/NaCsSim] Fix thermal init (set sum); split out set ns function

--- lib/NaCsSim/src/system.jl
+++ lib/NaCsSim/src/system.jl
@@ -16,6 +16,15 @@ function (::Type{State{T,N}}){T,N}(nx, ny, nz)
     ntuple(x->HybridArray{T,3}(nx + 1, ny + 1, nz + 1), Val{N})
 end
 
+function set_ns!{T}(atomic_state::State{T}, ary, nx, ny, nz)
+    for _ary in atomic_state
+        Utils.zero!(_ary)
+    end
+    ary.sum = 1
+    push!(ary.sparse, ((nx + 1, ny + 1, nz + 1), Complex{T}(1)))
+    return
+end
+
 # Initial condition
 
 immutable ThermalInit{Idx,T<:AbstractFloat}
@@ -25,15 +34,12 @@ immutable ThermalInit{Idx,T<:AbstractFloat}
 end
 
 function (init::ThermalInit{Idx,T}){Idx,T}(atomic_state::State{T})
-    for ary in atomic_state
-        Utils.zero!(ary)
-    end
     ary = atomic_state[Idx]
     nmax = size(ary.full)
     nx = Samplers.thermal(init.nx, nmax[1] - 1)
     ny = Samplers.thermal(init.ny, nmax[2] - 1)
     nz = Samplers.thermal(init.nz, nmax[3] - 1)
-    push!(ary.sparse, ((nx + 1, ny + 1, nz + 1), Complex{T}(1)))
+    set_ns!(atomic_state, ary, nx, ny, nz)
     return
 end
 
