64db731aa6af65c81370113e92a529fd455804e4
Yichao Yu <yyc1992@gmail.com>
2017-01-10 10:05:37 +0800

    [lib/NaCsSim] Raman pulse and temperature measure

--- lib/NaCsSim/src/system.jl
+++ lib/NaCsSim/src/system.jl
@@ -2,6 +2,7 @@
 
 module System
 
+import NaCsCalc: Trap
 import ..Utils
 import ..Samplers
 import ..Setup
@@ -121,7 +122,7 @@ function propagate_op!{T,N}(pulse::OPPulse{T}, state::State{T,N}, maxt::T)
 
     wf = state[n_i]
     # Then, given the initial hyperfine state, pick an initial vibrational state
-    v_i = Samplers.wavefunction(wf)
+    v_i = (-).(Samplers.wavefunction(wf), 1)
 
     # Finally, given the initial hyperfine+vibrational and final hyperfine state,
     # pick the final vibrational state.
@@ -145,9 +146,104 @@ function (pulse::OPPulse{T}){T,N}(state::State{T,N}, extern_state)
     return true
 end
 
+immutable Raman{T,N1,N2}
+    t::T
+    Ω::T
+    ηs::NTuple{3,T}
+    Δn::NTuple{3,Int}
+    nmax::NTuple{3,Int}
+end
+
+immutable RamanPulse{T,N1,N2}
+    t::T
+    Δn::NTuple{3,Int}
+    Ωs::NTuple{3,Vector{T}}
+end
+
+typealias RamanKey{T} Tuple{NTuple{3,T},NTuple{3,T},NTuple{3,Int}}
+typealias RamanCache{T} NTuple{3,Vector{T}}
+
+computeΩs{T}(Ω::T, η::T, Δn, nmax) =
+    T[Trap.sideband(n - 1, n - 1 + Δn, η) * Ω for n in 1:(nmax + abs(Δn) + 1)]
+
+function Setup.compile_pulse{T,N1,N2}(pulse::Raman{T,N1,N2}, cache)
+    type_cache = get!(cache, Raman{T}) do
+        Dict{RamanKey{T},RamanCache{T}}()
+    end::Dict{RamanKey{T},RamanCache{T}}
+    Ωs = get!(type_cache, (pulse.ηs, pulse.Δn, pulse.nmax)) do
+        Ωs1 = computeΩs(pulse.Ω, pulse.ηs[1], pulse.Δn[1], pulse.nmax[1])
+        Ωs2 = computeΩs(pulse.Ω, pulse.ηs[2], pulse.Δn[2], pulse.nmax[2])
+        Ωs3 = computeΩs(pulse.Ω, pulse.ηs[3], pulse.Δn[3], pulse.nmax[3])
+        return Ωs1, Ωs2, Ωs3
+    end
+    return RamanPulse{T,N1,N2}(pulse.t, pulse.Δn, Ωs)
+end
+
+function (pulse::RamanPulse{T,N1,N2}){T,N1,N2}(state::State{T}, extern_state)
+    ary1 = state[N1]
+    ary2 = state[N2]
+    # For now...
+    @assert !ary1.isfull && !ary2.isfull
+    l1 = length(ary1.sparse)
+    l2 = length(ary2.sparse)
+    l1 + l2 == 0 && return true
+    @assert l1 + l2 == 1
+    if l1 == 1
+        # forward
+        ele = ary1.sparse[1]
+        Δn = pulse.Δn
+        hf1 = N1
+        hf2 = N2
+    else
+        # backward
+        ele = ary2.sparse[1]
+        Δn = (-).(pulse.Δn)
+        hf1 = N2
+        hf2 = N1
+    end
+    @assert abs2(ele[2]) ≈ 1
+    ns_i = ele[1]
+    ns_f = (+).(ns_i, Δn)
+    if ns_f[1] < 0 || ns_f[2] < 0 || ns_f[3] < 0
+        return true
+    end
+    sz_x, sz_y, sz_z = size(ary1.full)
+    if ns_f[1] >= sz_x || ns_f[2] >= sz_y || ns_f[3] >= sz_z
+        Utils.zero!(state)
+        return false
+    end
+    if l1 == 1
+        Ω = pulse.Ωs[1][ns_i[1]] * pulse.Ωs[2][ns_i[2]] * pulse.Ωs[3][ns_i[3]]
+    else
+        Ω = pulse.Ωs[1][ns_f[1]] * pulse.Ωs[2][ns_f[2]] * pulse.Ωs[3][ns_f[3]]
+    end
+    p = sin(Ω * pulse.t)^2
+    if rand() < p
+        set_ns!(state, state[hf2], ns_f...)
+    end
+    return true
+end
+
 # External state / measure
 immutable HyperFineMeasure
 end
 (::HyperFineMeasure)(state::State, extern_state) = [a.sum for a in state]
+
+immutable NBarMeasure
+end
+function (::NBarMeasure){T,N}(state::State{T,N}, extern_state)
+    res = zeros(T, 4)
+    for ary in state
+        res[4] += ary.sum
+        @assert !ary.isfull
+        for ele in ary.sparse
+            ns, p = ele
+            res[1] += ns[1] * p
+            res[2] += ns[2] * p
+            res[3] += ns[3] * p
+        end
+    end
+    return res
+end
 
 end
