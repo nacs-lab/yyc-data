7e8660f4f8c1d10dcb70a0a605f5dcfe3283d648
Yichao Yu <yyc1992@gmail.com>
2017-01-11 22:40:58 -0500

    [lib/NaCsSim] Ideal ground state cooling

--- lib/NaCsSim/test/runtests.jl
+++ lib/NaCsSim/test/runtests.jl
@@ -28,6 +28,7 @@ const branching_22 = Float32[1 / 3 0 0
 const η_raman1 = (η(60f3) / sqrt(2f0), 0f0, 0f0)
 const η_raman2 = (0f0, η(420f3) * sqrt(2f0), 0f0)
 const η_raman3 = (0f0, 0f0, η(420f3) * sqrt(2f0))
+const η_ramans = (η_raman1, η_raman2, η_raman3)
 
 # 1: (2, -2); 2: (2, -1); 3: (1, -1)
 # builder = BuilderT(init, Setup.Dummy(), System.HyperFineMeasure())
@@ -42,20 +43,69 @@ function f2op(t, γ1, γ2, γ2′=0f0 * γ2, ηs=η_op, ηdri=η_op_dri)
                  branching_22 .* γ2′)
     System.OP{Float32}(t, branching, ηs, ηdri, isσ)
 end
-raman_pulse1_6 = System.Raman{Float32,1,3}(8, 1, η_raman1, (-6, 0, 0), sz)
-raman_pulse2_2 = System.Raman{Float32,1,3}(5, 1, η_raman2, (0, -2, 0), sz)
-raman_pulse3_2 = System.Raman{Float32,1,3}(5, 1, η_raman3, (0, 0, -2), sz)
+function raman_pulse(ax, order, t, Ω=1)
+    ns = ntuple(i->i == ax ? -order : 0, Val{3})
+    System.Raman{Float32,1,3}(t, Ω, η_ramans[ax], ns, sz)
+end
 f2op_pulse = f2op(40, 5, 0.5)
 
-for i in 1:100
-    Setup.add_pulse(builder, raman_pulse1_6)
+for i in 1:30
+    Setup.add_pulse(builder, raman_pulse(1, 6, 8))
     Setup.add_pulse(builder, f2op_pulse)
-    Setup.add_pulse(builder, raman_pulse2_2)
+    Setup.add_pulse(builder, raman_pulse(2, 2, 5))
     Setup.add_pulse(builder, f2op_pulse)
-    Setup.add_pulse(builder, raman_pulse1_6)
+    Setup.add_pulse(builder, raman_pulse(1, 6, 8))
     Setup.add_pulse(builder, f2op_pulse)
-    Setup.add_pulse(builder, raman_pulse3_2)
+    Setup.add_pulse(builder, raman_pulse(3, 2, 5))
     Setup.add_pulse(builder, f2op_pulse)
 end
-@time Setup.run(builder.seq, state, nothing, 1)
-@show @time Setup.run(builder.seq, state, nothing, 10000)
+for i in 1:40
+    Setup.add_pulse(builder, raman_pulse(1, 4, 10))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(2, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 4, 10))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(3, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+end
+for i in 1:30
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(2, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(3, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+end
+for i in 1:20
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 1, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(2, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 1, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(3, 2, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+end
+for i in 1:300
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 1, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(2, 1, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 2, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(1, 1, 4))
+    Setup.add_pulse(builder, f2op_pulse)
+    Setup.add_pulse(builder, raman_pulse(3, 1, 5))
+    Setup.add_pulse(builder, f2op_pulse)
+end
+
+@show Setup.run(builder.seq, state, nothing, 1000000)
