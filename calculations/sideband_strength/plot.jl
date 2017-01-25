05975ff36705c31424514ad05ee09d861d4bde31
Yichao Yu <yyc1992@gmail.com>
2017-01-25 12:22:22 -0500

    [calculations/sideband_strength] Plot to include more sidebands and both axial and radial

--- calculations/sideband_strength/plot.jl
+++ calculations/sideband_strength/plot.jl
@@ -10,25 +10,37 @@ PyPlot.matplotlib["rcParams"][:update](Dict("font.size" => 20,
 PyPlot.matplotlib[:rc]("xtick", labelsize=25)
 PyPlot.matplotlib[:rc]("ytick", labelsize=25)
 
-const ns = 0:70
 const m_Na = 23e-3 / 6.02e23
 const η_ax = Trap.η(m_Na, 60e3, 2π / 589e-9) / √(2)
+const η_rad = Trap.η(m_Na, 400e3, 2π / 589e-9) * √(2)
 
-plot(ns, abs.(Trap.sideband.(ns, ns .- 4, η_ax)), "bo-",
-     label="\$\\Delta n=-4\$")
-plot(ns, abs.(Trap.sideband.(ns, ns .- 3, η_ax)), "go-",
-     label="\$\\Delta n=-3\$")
-plot(ns, abs.(Trap.sideband.(ns, ns .- 2, η_ax)), "yo-",
-     label="\$\\Delta n=-2\$")
-plot(ns, abs.(Trap.sideband.(ns, ns .- 1, η_ax)), "ro-",
-     label="\$\\Delta n=-1\$")
-text(5, 0.8, "\$\\eta\\approx$(@sprintf("%.2f", η_ax))\$", fontsize=30)
-ylim([0, 1])
-legend()
-grid()
-xlabel("n")
-ylabel("\$|\\langle n |e^{ikr}| n + \\Delta n \\rangle|\$")
-title("Sideband coupling strength")
-savefig(joinpath(ARGS[1], "coupling.svg"),
+const colors = ["k", "r", "y", "g", "c", "b", "m"]
+function plot_sidebands(ns, Δns, η)
+    for Δn in Δns
+        plot(ns, abs.(Trap.sideband.(ns, ns .+ Δn, η)), "$(colors[1 - Δn])o-",
+             label="\$\\Delta n=$(Δn)\$")
+    end
+    text(5, 0.8, "\$\\eta\\approx$(@sprintf("%.2f", η))\$", fontsize=30)
+    ylim([0, 1])
+    legend(bbox_to_anchor=(1.05, 1), loc=2)
+    grid()
+    xlabel("n")
+    ylabel("\$|\\langle n |e^{ikr}| n + \\Delta n \\rangle|\$")
+end
+figure()
+plot_sidebands(0:70, -6:0, η_ax)
+title("Axial coupling strength")
+savefig(joinpath(ARGS[1], "coupling_0.46_0-6.svg"),
         bbox_inches="tight", transparent=true)
+savefig(joinpath(ARGS[1], "coupling_0.46_0-6.png"),
+        bbox_inches="tight", transparent=true)
+close()
+figure()
+plot_sidebands(0:25, -2:0, η_rad)
+title("Radial coupling strength")
+savefig(joinpath(ARGS[1], "coupling_0.35_0-2.svg"),
+        bbox_inches="tight", transparent=true)
+savefig(joinpath(ARGS[1], "coupling_0.35_0-2.png"),
+        bbox_inches="tight", transparent=true)
+close()
 # show()
