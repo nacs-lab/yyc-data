ab133b77cf3afba6b1ce2f7a29bb032a659380a8
Yichao Yu <yyc1992@gmail.com>
2017-04-08 18:56:53 -0400

    [calculations/multistate-op] plot OP curve

--- calculations/multistate-op/script.jl
+++ calculations/multistate-op/script.jl
@@ -31,7 +31,7 @@ end
 coupling_D(D2::Bool, Ix2, F1x2, F2x2, mF1x2, mF2x2) =
     coupling_LS(0, 2, 1, D2 ? 3 : 1, Ix2, F1x2, F2x2, mF1x2, mF2x2)
 
-function na_scattering_d2(F1::Integer, mF1::Integer, pol::Integer, F2::Integer, mF2::Integer)
+function na_scattering_d2(F1::Integer, F2::Integer, mF1::Integer, mF2::Integer, pol::Integer)
     if !(F1 in (1, 2)) || !(F2 in (1, 2))
         throw(ArgumentError("Invalid F for Na"))
     elseif !(pol in (-1, 0, 1))
@@ -59,3 +59,120 @@ function na_scattering_d2(F1::Integer, mF1::Integer, pol::Integer, F2::Integer, 
     end
     return cpl
 end
+
+function na_scattering_d2_rate(F1::Integer, F2::Integer, mF1::Integer, mF2::Integer,
+                               pol::NTuple{3,Real}, δc::Real)
+    if F1 == 1
+        δ = δc + 664.360e6
+    else
+        δ = δc - 1.107266e9
+    end
+    rate = 0.0
+    for i in 1:3
+        p = pol[i]
+        if p == 0
+            continue
+        end
+        rate += na_scattering_d2(F1, F2, mF1, mF2, i - 2)^2 * p
+    end
+    return rate * (61.542e6 / δ)^2
+end
+
+const states = Tuple{Int,Int}[]
+for i in -2:2
+    push!(states, (2, i))
+end
+for i in -1:1
+    push!(states, (1, i))
+end
+const nstates = length(states)
+
+function gen_rates(pol, δ)
+    rates = Matrix{Float64}(nstates, nstates)
+    @inbounds for i in 1:nstates
+        state1 = states[i]
+        for j in 1:nstates
+            state2 = states[j]
+            rates[j, i] = na_scattering_d2_rate(state1[1], state2[1],
+                                                state1[2], state2[2], pol, δ)
+        end
+    end
+    return rates
+end
+
+const rates_f1_up = gen_rates((0.25, 0.5, 0.25), -25.0e9)
+const rates_f1_down = rates_f1_up
+const rates_f1_coprop = gen_rates((0.5, 0.0, 0.5), -25.0e9)
+const rates_f2_coprop = gen_rates((0.25, 0.5, 0.25), -25.0e9 - 1.77e9)
+const rates_f2_counterop = gen_rates((0, 0, 1), -25.0e9 - 1.77e9)
+
+function propagate(rates, init, ts)
+    # ∂ₜpᵢ = ΣⱼΓᵢⱼpⱼ - ΣⱼΓⱼᵢpᵢ
+    #      = ΣⱼAᵢⱼpⱼ
+    #  Aᵢⱼ = Γᵢⱼ - ΣₖΓₖᵢδᵢⱼ
+    nt = length(ts)
+    ns = length(init)
+    res = Matrix{Float64}(nt, ns)
+    A = Matrix{Float64}(ns, ns)
+    for i in 1:ns
+        s = 0.0
+        for j in 1:ns
+            r = rates[j, i]
+            A[j, i] = r
+            s += r
+        end
+        A[i, i] -= s
+    end
+    A2 = similar(A)
+    for i in 1:nt
+        t = ts[i]
+        A2 .= A .* t
+        res[i, :] = expm(A2) * init
+    end
+    return res
+end
+
+using PyPlot
+
+function plot_f12(rates, tscale)
+    init = Float64[1, 0, 0, 0, 0, 0, 0, 0]
+    init_f1 = Float64[0, 0, 0, 0, 0, 1, 0, 0]
+    r = sum(rates * init)
+    r_f1 = sum(rates * init_f1)
+    τ = 1 / r
+    τ_f1 = 1 / r_f1
+    τ_max = max(τ, τ_f1)
+    ts = linspace(0, τ_max * tscale)
+    res = propagate(rates, init, ts)
+    nt = length(ts)
+    f1 = Vector{Float64}(nt)
+    f2 = Vector{Float64}(nt)
+    for i in 1:nt
+        f2[i] = res[i, 1] + res[i, 2] + res[i, 3] + res[i, 4] + res[i, 5]
+        f1[i] = res[i, 6] + res[i, 7] + res[i, 8]
+    end
+    plot(ts, f1, label="F1")
+    plot(ts, f2, label="F2")
+    legend()
+    ylim(0, 1)
+    maxt = maximum(ts)
+    xlim(0, maxt)
+    axvline(τ_f1, color="blue")
+    axvline(τ, color="red")
+end
+
+ts = linspace(0, 2e7, 1000)
+
+figure()
+title("F2 Counter OP")
+plot_f12(rates_f2_counterop, 10)
+figure()
+title("F2 Co-prop")
+plot_f12(rates_f2_coprop, 10)
+figure()
+title("F1 Up/Down")
+plot_f12(rates_f1_up, 10)
+figure()
+title("F1 Co-prop")
+plot_f12(rates_f1_coprop, 10)
+show()
