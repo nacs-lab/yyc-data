6e5c4cdf2e609fd67c087e73e3c9a531a51947b3
Yichao Yu <yyc1992@gmail.com>
2017-04-14 09:45:11 -0400

    [calculations/raman-scatter] propagate_2state

--- calculations/raman-scatter/raman-scatter.jl
+++ calculations/raman-scatter/raman-scatter.jl
@@ -3,6 +3,153 @@
 # Compute Rabi flopping with the present of decay terms
 # The Hamiltonian is assumed to be time independent and the Rabi drive is on-resonance
 
+struct RabiDecayParams{T}
+    Γ₁::T
+    Γ₂::T
+    Δ::T
+    Ω::T
+    Γ::T
+    Δ²::T
+    Ω²::T
+    Γ²::T
+    Ω′²::T # Or Δ′²
+    Ω′::T # Or Δ′
+    ΔΩ′::T # Or ΔΔ′
+    ΓΩ′::T # Or ΓΔ′
+    Ω²pΓ²mΔ²::T
+    p2max::T
+    overdamp::Bool
+end
+
+function RabiDecayParams(Ω::T, Γ₁::T, Γ₂::T) where T
+    Δ = (Γ₁ - Γ₂) / 2
+    Γ = (Γ₁ + Γ₂) / 2
+    Δ² = Δ^2
+    Ω² = Ω^2
+    Γ² = Γ^2
+    overdamp = Ω² < Δ²
+    if overdamp
+        Ω′² = Δ² - Ω²
+    else
+        Ω′² = Ω² - Δ²
+    end
+    Ω′ = sqrt(Ω′²)
+    Ω²pΓ²mΔ² = Ω² + Γ² - Δ²
+    p2max = Ω^2 * Γ₂ / 2 / Γ / Ω²pΓ²mΔ²
+    return RabiDecayParams{T}(Γ₁, Γ₂, Δ, Ω, Γ, Δ², Ω², Γ², Ω′², Ω′, Δ * Ω′, Γ * Ω′, Ω²pΓ²mΔ²,
+                              p2max, overdamp)
+end
+
+"""
+    propagate_2states(params::RabiDecayParams, tmax) -> (t, i, ψ)
+
+The atom start in state 1 and is doing Rabi flopping with (angular) Rabi frequency `Ω`
+to state 2. The state 1(2) has a decay rate of `Γ₁`(`Γ₂`). Propagate this system under the
+modified Hamiltonian for quantum jump method for at most `tmax` or until a decay happens.
+
+Returns the actual time propagated `t`.
+If a decay has happend, `t` is the decay time. `i` (either `1` or `2`) is the state from which
+the decay occurs. `ψ` is unused.
+If no decay happens, `t == tmax`, `i == 0`, `ψ` is a tuple of the wavefunctions
+"""
+function propagate_2states_underdamp{T}(params::RabiDecayParams{T}, tmax, rd)
+    r = T(rand(rd)) * params.Ω′²
+    # Now find the t for which `ψ^2(t) * Ω′² = r`.
+    # First check if `ψ^2(tmax) * Ω′² > r`
+    t::T = tmax
+    Ω′t = params.Ω′ * t
+    @fastmath sinΩ′t = sin(Ω′t)
+    @fastmath cosΩ′t = cos(Ω′t)
+    @fastmath expΓt = exp(-params.Γ * t)
+    ψ² = -expΓt * muladd(params.ΔΩ′, sinΩ′t, muladd(params.Δ², cosΩ′t, -params.Ω²))
+    if ψ² > r
+        # No decay happened, return the wave function at tmax
+        Ω′t_2 = Ω′t / 2
+        @fastmath sinΩ′t_2 = sin(Ω′t_2)
+        @fastmath cosΩ′t_2 = cos(Ω′t_2)
+        ψ1 = muladd(params.Ω′, cosΩ′t_2, -params.Δ * sinΩ′t_2)
+        ψ2 = params.Ω * sinΩ′t_2
+        @fastmath factor = sqrt(expΓt / ψ²)
+        return (t, 0, (ψ1 * factor, ψ2 * factor))
+    end
+    # Tolerance
+    yδ = max(T(2e-7), eps(T) * 10) * params.Ω′²
+    # Find the root using a combination of newton's method and bisecting.
+    # The bisection is to avoid newton's method overshotting since the function we want
+    # to solve is known to have partial oscillation.
+    tlo::T = 0
+    thi::T = t
+    if r - ψ² <= yδ
+        @goto ret
+    end
+    if params.Ω′² - r <= yδ
+        return (tlo, 1, (one(T), zero(T)))
+    end
+    if params.Ω′ > 0
+        tthresh = min(T(2 / params.Ω′), T(tmax))
+    else
+        tthresh = T(tmax)
+    end
+    c1 = params.Γ * params.Ω²
+    c2 = params.ΔΩ′ * params.Γ₁
+    c3 = params.Δ * muladd(params.Γ, -params.Δ, params.Ω′²)
+    while thi - tlo > 5 * eps(T) * thi
+        if thi - tlo < tthresh
+            # Try Newton's method
+            diff = -expΓt * muladd(c3, cosΩ′t, -muladd(c2, sinΩ′t, - c1))
+            δ1 = (ψ² - r)
+            t2 = t - δ1 / diff
+            if tlo < t2 < thi
+                δt = thi - tlo
+                t = t2
+                Ω′t = params.Ω′ * t
+                @fastmath sinΩ′t = sin(Ω′t)
+                @fastmath cosΩ′t = cos(Ω′t)
+                @fastmath expΓt = exp(-params.Γ * t)
+                ψ² = -expΓt * muladd(params.ΔΩ′, sinΩ′t, muladd(params.Δ², cosΩ′t, -params.Ω²))
+                δ2 = ψ² - r
+                # We've already computed this point, even if it's not enough this time,
+                # might as well use it to narrow down the range
+                if δ2 < 0
+                    δ2 = -δ2
+                    δ2 < yδ && @goto ret
+                    thi = t
+                else
+                    δ2 < yδ && @goto ret
+                    tlo = t
+                end
+                δt2 = thi - tlo
+                # If we shrink either x or y by at least √2, keep using the Newton's method
+                # otherwise, shrink it further with bisect.
+                if δt2 * 1.4 <= δt || δ2 * 1.4 < abs(δ1)
+                    continue
+                end
+            end
+        end
+        t = (thi + tlo) / 2
+        Ω′t = params.Ω′ * t
+        @fastmath sinΩ′t = sin(Ω′t)
+        @fastmath cosΩ′t = cos(Ω′t)
+        @fastmath expΓt = exp(-params.Γ * t)
+        ψ² = -expΓt * muladd(params.ΔΩ′, sinΩ′t, muladd(params.Δ², cosΩ′t, -params.Ω²))
+        δ2 = ψ² - r
+        if δ2 < 0
+            -δ2 < yδ && @goto ret
+            thi = t
+        else
+            δ2 < yδ && @goto ret
+            tlo = t
+        end
+    end
+
+    @label ret
+    p2 = params.p2max * (1 - expΓt / params.Ω′² * muladd(params.ΓΩ′, sinΩ′t,
+                                                            -(muladd(params.Γ², cosΩ′t,
+                                                                     -params.Ω²pΓ²mΔ²))))
+    ptotal = 1 - ψ² / params.Ω′²
+    return (t, ptotal * rand(rd) < p2 ? 2 : 1, (one(T), zero(T)))
+end
+
 # Propagate the state `ϕ` by `nstep` number of time steps each of length `δt`
 # The decay matrix is `Γ`, the element `(i,j)` of this matrix represents the decay rate
 # from state `j` to state `i`.
@@ -128,51 +275,67 @@ function average(ϕ0, δt, nstep, Γ, i1, i2, Ω, n, rates, rd)
 end
 
 
-ϕ = [1.0, 0.0]
-δt = 1e-7
-nstep = 100_000
-Γ = [0 3e4
-      2e4 0]
-i1 = 1
-i2 = 2
-Ω = 2π * 400e3
+# ϕ = [1.0, 0.0]
+# δt = 1e-7
+# Γ = [0 3e4
+#       2e4 0]
+# i1 = 1
+# i2 = 2
+# Ω = 2π * 400e3
 
-using PyPlot
-pts = 0:10:1000
-res = Vector{Float64}(length(pts))
-unc = Vector{Float64}(length(pts))
+# using PyPlot
+# pts = 0:10:1000
+# res = Vector{Float64}(length(pts))
+# unc = Vector{Float64}(length(pts))
 
-function f(pts, ϕ, Γ, i1, i2, Ω, res, unc)
-    len = length(ϕ)
-    rates = Vector{eltype(Γ)}(len)
-    for i in 1:len
-        local s = zero(eltype(Γ))
-        for j in 1:len
-            s += Γ[j, i]
-        end
-        rates[i] = s
-    end
-    rds = [MersenneTwister(0) for i in 1:Threads.nthreads()]
-    # @show average(ϕ, 1.1e-7, 0, Γ, i1, i2, Ω, 10000, rates, rds[Threads.threadid()])
-    @time Threads.@threads for i in 1:length(pts)
-        local a, s
-        a, s = average(ϕ, 1.1e-7, pts[i], Γ, i1, i2, Ω, 10000, rates, rds[Threads.threadid()])
-        res[i] = a[1]
-        unc[i] = s[1]
-    end
-end
-f(pts, ϕ, Γ, i1, i2, Ω, res, unc)
-errorbar(pts, res, unc)
-# # for i in 1:npts
-# #     a, s = average(ϕ, 1.1e-7, 10 * (i - 1), Γ, i1, i2, 0, 10000)
-# #     res[i] = a[1]
-# #     unc[i] = s[1]
-# # end
-# # errorbar(1:npts, res, unc)
+# function f(pts, ϕ, Γ, i1, i2, Ω, res, unc)
+#     len = length(ϕ)
+#     rates = Vector{eltype(Γ)}(len)
+#     for i in 1:len
+#         local s = zero(eltype(Γ))
+#         for j in 1:len
+#             s += Γ[j, i]
+#         end
+#         rates[i] = s
+#     end
+#     rds = [MersenneTwister(0) for i in 1:Threads.nthreads()]
+#     # @show average(ϕ, 1.1e-7, 0, Γ, i1, i2, Ω, 10000, rates, rds[Threads.threadid()])
+#     @time Threads.@threads for i in 1:length(pts)
+#         local a, s
+#         a, s = average(ϕ, 1.1e-7, pts[i], Γ, i1, i2, Ω, 10000, rates, rds[Threads.threadid()])
+#         res[i] = a[1]
+#         unc[i] = s[1]
+#     end
+#     errorbar(pts, res, unc)
+#     @time Threads.@threads for i in 1:length(pts)
+#         local a, s
+#         a, s = average(ϕ, 1.1e-7 * 5, pts[i] ÷ 5, Γ, i1, i2, Ω, 10000, rates, rds[Threads.threadid()])
+#         res[i] = a[1]
+#         unc[i] = s[1]
+#     end
+#     errorbar(pts, res, unc)
+# end
+# f(pts, ϕ, Γ, i1, i2, Ω, res, unc)
+# # # for i in 1:npts
+# # #     a, s = average(ϕ, 1.1e-7, 10 * (i - 1), Γ, i1, i2, 0, 10000)
+# # #     res[i] = a[1]
+# # #     unc[i] = s[1]
+# # # end
+# # # errorbar(1:npts, res, unc)
 
-grid()
-show()
+# grid()
+# show()
 
-# @time @show average(ϕ, 5.5e-7, 200, Γ, i1, i2, 0, 1)
-# @time @show average(ϕ, 2.75e-7, 40_000, Γ, i1, i2, Ω, 100)
-# @time @show average(ϕ, 1.1e-7, 100_000, Γ, i1, i2, Ω, 100)
+# # @time @show average(ϕ, 5.5e-7, 200, Γ, i1, i2, 0, 1)
+# # @time @show average(ϕ, 2.75e-7, 40_000, Γ, i1, i2, Ω, 100)
+# # @time @show average(ϕ, 1.1e-7, 100_000, Γ, i1, i2, Ω, 100)
+
+params = RabiDecayParams(2.0, 0.5, 0.4)
+
+@code_native propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
+@show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
