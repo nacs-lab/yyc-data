e9c8a54bf736093f021b02eb7fe15658b128e924
Yichao Yu <yyc1992@gmail.com>
2017-04-16 00:53:03 -0400

    [calculations/raman-scatter] Remove slow version

--- calculations/raman-scatter/raman-scatter.jl
+++ calculations/raman-scatter/raman-scatter.jl
@@ -384,131 +384,6 @@ function average(Ω::T, Γ::AbstractMatrix{T}, rates::AbstractVector{T}, tmax, n
     return (avgϕ1, avgϕ2), (σϕ1, σϕ2)
 end
 
-# Propagate the state `ϕ` by `nstep` number of time steps each of length `δt`
-# The decay matrix is `Γ`, the element `(i,j)` of this matrix represents the decay rate
-# from state `j` to state `i`.
-# The Rabi flopping happens on resonant from state `i1` to state `i2` with Rabi frequency `Ω`
-function propagate0(ϕ, δt, nstep, Γ, i1, i2, Ω, nstate, rates, buff1, rd)
-    # Do propagation of coherent and dissipative part separately.
-    # Use precise exponentiation for the (potentially fast) Rabi flopping and use linear
-    # approximation for the decay probability.
-    # Compute the decay probability in the middle of the time step to improve precision.
-    if nstep == 0
-        return ϕ
-    end
-
-    # Half step
-    @fastmath sinΩt_2 = sin(Ω * δt / 4)
-    @fastmath cosΩt_2 = cos(Ω * δt / 4)
-    # Full step
-    @fastmath sinΩt = sin(Ω * δt / 2)
-    @fastmath cosΩt = cos(Ω * δt / 2)
-    # Decay rate
-    T = real(eltype(ϕ))
-    s::T = 0
-    for i in 1:nstate
-        s += abs2(ϕ[i])
-    end
-
-    # First compute the shifted wavefunction. We'll propagate full steps from here
-    # until the last step.
-    # This also checks bound so we can skip bound checking later
-    ϕ1 = ϕ[i1]
-    ϕ2 = ϕ[i2]
-    ϕ1, ϕ2 = ϕ1 * cosΩt_2 + ϕ2 * sinΩt_2, ϕ2 * cosΩt_2 - ϕ1 * sinΩt_2
-    ϕ[i1] = ϕ1
-    ϕ[i2] = ϕ2
-    @inbounds for i in 1:(nstep - 1)
-        # We are at `t = (i - 1/2) * δt`
-        # Decide if we need to decay first
-        @simd for j in 1:nstate
-            buff1[j] = abs2(ϕ[j]) * rates[j] * δt
-        end
-        # buff1 .= abs2.(ϕ) .* rates .* δt
-        r = rand(rd) * s
-        decay = 0
-        for j in 1:nstate
-            r -= buff1[j]
-            if r < 0
-                decay = j
-                break
-            end
-        end
-        if decay != 0
-            ϕ .= 0
-            # decay happend from state `decay`
-            # Decide which state it'll fall into
-            r = rand(rd) * rates[decay]
-            local k = 0
-            for k in 1:nstate
-                r -= Γ[k, decay]
-                if r < 0
-                    break
-                end
-            end
-            s = 1
-            if i1 == k
-                ϕ[i1] = cosΩt_2
-                ϕ[i2] = -sinΩt_2
-            elseif i2 == k
-                ϕ[i1] = sinΩt_2
-                ϕ[i2] = cosΩt_2
-            else
-                ϕ[k] = 1
-            end
-            continue
-        end
-        # No decay, cool. Propagate by a full time step to `t = (i + 1/2) * δt`
-        # First propagate the decay term
-        @fastmath factor = 1 / 2 / sqrt(s)
-        s = 0 # normalization
-        @simd for j in 1:nstate
-            v = ϕ[j] * (1 - rates[j] * δt / 2) * factor
-            s += abs2(v)
-            ϕ[j] = v
-        end
-        # Then propagate the drive
-        ϕ1 = ϕ[i1]
-        ϕ2 = ϕ[i2]
-        ϕ1, ϕ2 = ϕ1 * cosΩt + ϕ2 * sinΩt, ϕ2 * cosΩt - ϕ1 * sinΩt
-        ϕ[i1] = ϕ1
-        ϕ[i2] = ϕ2
-    end
-    ϕ1 = ϕ[i1]
-    ϕ2 = ϕ[i2]
-    ϕ1, ϕ2 = ϕ1 * cosΩt_2 + ϕ2 * sinΩt_2, ϕ2 * cosΩt_2 - ϕ1 * sinΩt_2
-    ϕ[i1] = ϕ1
-    ϕ[i2] = ϕ2
-    ϕ ./= √(s)
-    return ϕ
-end
-
-function average0(ϕ0, δt, nstep, Γ, i1, i2, Ω, n, rates, rd)
-    len = length(ϕ0)
-    ϕ = similar(ϕ0)
-    sumϕ = zeros(real(eltype(ϕ0)), len)
-    sumϕ² = zeros(real(eltype(ϕ0)), len)
-    T = real(eltype(ϕ))
-    buff1 = Vector{T}(len)
-    @inbounds for i in 1:n
-        ϕ .= ϕ0
-        propagate0(ϕ, δt, nstep, Γ, i1, i2, Ω, len, rates, buff1, rd)
-        for j in 1:len
-            ϕi = abs2(ϕ[j])
-            sumϕ[j] += ϕi
-            sumϕ²[j] += ϕi^2
-        end
-    end
-    @inbounds for i in 1:len
-        avgϕi = sumϕ[i] / n
-        avgϕ²i = sumϕ²[i] / n
-        sumϕ[i] = avgϕi
-        sumϕ²[i] = (avgϕ²i - avgϕi^2) / sqrt(n - 1)
-    end
-    return sumϕ, sumϕ²
-end
-
-
 function Γ_to_rates(Γ)
     len = size(Γ, 1)
     rates = Vector{eltype(Γ)}(len)
@@ -525,7 +400,6 @@ end
 ϕ = [1.0, 0.0]
 i1 = 1
 i2 = 2
-Ω = 2π * 20e3
 const δt = 1e-8
 
 using PyPlot
@@ -540,24 +414,6 @@ function f(pts, Γ, ϕ, i1, i2, Ω, res, unc, color)
     params1 = RabiDecayParams{Float64}(Ω, Γ₁, Γ₂)
     dump(params1)
     rds = [MersenneTwister(0) for i in 1:Threads.nthreads()]
-    res .= 0
-    unc .= 0
-    @time Threads.@threads for i in 1:length(pts)
-        local a, s
-        a, s = average0(ϕ, δt, pts[i], Γ, i1, i2, Ω, 2000, rates,
-                        rds[Threads.threadid()])
-        res[i] = a[1]
-        unc[i] = s[1]
-    end
-    errorbar(pts * δt, res, unc, fmt=".", label="1.1", color=color)
-    # @time Threads.@threads for i in 1:length(pts)
-    #     local a, s
-    #     a, s = average0(ϕ, δt / 100, pts[i] * 100, Γ, i1, i2, Ω, 10000,
-    #                     rates, rds[Threads.threadid()])
-    #     res[i] = a[1]
-    #     unc[i] = s[1]
-    # end
-    # errorbar(pts * δt, res, unc, fmt=".", label="0.55")
     res .= 0
     unc .= 0
     @time Threads.@threads for i in 1:length(pts)
@@ -581,12 +437,22 @@ function f(pts, Γ, ϕ, i1, i2, Ω, res, unc, color)
     end
     errorbar(pts * δt, res, unc, fmt="^-", label="0", color=color)
 end
+Ω = 2π * 20e3
 Γ = [2e4 0
       0 3e4] * 4
 f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "blue")
+Ω = 2π * 20e3
 Γ = [0 1e4
       3e4 0] * 4
 f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "red")
+Ω = 2π * 2e3
+Γ = [2e4 0
+      0 3e4] * 4
+f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "green")
+Ω = 2π * 2e3
+Γ = [0 1e4
+      3e4 0] * 4
+f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "orange")
 # Γ = [0 3e4
 #       3e4 0] * 2
 # f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "red")
@@ -618,21 +484,12 @@ f(pts, Γ, ϕ, i1, i2, Ω, res, unc, "red")
 # Γ = [0 3e4
 #       3e4 0] * 2
 # f(pts, Γ, ϕ, i1, i2, 0.1e3, res, unc, "blue")
-# for i in 1:npts
-#     a, s = average0(ϕ, δt, 10 * (i - 1), Γ, i1, i2, 0, 10000)
-#     res[i] = a[1]
-#     unc[i] = s[1]
-# end
-# errorbar(1:npts, res, unc)
 
 legend()
 grid()
 show()
 
 # @show propagate(Ω, Γ, rates, 2e-9 * 1000, Base.Random.GLOBAL_RNG)
-# # @time @show average0(ϕ, 5.5e-7, 200, Γ, i1, i2, 0, 1)
-# # @time @show average0(ϕ, 2.75e-7, 40_000, Γ, i1, i2, Ω, 100)
-# # @time @show average0(ϕ, 1.1e-7, 100_000, Γ, i1, i2, Ω, 100)
 
 function f2(Ω, Γ)
     rates = Γ_to_rates(Γ)
