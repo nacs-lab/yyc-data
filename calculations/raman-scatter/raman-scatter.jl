5891362abe9c2e67108b98c0c78db11e82d8b939
Yichao Yu <yyc1992@gmail.com>
2017-04-16 17:14:59 -0400

    [calculations/raman-scatter] More clean up

--- calculations/raman-scatter/raman-scatter.jl
+++ calculations/raman-scatter/raman-scatter.jl
@@ -7,7 +7,7 @@ using NaCsSim.DecayRabi: propagate, average, Γ_to_rates
 using PyPlot
 
 δt = 1e-8
-pts = 0:200:10000
+pts = 0:2:10000
 
 function f(δt, pts, Γ, Ω, color)
     res = Vector{Float64}(length(pts))
@@ -30,28 +30,28 @@ function f(δt, pts, Γ, Ω, color)
     unc .= 0
     @time Threads.@threads for i in 1:length(pts)
         local a, s
-        a, s = average(Ω32, Γ32, rates32, δt32 * pts[i], 10000)
+        a, s = average(Ω32, Γ32, rates32, δt32 * pts[i], 1000)
         res[i] = a[1]
         unc[i] = s[1]
     end
     errorbar(pts * δt, res, unc, fmt="^-", label="0", color=color)
 end
-Ω = 2π * 20e3
-Γ = [2e4 0
-      0 3e4] * 4
-f(δt, pts, Γ, Ω, "blue")
-Ω = 2π * 20e3
-Γ = [0 1e4
-      3e4 0] * 4
-f(δt, pts, Γ, Ω, "red")
-Ω = 2π * 2e3
-Γ = [2e4 0
-      0 3e4] * 4
-f(δt, pts, Γ, Ω, "green")
-Ω = 2π * 2e3
-Γ = [0 1e4
-      3e4 0] * 4
-f(δt, pts, Γ, Ω, "orange")
+# Ω = 2π * 20e3
+# Γ = [2e4 0
+#       0 3e4] * 4
+# f(δt, pts, Γ, Ω, "blue")
+# Ω = 2π * 20e3
+# Γ = [0 1e4
+#       3e4 0] * 4
+# f(δt, pts, Γ, Ω, "red")
+# Ω = 2π * 2e3
+# Γ = [2e4 0
+#       0 3e4] * 4
+# f(δt, pts, Γ, Ω, "green")
+# Ω = 2π * 2e3
+# Γ = [0 1e4
+#       3e4 0] * 4
+# f(δt, pts, Γ, Ω, "orange")
 # Γ = [0 3e4
 #       3e4 0] * 2
 # f(δt, pts, Γ, Ω, "red")
@@ -64,16 +64,17 @@ f(δt, pts, Γ, Ω, "orange")
 #     -exp(-Γ * t / 2) * (Γ / 2 / Ω′ * sin(Ω′ * t) + cos(Ω′ * t))
 # end
 # plot(ts, (1 .- y.(ts, Ω)) ./ 2, color="orange")
-# Γ = [3e4 0
-#       0 3e4] * 2
-# f(δt, pts, Γ, Ω, "blue")
-# ts = δt * pts
-# function y(t, Ω)
-#     Γ = 3e4 * 2
-#     Ω′ = √(Ω^2 - Γ^2 / 4)
-#     -exp(-Γ * t / 2) * (Γ / 2 / Ω′ * sin(Ω′ * t) + cos(Ω′ * t))
-# end
-# plot(ts, (1 .- y.(ts, Ω)) ./ 2, color="orange")
+Ω = 2π * 400e3
+Γ = [3e4 0
+      0 3e4] * 2
+f(δt, pts, Γ, Ω, "blue")
+ts = δt * pts
+function y(t, Ω)
+    Γ = 3e4 * 2
+    Ω′ = √(Ω^2 - Γ^2 / 4)
+    -exp(-Γ * t / 2) * (Γ / 2 / Ω′ * sin(Ω′ * t) + cos(Ω′ * t))
+end
+plot(ts, (1 .- y.(ts, Ω)) ./ 2, color="orange")
 # Γ = [0 4e4
 #       2e4 0] * 2
 # f(δt, pts, Γ, Ω, "cyan")
@@ -96,14 +97,6 @@ function f2(Ω, Γ)
         propagate(Ω, Γ, rates, 0.11e-3)
     end
 end
-f2(Ω, Γ)
+# f2(Ω, Γ)
 
 show()
-
-# @code_native propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
-# @show propagate_2states_underdamp(params, 100, Base.Random.GLOBAL_RNG)
