0626458b440b9eae3f19016dcab555261197ea0f
Yichao Yu <yyc1992@gmail.com>
2017-04-20 16:36:14 -0400

    [lib] Use more where

--- lib/NaCsSim/src/setup.jl
+++ lib/NaCsSim/src/setup.jl
@@ -11,8 +11,8 @@ struct Sequence{AS,ES,IA,I,M}
     measure::M
 end
 
-function run{AS,ES}(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES,
-                    res=create_measure(seq))
+function run(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES,
+             res=create_measure(seq)) where {AS,ES}
     seq.init_atom(atomic_state)
     seq.init(atomic_state, extern_state)
     @inbounds for p in seq.pulses
@@ -24,8 +24,7 @@ function run{AS,ES}(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES,
 end
 
 # TODO, run in parallel/run multiple sequences
-function run{AS,ES}(seq::Sequence{AS,ES},
-                    atomic_state::AS, extern_state::ES, n::Integer)
+function run(seq::Sequence{AS,ES}, atomic_state::AS, extern_state::ES, n::Integer) where {AS,ES}
     res = create_measure(seq)
     for i in 1:n
         run(seq, atomic_state, extern_state, res)
@@ -41,8 +40,8 @@ struct SeqBuilder{AS,ES,IA,I,M}
     pulsemap::Dict{Any,Any}
     datacache::Dict{Any,Any}
 end
-function (::Type{SeqBuilder{AS,ES}}){AS,ES,IA,I,M}(init_atom::IA, init::I,
-                                                   measure::M)
+function (::Type{SeqBuilder{AS,ES}})(init_atom::IA, init::I,
+                                     measure::M) where {AS,ES,IA,I,M}
     seq = Sequence{AS,ES,IA,I,M}(init_atom, init,
                                  FunctionWrapper{Bool,Tuple{AS,ES}}[],
                                  measure)
@@ -68,31 +67,31 @@ end
 struct CombinedMeasure{T<:Tuple}
     measures::T
     # Kill default constructors
-    (::Type{CombinedMeasure{T}}){T}(measures) = new{T}(measures)
+    CombinedMeasure{T}(measures) where T = new(measures)
 end
 CombinedMeasure(measures...) = CombinedMeasure{typeof(measures)}(measures)
-@generated function create_measure{T}(m::CombinedMeasure{T}, seq)
+@generated function create_measure(m::CombinedMeasure{T}, seq) where T
     N = length(T.parameters)
     quote
         ms = m.measures
         ($((:(create_measure(ms[$i], seq)) for i in 1:N)...),)
     end
 end
-@generated function (m::CombinedMeasure{T}){T}(res, state, extern_state)
+@generated function (m::CombinedMeasure{T})(res, state, extern_state) where {T}
     N = length(T.parameters)
     quote
         ms = m.measures
         ($((:(ms[$i](res[$i], state, extern_state)) for i in 1:N)...),)
     end
 end
-@generated function finalize_measure{T}(m::CombinedMeasure{T}, res, n)
+@generated function finalize_measure(m::CombinedMeasure{T}, res, n) where {T}
     N = length(T.parameters)
     quote
         ms = m.measures
         ($((:(finalize_measure(ms[$i], res[$i], n)) for i in 1:N)...),)
     end
 end
-@generated function abort_measure{T}(m::CombinedMeasure{T}, res, n)
+@generated function abort_measure(m::CombinedMeasure{T}, res, n) where T
     N = length(T.parameters)
     val = true
     for i in N:-1:1
