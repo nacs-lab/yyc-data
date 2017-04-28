0432195771e9c9fa3acf2d01d6e1beb8e1fc330c
Yichao Yu <yyc1992@gmail.com>
2017-04-27 23:17:12 -0400

    [calculations/sideband] OP depumping

--- calculations/sideband/sideband.jl
+++ calculations/sideband/sideband.jl
@@ -292,14 +292,7 @@ const trapscatter = System.Scatter{Float32}(eye(Float32, 8) .* 0.033e-3,
                                             ηs_trap(1, 1, 1), ηs_trap(1, 0, 0),
                                             zeros(Bool, 8, 8),
                                             (0, 1, 1))
-
-function create_op(t, p1, p2, pol)
-    bs1 = gen_f1op_spec(p1, pol)
-    bs2 = gen_f2op_spec(p2, pol)
-    s1s = [System.Scatter{Float32}(r[1], η_op, abs.(bs1.η), r[2], (0, 1, 1)) for r in bs1.rates]
-    s2s = [System.Scatter{Float32}(r[1], η_op, abs.(bs2.η), r[2], (0, 1, 1)) for r in bs2.rates]
-    System.MultiOP{Float32}(t, [s1s; s2s; trapscatter])
-end
+const op_pol = (0.9983, 0, 0.0017)
 
 function create_raman_raw(t, p1, p2, ramp1, ramp2, bs1::BeamSpec, bs2::BeamSpec, Ω0, Δn)
     Ω = sqrt(p1 * p2) * Ω0
@@ -353,23 +346,30 @@ function create_raman(t, p1, p2, ramp1, ramp2, ax, Δn=0)
     return create_raman_raw(t, p1, p2, ramp1, ramp2, rs.bs1, rs.bs2, rs.Ω0, get_Δns(ax, Δn))
 end
 create_wait(t) = System.MultiOP{Float32}(t, [trapscatter])
+function create_op(t, p1, p2, pol=op_pol)
+    bs1 = gen_f1op_spec(p1, pol)
+    bs2 = gen_f2op_spec(p2, pol)
+    s1s = [System.Scatter{Float32}(r[1], η_op, abs.(bs1.η), r[2], (0, 1, 1)) for r in bs1.rates]
+    s2s = [System.Scatter{Float32}(r[1], η_op, abs.(bs2.η), r[2], (0, 1, 1)) for r in bs2.rates]
+    System.MultiOP{Float32}(t, [s1s; s2s; trapscatter])
+end
 
 const BuilderT = Setup.SeqBuilder{System.StateC,Void}
 statec() = System.StateC(sz...)
 
 function create_sequence(t)
-    builder = BuilderT(System.ThermalInit{6,Float32}(0, 0, 0), Setup.Dummy(),
+    builder = BuilderT(System.ThermalInit{1,Float32}(0, 0, 0), Setup.Dummy(),
                        Setup.CombinedMeasure(System.NBarMeasure(),
                                              System.GroundStateMeasure(),
                                              System.HyperFineMeasure{8}()))
     # Setup.add_pulse(builder, create_wait(t * 1e3))
     # Setup.add_pulse(builder, create_raman(81, 0.100, 0.461, false, true, 1, -1))
     # Setup.add_pulse(builder, create_raman(t, 1, 1, false, false, 3, 1))
-    Setup.add_pulse(builder, create_op(t, 1, 0, (1, 0, 0)))
+    Setup.add_pulse(builder, create_op(t, 0, 0))
     return builder.seq
 end
 
-const params = linspace(0, 100, 100)
+const params = linspace(0, 100 * 1000, 100)
 res = @time threadmap(p->Setup.run(create_sequence(p), statec(), nothing, 100000), params)
 
 if interactive()
