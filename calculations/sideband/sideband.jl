71b7772c56c7dfead90fb9d20856ba32f93ac7b5
Yichao Yu <yyc1992@gmail.com>
2017-06-28 16:11:13 +0800

    [calculations/sideband] restore cooling simulation

--- calculations/sideband/sideband.jl
+++ calculations/sideband/sideband.jl
@@ -295,8 +295,7 @@ const trapscatter = System.Scatter{Float32}(eye(Float32, 8) .* 0.033e-3,
                                             ηs_trap(1, 1, 1), ηs_trap(1, 0, 0),
                                             zeros(Bool, 8, 8),
                                             (0, 1, 1))
-# const op_pol = (0.99358, 0, 0.00642)
-const op_pol = (1, 0, 0)
+const op_pol = (0.99358, 0, 0.00642)
 
 function create_raman_raw(t, p1, p2, ramp1, ramp2, bs1::BeamSpec, bs2::BeamSpec, Ω0, Δn)
     Ω = sqrt(p1 * p2) * Ω0
@@ -309,14 +308,16 @@ function create_raman_raw(t, p1, p2, ramp1, ramp2, bs1::BeamSpec, bs2::BeamSpec,
         p2 /= 3
         Ω /= 2
     end
+    # s1s = [System.Scatter{Float32}(p1 * 0 * r[1], η_op, abs.(bs1.η), r[2],
+    #                                (0, 1, 1)) for r in bs1.rates]
+    # s2s = [System.Scatter{Float32}(p2 * 0 * r[1], η_op, abs.(bs2.η), r[2],
+    #                                (0, 1, 1)) for r in bs2.rates]
     s1s = [System.Scatter{Float32}(p1 * r[1], η_op, abs.(bs1.η), r[2],
                                    (0, 1, 1)) for r in bs1.rates]
     s2s = [System.Scatter{Float32}(p2 * r[1], η_op, abs.(bs2.η), r[2],
                                    (0, 1, 1)) for r in bs2.rates]
-    # return System.RealRaman{Float32,1,6}(t, Ω, abs.(bs1.η .- bs2.η), Δn, sz,
-    #                                      [s1s; s2s; trapscatter])
     return System.RealRaman{Float32,1,6}(t, Ω, abs.(bs1.η .- bs2.η), Δn, sz,
-                                         [trapscatter])
+                                         [s1s; s2s; trapscatter])
 end
 
 struct RamanSpec{T}
@@ -364,11 +365,7 @@ const BuilderT = Setup.SeqBuilder{System.StateC,Void}
 statec() = System.StateC(sz...)
 
 function create_sequence(n)
-    # builder = BuilderT(System.ThermalInit{1,Float32}(18, 3, 2), Setup.Dummy(),
-    #                    Setup.CombinedMeasure(System.NBarMeasure(),
-    #                                          System.GroundStateMeasure(),
-    #                                          System.HyperFineMeasure{8}()))
-    builder = BuilderT(System.ThermalInit{1,Float32}(0, 0, 0), Setup.Dummy(),
+    builder = BuilderT(System.ThermalInit{1,Float32}(10.0, 3.6, 2.5), Setup.Dummy(),
                        Setup.CombinedMeasure(System.NBarMeasure(),
                                              System.GroundStateMeasure(),
                                              System.HyperFineMeasure{8}()))
@@ -394,103 +391,103 @@ function create_sequence(n)
         Setup.add_pulse(builder, op)
     end
 
-    # for i in 1:take_pulse(5)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -2)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(25, 1, 1, true, false, 3, -2)
-    #     add_raman_cool(21, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(5)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -8)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -2)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -8)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
-    #     add_raman_cool(25, 1, 1, true, false, 3, -2)
-    #     add_raman_cool(21, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(6)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -2)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
-    #     add_raman_cool(25, 1, 1, true, false, 3, -2)
-    #     add_raman_cool(21, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(7)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
-    #     add_raman_cool(60, 0.351, 0.740, false, false, 1, -5)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -2)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
-    #     add_raman_cool(60, 0.351, 0.740, false, false, 1, -5)
-    #     add_raman_cool(25, 1, 1, true, false, 3, -2)
-    #     add_raman_cool(21, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(7)
-    #     add_raman_cool(90, 0.351, 0.740, false, true, 1, -5)
-    #     add_raman_cool(38, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(90, 0.351, 0.740, false, true, 1, -5)
-    #     add_raman_cool(43, 1, 1, true, false, 3, -1)
-    #     add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
-    #     add_raman_cool(28, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(8)
-    #     add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
-    #     add_raman_cool(38, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
-    #     add_raman_cool(43, 1, 1, true, false, 3, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
-    #     add_raman_cool(28, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(10)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
-    #     add_raman_cool(38, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
-    #     add_raman_cool(43, 1, 1, true, false, 3, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
-    #     add_raman_cool(28, 1, 1, true, false, 3, -1)
-    # end
-    # for i in 1:take_pulse(50)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
-    #     add_raman_cool(38, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -1)
-    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
-    #     add_raman_cool(43, 1, 1, true, false, 3, -1)
-    #     add_raman_cool(60, 0.351, 0.740, false, true, 1, -1)
-    #     add_raman_cool(28, 1, 1, true, false, 3, -1)
-    # end
-    for i in 1:take_pulse(50)
-        add_raman_cool(70, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(100, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(21, 1, 1, true, false, 3, -1)
-        add_raman_cool(70, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(100, 0.1, 1, false, true, 1, -1)
+    for i in 1:take_pulse(5)
+        add_raman_cool(25, 1, 1, true, false, 2, -2)
         add_raman_cool(25, 1, 1, true, false, 2, -1)
-
-        add_raman_cool(70, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(100, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(43, 1, 1, true, false, 3, -1)
-        add_raman_cool(70, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(100, 0.1, 1, false, true, 1, -1)
-        add_raman_cool(38, 1, 1, true, false, 2, -1)
+        add_raman_cool(25, 1, 1, true, false, 3, -2)
+        add_raman_cool(21, 1, 1, true, false, 3, -1)
     end
-    Setup.add_pulse(builder, System.Filter((hf, v)->sum(v .* trap_freq) <= 12000f3))
+    for i in 1:take_pulse(5)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -8)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
+        add_raman_cool(25, 1, 1, true, false, 2, -2)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -8)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
+        add_raman_cool(25, 1, 1, true, false, 3, -2)
+        add_raman_cool(21, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(6)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
+        add_raman_cool(25, 1, 1, true, false, 2, -2)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -7)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
+        add_raman_cool(25, 1, 1, true, false, 3, -2)
+        add_raman_cool(21, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(7)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
+        add_raman_cool(60, 0.351, 0.740, false, false, 1, -5)
+        add_raman_cool(25, 1, 1, true, false, 2, -2)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(80, 0.351, 0.740, false, false, 1, -6)
+        add_raman_cool(60, 0.351, 0.740, false, false, 1, -5)
+        add_raman_cool(25, 1, 1, true, false, 3, -2)
+        add_raman_cool(21, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(7)
+        add_raman_cool(90, 0.351, 0.740, false, true, 1, -5)
+        add_raman_cool(38, 1, 1, true, false, 2, -1)
+        add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(90, 0.351, 0.740, false, true, 1, -5)
+        add_raman_cool(43, 1, 1, true, false, 3, -1)
+        add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
+        add_raman_cool(28, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(8)
+        add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
+        add_raman_cool(38, 1, 1, true, false, 2, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(75, 0.351, 0.740, false, true, 1, -4)
+        add_raman_cool(43, 1, 1, true, false, 3, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
+        add_raman_cool(28, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(10)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
+        add_raman_cool(38, 1, 1, true, false, 2, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -3)
+        add_raman_cool(43, 1, 1, true, false, 3, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
+        add_raman_cool(28, 1, 1, true, false, 3, -1)
+    end
+    for i in 1:take_pulse(50)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
+        add_raman_cool(38, 1, 1, true, false, 2, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -1)
+        add_raman_cool(25, 1, 1, true, false, 2, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -2)
+        add_raman_cool(43, 1, 1, true, false, 3, -1)
+        add_raman_cool(60, 0.351, 0.740, false, true, 1, -1)
+        add_raman_cool(28, 1, 1, true, false, 3, -1)
+    end
+    # for i in 1:take_pulse(50)
+    #     add_raman_cool(70, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(100, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(21, 1, 1, true, false, 3, -1)
+    #     add_raman_cool(70, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(100, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(25, 1, 1, true, false, 2, -1)
+
+    #     add_raman_cool(70, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(100, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(43, 1, 1, true, false, 3, -1)
+    #     add_raman_cool(70, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(100, 0.1, 1, false, true, 1, -1)
+    #     add_raman_cool(38, 1, 1, true, false, 2, -1)
+    # end
+    Setup.add_pulse(builder, System.Filter((hf, v)->sum(v .* trap_freq) <= 20000f3))
     return builder.seq
 end
 
 # const params = linspace(0, 3000000, 101)
-const params = 0:2:50
+const params = 0:2:100
 res = @time threadmap(p->Setup.run(create_sequence(p), statec(), nothing, 100000), params)
 
 if interactive()
