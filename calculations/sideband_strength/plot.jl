7071f51fc2ffd37b5655de138ca3786b801c0d9e
Yichao Yu <yyc1992@gmail.com>
2017-09-15 16:48:00 -0400

    [calculations/sideband_strength] New matrix elements

--- calculations/sideband_strength/plot.jl
+++ calculations/sideband_strength/plot.jl
@@ -12,32 +12,57 @@ PyPlot.matplotlib[:rc]("xtick", labelsize=25)
 PyPlot.matplotlib[:rc]("ytick", labelsize=25)
 
 const m_Na = 23e-3 / 6.02e23
-const η_ax = Trap.η(m_Na, 68.8e3, 2π / 589e-9) * 0.67
-const η_rad1 = Trap.η(m_Na, 430e3, 2π / 589e-9) * √(2)
-const η_rad2 = Trap.η(m_Na, 589.5e3, 2π / 589e-9) * √(2)
+const η_ax = Trap.η(m_Na, 87.5e3, 2π / 589e-9) * 0.67
+const η_rad1 = Trap.η(m_Na, 478e3, 2π / 589e-9) * √(2)
+const η_rad2 = Trap.η(m_Na, 499e3, 2π / 589e-9) * √(2)
 
-function calc_fraction(η)
-    m1 = Trap.sideband(0, 1, η)
-    m2 = Trap.sideband(1, 2, η)
-    @assert m1 < m2
-    mmax = m2
-    i = 2
+# function calc_fraction(η)
+#     m1 = Trap.sideband(0, 1, η)
+#     m2 = Trap.sideband(1, 2, η)
+#     @assert m1 < m2
+#     mmax = m2
+#     i = 2
+#     while true
+#         m = Trap.sideband(i, i + 1, η)
+#         if m > mmax
+#             mmax = m
+#         else
+#             break
+#         end
+#         i += 1
+#     end
+#     v1 = sin(m1 / mmax * π / 2)^2
+#     v2 = sin(m2 / mmax * π / 2)^2
+#     return v2 / v1
+# end
+
+# @show calc_fraction(η_ax)
+# @show calc_fraction(η_rad1)
+# @show calc_fraction(η_rad2)
+
+function find_max_mele(η, order)
+    mmax = Trap.sideband(0, order, η)
+    i = 1
     while true
-        m = Trap.sideband(i, i + 1, η)
-        if m > mmax
+        m = Trap.sideband(i, i + order, η)
+        if m >= mmax
             mmax = m
         else
             break
         end
+        i += 1
     end
-    v1 = sin(m1 / mmax * π / 2)^2
-    v2 = sin(m2 / mmax * π / 2)^2
-    return v2 / v1
+    return mmax
 end
 
-@show calc_fraction(η_ax)
-@show calc_fraction(η_rad1)
-@show calc_fraction(η_rad2)
+function sideband_max_ratio(η, orders)
+    m0 = Trap.sideband(0, 1, η)
+    return [m0 / find_max_mele(η, o) for o in orders]
+end
+
+@show sideband_max_ratio(η_ax, 1:6)
+@show sideband_max_ratio(η_rad1, 1:2)
+@show sideband_max_ratio(η_rad2, 1:2)
 
 const colors = ["k", "r", "y", "g", "c", "b", "m"]
 function plot_sidebands(ns, Δns, η)
@@ -72,17 +97,17 @@ end
 
 figure()
 plot_sidebands(0:70, -6:0, η_ax)
-title("Axial coupling strength")
-maybe_save(joinpath(@__DIR__, "imgs/coupling_0.43_0-6"))
+title("Axial Z coupling strength")
+maybe_save(joinpath(@__DIR__, "imgs/coupling_z_0.36_0-6"))
 
 figure()
 plot_sidebands(0:25, -2:0, η_rad1)
-title("Radial 2 coupling strength")
-maybe_save(joinpath(@__DIR__, "imgs/coupling_0.35_0-2"))
+title("Radial X coupling strength")
+maybe_save(joinpath(@__DIR__, "imgs/coupling_x_0.32_0-2"))
 
 figure()
 plot_sidebands(0:25, -2:0, η_rad2)
-title("Radial 3 coupling strength")
-maybe_save(joinpath(@__DIR__, "imgs/coupling_0.29_0-2"))
+title("Radial Y coupling strength")
+maybe_save(joinpath(@__DIR__, "imgs/coupling_y_0.32_0-2"))
 
 maybe_show()
