dda235c795a76c3730a5ae376343ac3d5f8f63bb
Yichao Yu <yyc1992@gmail.com>
2017-10-18 14:59:28 -0400

    [experiments/cs_rsc_201710] Cs hot sidebands 2

---
+++ experiments/cs_rsc_201710/plot_20171016_003707_3d.jl
@@ -0,0 +1,58 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+
+const iname_a = joinpath(@__DIR__, "data", "data_20171016_003707.csv")
+
+const data_a = NaCsData.load_count_csv(iname_a)
+
+const spec_a = OrderedDict(
+    :axial_z=>-20:2:120,
+    :radial_x=>-150:8:250,
+    :radial_y=>-150:8:250,
+)
+
+const split_a = NaCsData.split_data(data_a, spec_a)
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20171016_003707_3d")
+
+to_sideband(f) = (i, v)->(v - f)
+
+data_rx = NaCsData.map_params(to_sideband(46), split_a[:radial_x])
+data_ry = NaCsData.map_params(to_sideband(46), split_a[:radial_y])
+data_az = NaCsData.map_params(to_sideband(47), split_a[:axial_z])
+
+figure()
+NaCsPlot.plot_survival_data(data_rx, fmt="C0o-")
+grid()
+ylim([0, 1])
+title("Axis X (radial)")
+xlabel("Detuning from carrier (kHz)")
+ylabel("Survival")
+NaCsPlot.maybe_save("$(prefix)_rx")
+
+figure()
+NaCsPlot.plot_survival_data(data_ry, fmt="C0o-")
+grid()
+ylim([0, 1])
+title("Axis Y (radial)")
+xlabel("Detuning from carrier (kHz)")
+ylabel("Survival")
+NaCsPlot.maybe_save("$(prefix)_ry")
+
+figure()
+NaCsPlot.plot_survival_data(data_az, fmt="C0o-")
+grid()
+ylim([0, 0.5])
+title("Axis Z (axial)")
+xlabel("Detuning from carrier (kHz)")
+ylabel("Survival")
+NaCsPlot.maybe_save("$(prefix)_az")
+
+NaCsPlot.maybe_show()
