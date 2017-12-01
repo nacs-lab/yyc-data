dc3e3488da76bdaa4e5060ea792dd03c36346143
Yichao Yu <yyc1992@gmail.com>
2017-12-01 12:36:18 -0500

    [lib/NaCsData] add strip mat script

---
+++ lib/NaCsData/scripts/strip_mat.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/julia --startup-file=no
+
+using MAT
+
+iname = ARGS[1]
+oname = ARGS[2]
+if isdir(oname)
+    oname = joinpath(oname, "$(splitext(basename(iname))[1]).csv")
+end
+
+const mf = matopen(iname)
+const pl = read(mf, "ParamList")
+const sa = read(mf, "Analysis")["SingleAtomLogical"]
+const param_name = read(mf, "Scan")["ParamName"]
+close(mf)
+
+const out = matopen(oname, "w")
+write(out, "ParamList", pl)
+write(out, "SingleAtomLogical", sa)
+write(out, "ParamName", param_name)
+close(out)
