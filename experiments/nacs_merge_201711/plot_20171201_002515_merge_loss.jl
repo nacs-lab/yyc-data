9ce6df6b4fa0e78ee612a53a0ec03680c97bdfc0
Yichao Yu <yyc1992@gmail.com>
2017-12-01 14:44:42 -0500

    [nacs_merge_201711] fix selector

--- experiments/nacs_merge_201711/plot_20171201_002515_merge_loss.jl
+++ experiments/nacs_merge_201711/plot_20171201_002515_merge_loss.jl
@@ -19,22 +19,18 @@ function gen_selector(na, cs, survival_na)
     end
     function selector(logicals)
         @assert size(logicals, 2) == 1
-        if logicals[1] != na && logicals[2] != cs
+        if logicals[1] != na || logicals[2] != cs
             return Int[1, 0, 0]
         end
         return Int[1, 1, survival_na ? logicals[3] : logicals[4]]
     end
 end
 
+# Only first 9600 sequences are likely to be good
 data_na = NaCsData.select_count(params_a, logicals_a, gen_selector(true, false, true), 9600)
 data_cs = NaCsData.select_count(params_a, logicals_a, gen_selector(false, true, false), 9600)
 data_na_both = NaCsData.select_count(params_a, logicals_a, gen_selector(true, true, true), 9600)
 data_cs_both = NaCsData.select_count(params_a, logicals_a, gen_selector(true, true, false), 9600)
-
-# data_na = NaCsData.select_count(params_a, logicals_a, gen_selector(true, false, true))
-# data_cs = NaCsData.select_count(params_a, logicals_a, gen_selector(false, true, false))
-# data_na_both = NaCsData.select_count(params_a, logicals_a, gen_selector(true, true, true))
-# data_cs_both = NaCsData.select_count(params_a, logicals_a, gen_selector(true, true, false))
 
 const spec_a = OrderedDict(
     :lo_mf=>[0, 10, 20, 50, 100, 200, 500, 1000, 2000, 3000],
@@ -53,11 +49,11 @@ const prefix = joinpath(@__DIR__, "imgs", "data_20171201_002515")
 figure()
 # low stretched
 NaCsPlot.plot_survival_data(split_cs[:lo_mf], fmt="C1o-", label="Cs only")
-NaCsPlot.plot_survival_data(split_cs_both[:lo_mf], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na[:lo_mf], fmt="C0o-", label="Na only")
+NaCsPlot.plot_survival_data(split_cs_both[:lo_mf], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na_both[:lo_mf], fmt="C2o-", label="Na both")
 grid()
-ylim([0, 0.8])
+ylim([0, 1])
 legend()
 title("Cs (3, 3) and Na (1, 1)")
 xlabel("Wait time (ms)")
@@ -67,11 +63,11 @@ NaCsPlot.maybe_save("$(prefix)_lo_mf")
 figure()
 # high stretched
 NaCsPlot.plot_survival_data(split_cs[:hi_mf], fmt="C1o-", label="Cs only")
-NaCsPlot.plot_survival_data(split_cs_both[:hi_mf], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na[:hi_mf], fmt="C0o-", label="Na only")
+NaCsPlot.plot_survival_data(split_cs_both[:hi_mf], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na_both[:hi_mf], fmt="C2o-", label="Na both")
 grid()
-ylim([0, 0.8])
+ylim([0, 1])
 legend()
 title("Cs (4, 4) and Na (2, 2)")
 xlabel("Wait time (ms)")
@@ -81,11 +77,11 @@ NaCsPlot.maybe_save("$(prefix)_hi_mf")
 figure()
 # Mixed
 NaCsPlot.plot_survival_data(split_cs[:mixed], fmt="C1o-", label="Cs only")
-NaCsPlot.plot_survival_data(split_cs_both[:mixed], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na[:mixed], fmt="C0o-", label="Na only")
+NaCsPlot.plot_survival_data(split_cs_both[:mixed], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na_both[:mixed], fmt="C2o-", label="Na both")
 grid()
-ylim([0, 0.8])
+ylim([0, 1])
 legend()
 title("Mixed")
 xlabel("Wait time (ms)")
@@ -95,11 +91,11 @@ NaCsPlot.maybe_save("$(prefix)_mixed")
 figure()
 # Low F
 NaCsPlot.plot_survival_data(split_cs[:lo_f], fmt="C1o-", label="Cs only")
-NaCsPlot.plot_survival_data(split_cs_both[:lo_f], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na[:lo_f], fmt="C0o-", label="Na only")
+NaCsPlot.plot_survival_data(split_cs_both[:lo_f], fmt="C3o-", label="Cs both")
 NaCsPlot.plot_survival_data(split_na_both[:lo_f], fmt="C2o-", label="Na both")
 grid()
-ylim([0, 0.8])
+ylim([0, 1])
 legend()
 title("Low F")
 xlabel("Wait time (ms)")
