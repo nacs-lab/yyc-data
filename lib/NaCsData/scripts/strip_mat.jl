3c778e0194b53d1b3ca1953f7c272ef50b8e6548
Yichao Yu <yyc1992@gmail.com>
2018-01-08 23:28:59 -0500

    [lib/NaCsData] add option to save imamges

--- lib/NaCsData/scripts/strip_mat.jl
+++ lib/NaCsData/scripts/strip_mat.jl
@@ -2,20 +2,57 @@
 
 using MAT
 
-iname = ARGS[1]
-oname = ARGS[2]
-if isdir(oname)
-    oname = joinpath(oname, "$(splitext(basename(iname))[1]).mat")
+function process_args()
+    local iname, oname
+    images = false
+    i = 0
+    while i < length(ARGS)
+        i += 1
+        arg = ARGS[i]
+        if arg[1] != '-'
+            if !@isdefined(iname)
+                iname = arg
+            elseif !@isdefined(oname)
+                oname = arg
+            else
+                throw(ArgumentError("Extra argument: \"$arg\""))
+            end
+            continue
+        end
+        if arg == "--images"
+            images = true
+        else
+            throw(ArgumentError("Unknown option: \"$arg\""))
+        end
+    end
+    if !@isdefined(iname)
+        throw(ArgumentError("Missing input file name"))
+    elseif !@isdefined(oname)
+        throw(ArgumentError("Missing output file name"))
+    end
+    if stat(oname).size == 0
+        mkpath(oname, 0o755)
+    end
+    if isdir(oname)
+        oname = joinpath(oname, "$(splitext(basename(iname))[1]).mat")
+    end
+    return (iname=iname, oname=oname, images=images)
 end
 
-const mf = matopen(iname)
-const pl = read(mf, "ParamList")
-const sa = read(mf, "Analysis")["SingleAtomLogical"]
-const param_name = read(mf, "Scan")["ParamName"]
-close(mf)
+const opts = process_args()
 
-const out = matopen(oname, "w")
-write(out, "ParamList", pl)
-write(out, "SingleAtomLogical", sa)
-write(out, "ParamName", param_name)
-close(out)
+matopen(opts.iname) do mf
+    pl = read(mf, "ParamList")
+    sa = read(mf, "Analysis")["SingleAtomLogical"]
+    scan = read(mf, "Scan")
+    param_name = scan["ParamName"]
+
+    matopen(opts.oname, "w") do out
+        write(out, "ParamList", pl)
+        write(out, "SingleAtomLogical", sa)
+        write(out, "ParamName", param_name)
+        if opts.images
+            write(out, "Images", scan["Images"])
+        end
+    end
+end
