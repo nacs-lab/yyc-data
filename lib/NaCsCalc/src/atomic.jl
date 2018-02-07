cb7a0e2890c975fa16eb4f97552554c8d8dd739b
Yichao Yu <yyc1992@gmail.com>
2018-02-07 15:11:28 -0500

    [lib/NaCsCalc] calculate scattering rate

--- lib/NaCsCalc/src/atomic.jl
+++ lib/NaCsCalc/src/atomic.jl
@@ -12,6 +12,10 @@ relative to the reduced matrix element on two `L` states for a single electron a
 in the LS coupling regime (i.e. Hydrogen or Alkali-like).
 The `I` for the atom and `L`, `J`, `F` and `mF` for the two states are given in their value
 multiplying by two so they are all integers.
+
+Note that the reduced matrix element between L=0 and L=1 is `√(3)` that of the
+actual matrix element for each transition. This is a result of following the steck
+convention.
 """
 function coupling_LS(L1x2::Integer, L2x2::Integer, J1x2::Integer, J2x2::Integer, Ix2::Integer,
                      F1x2::Integer, F2x2::Integer, mF1x2::Integer, mF2x2::Integer)::Float64
@@ -152,9 +156,47 @@ struct Alkali
     Ix2::Int
     D1::Float64
     D2::Float64
+    Γ1::Float64
+    Γ2::Float64
     HF_S::NTuple{2,Float64}
     HF_P1_2::NTuple{2,Float64}
     HF_P3_2::NTuple{4,Float64}
+end
+
+function _check_j1_2(atom::Alkali, name, Fx2)
+    if Fx2 != atom.Ix2 + 1 && Fx2 != atom.Ix2 - 1
+        throw(ArgumentError(
+            "Invalid F for $(name)1/2 state with I=$(atom.Ix2 / 2): $(Fx2 / 2)"))
+    end
+end
+
+function _check_j3_2(atom::Alkali, Fx2)
+    if Fx2 != atom.Ix2 + 1 && Fx2 != atom.Ix2 - 1 && Fx2 != atom.Ix2 + 3 && Fx2 != atom.Ix2 - 3
+        throw(ArgumentError(
+            "Invalid F for P3/2 state with I=$(atom.Ix2 / 2): $(Fx2 / 2)"))
+    end
+end
+
+function _check_state(atom::Alkali, Lx2, Jx2, Fx2, mFx2=Fx2)
+    if Lx2 < 0 || Jx2 < 0 || Fx2 < 0 || mFx2 < 0
+        throw(ArgumentError("Negative angular momentum"))
+    elseif Lx2 == 0
+        Jx2 == 1 || throw(ArgumentError("Invalid J for S state: $(Jx2 / 2)"))
+        _check_j1_2(atom, :S, Fx2)
+    elseif Lx2 == 2
+        if Jx2 == 1
+            _check_j1_2(atom, :P, Fx2)
+        elseif Jx2 == 3
+            _check_j3_2(atom, Fx2)
+        else
+            throw(ArgumentError("Invalid J for P state: $(Jx2 / 2)"))
+        end
+    else
+        throw(ArgumentError("Invalid L $(Lx2 / 2)"))
+    end
+    if mFx2 > Fx2 || mFx2 < -Fx2 || (Fx2 - mFx2) % 2 != 0
+        throw(ArgumentError("Invalid m_F for F=$(Fx2 / 2): $(mFx2 / 2)"))
+    end
 end
 
 function _get_freq_s(atom, Jx2, Fx2)
@@ -184,40 +226,40 @@ function _get_freq(atom::Alkali, Lx2, Jx2, Fx2)
     return _get_freq_p(atom, Jx2, Fx2)
 end
 
-function _check_j1_2(atom::Alkali, name, Fx2)
-    if Fx2 != atom.Ix2 + 1 && Fx2 != atom.Ix2 - 1
-        throw(ArgumentError(
-            "Invalid F for $(name)1/2 state with I=$(atom.Ix2 / 2): $(Fx2 / 2)"))
-    end
-end
-
-function _check_j3_2(atom::Alkali, Fx2)
-    if Fx2 != atom.Ix2 + 1 && Fx2 != atom.Ix2 - 1 && Fx2 != atom.Ix2 + 3 && Fx2 != atom.Ix2 - 3
-        throw(ArgumentError(
-            "Invalid F for P3/2 state with I=$(atom.Ix2 / 2): $(Fx2 / 2)"))
-    end
-end
-
 @inline function get_freq(atom::Alkali, Lx2, Jx2, Fx2)
-    @boundscheck begin
-        if Lx2 < 0 || Jx2 < 0 || Fx2 < 0
-            throw(ArgumentError("Negative angular momentum"))
-        elseif Lx2 == 0
-            Jx2 == 1 || throw(ArgumentError("Invalid J for S state: $(Jx2 / 2)"))
-            _check_j1_2(atom, :S, Fx2)
-        elseif Lx2 == 2
-            if Jx2 == 1
-                _check_j1_2(atom, :P, Fx2)
-            elseif Jx2 == 3
-                _check_j3_2(atom, Fx2)
-            else
-                throw(ArgumentError("Invalid J for P state: $(Jx2 / 2)"))
-            end
-        else
-            throw(ArgumentError("Invalid L $(Lx2 / 2)"))
-        end
-    end
+    @boundscheck _check_state(atom, Lx2, Jx2, Fx2)
     _get_freq(atom, Lx2, Jx2, Fx2)
+end
+
+function _get_amp_j(atom, F1x2, mF1x2, F2x2, mF2x2, Jex2, freq0, p, sqrtΓ)::Float64
+    get_scale = function (Fex2, mFex2)
+        @inbounds freqe = get_freq(atom, 2, Jex2, Fex2)
+        return sqrtΓ / (2π) / (freqe - freq0)
+    end
+    return coupling_D_raman(Jex2 == 3, atom.Ix2, F1x2, F2x2, mF1x2, mF2x2, p, get_scale)
+end
+
+function _get_amp_p(atom, F1x2, mF1x2, F2x2, mF2x2, freq0, p)::Float64
+    return _get_amp_j(atom, F1x2, mF1x2, F2x2, mF2x2, 1, freq0, p, sqrt(atom.Γ1)) +
+        _get_amp_j(atom, F1x2, mF1x2, F2x2, mF2x2, 3, freq0, p, sqrt(atom.Γ2))
+end
+
+function get_scatter(atom::Alkali, Ω, freq, F1x2, mF1x2, F2x2, mF2x2, pol)
+    _check_state(atom, 0, 1, F1x2, mF1x2)
+    _check_state(atom, 0, 1, F2x2, mF2x2)
+    @inbounds freqg = get_freq(atom, 0, 1, F1x2)
+    freq0 = freqg + freq
+    rate = 0.0
+    for i in 1:3
+        p = pol[i]
+        if p == 0
+            continue
+        end
+        rate += abs2(_get_amp_p(atom, F1x2, mF1x2, F2x2, mF2x2, freq0, i - 2)) * p
+    end
+    # The 9 below is the factor of 3 (√3 in each leg) in amplitude coming from the
+    # reduced matrix element of L above.
+    return rate * Ω^2 / 8 * 9
 end
 
 end
