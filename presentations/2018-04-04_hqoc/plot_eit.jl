683c0ff3c6ea7de31d10e6d55fd01c1677d379e3
Yichao Yu <yyc1992@gmail.com>
2018-04-03 10:46:44 -0400

    [presentations/2018-04-04_hqoc] change eit plot

---
+++ presentations/2018-04-04_hqoc/plot_eit.jl
@@ -0,0 +1,27 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using MAT
+
+matopen(joinpath(@__DIR__, "AT_690peak_1.mat")) do f
+    global xplot, yplot, yerrplot
+    xplot = read(f, "xplot")
+    yplot = read(f, "yplot")
+    yerrplot = read(f, "yerrplot")
+end
+
+const prefix = joinpath(@__DIR__, "eit")
+
+figure()
+errorbar(xplot[3:end], yplot[3:end], yerrplot[3:end])
+xlabel("Two photon detuning (MHz)")
+ylim([0, 1])
+grid()
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
