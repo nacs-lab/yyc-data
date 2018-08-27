ffbd4921ad8fcf907514b9bad91e92d4fdde3006
Yichao Yu <yyc1992@gmail.com>
2018-08-27 17:06:50 -0400

    [calculations/clock_shift] redo scan

--- calculations/clock_shift/run.jl
+++ calculations/clock_shift/run.jl
@@ -1,6 +1,6 @@
 #!/usr/bin/julia
 
-const δ0s = -50e3:200:50e3
+const δ0s = -50e3:500:50e3
 const outdir = ARGS[1]
 const prange = parse.(Int, split(ARGS[2], ','))
 @assert all(0 .<= prange .< 8)
@@ -9,5 +9,5 @@ const prange = parse.(Int, split(ARGS[2], ','))
 
 @sync @distributed for p in prange
     BLAS.set_num_threads(2)
-    run(p, outdir, δ0s, f1=(30e3, 150e3, 150e3), f2=(30e3, 150e3, 150e3), z1=1.0, z2=1.0)
+    run(p, outdir, δ0s, f1=f_cs, f2=f_na, z1=0.4, z2=1.0)
 end
