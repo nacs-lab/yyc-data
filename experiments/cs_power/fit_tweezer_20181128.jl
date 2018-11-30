1ae36c40da64d16ea038bc06bad2556b7997adcc
Yichao Yu <yyc1992@gmail.com>
2018-11-30 09:11:11 -0500

    [experiments/cs_power] tweak plots for new innolume calibration

--- experiments/cs_power/fit_tweezer_20181128.jl
+++ experiments/cs_power/fit_tweezer_20181128.jl
@@ -10,10 +10,10 @@ using LsqFit
 import NaCsCalc.Format: Unc, Sci
 
 param_str(fit, i) = @sprintf("%.4f", fit.param[i])
-params_strs(fit) =
-    "\$a=$(param_str(fit, 1))\$\n\$b=$(param_str(fit, 2))\$\n\$c=$(param_str(fit, 3))\$"
-params_strs2(fit) =
-    "\$a=$(param_str(fit, 1))\$\n\$b=$(param_str(fit, 2))\$"
+params_strs(fit, suffix="") =
+    "\$a$suffix=$(param_str(fit, 1))\$\n\$b$suffix=$(param_str(fit, 2))\$\n\$c$suffix=$(param_str(fit, 3))\$"
+params_strs2(fit, suffix="") =
+    "\$a$suffix=$(param_str(fit, 1))\$\n\$b$suffix=$(param_str(fit, 2))\$"
 
 const iname_paaom = joinpath(@__DIR__, "data", "paaom_20181128.csv")
 const data_paaom = readdlm(iname_paaom, ',', Float64, skipstart=1)
@@ -30,20 +30,38 @@ const prefix = joinpath(@__DIR__, "imgs", "data_tweezer_20181128")
 model(x, p) = p[1] .* sin.(p[2] .* sin.(p[3] .* x).^2)
 model2(x, p) = p[1] .* sin.(p[2] .* x).^4
 model3(x, p) = p[1] .* cos.(p[2] .* x).^4
+model2′(x, p) = (4 * p[1] * p[2]) .* sin.(p[2] .* x).^3 .* cos.(p[2] .* x)
+model3′(x, p) = (-4 * p[1] * p[2]) .* cos.(p[2] .* x).^3 .* sin.(p[2] .* x)
+
+diff23(x, p2, p3) = model2(x, p2) .- model3(x, p3)
+diff23′(x, p2, p3) = model2′(x, p2) .- model3′(x, p3)
+
+function solve23(x0, p2, p3)
+    while true
+        y = diff23(x0, p2, p3)
+        y′ = diff23′(x0, p2, p3)
+        dx = y / y′
+        x1 = x0 - dx
+        if abs(dx) < 1e-8
+            return (x1, (model2(x1, p2) + model3(x1, p3)) / 2)
+        end
+        x0 = x1
+    end
+end
 
 plotmax_paaom = maximum(data_paaom[:, 1]) * 1.1
 plotamp_paaom = linspace(0, plotmax_paaom, 10000)
 fit_paaom = curve_fit(model, data_paaom[:, 1], data_paaom[:, 2],
                       [maximum(data_paaom[:, 2]), 1.5, 2.1])
 figure()
-plot(data_paaom[:, 1], data_paaom[:, 2], "o")
-plot(plotamp_paaom, model(plotamp_paaom, fit_paaom.param))
+plot(data_paaom[:, 1], data_paaom[:, 2], "C2o")
+plot(plotamp_paaom, model(plotamp_paaom, fit_paaom.param), "C0")
 title("PAAOM")
 grid()
 xlim([0, plotmax_paaom])
 ylim([0, ylim()[2]])
-text(0.35, 19, "\$a\\cdot\\sin(b\\cdot\\sin^2(c\\cdot AMP))\$", fontsize=18)
-text(0.45, 3, params_strs(fit_paaom), fontsize=20)
+text(0.32, 15.5, "\$a_1\\cdot\\sin(b_1\\cdot\\sin^2(c_1\\cdot AMP))\$", fontsize=18, color="C0")
+text(0.45, 3, params_strs(fit_paaom, "_1"), fontsize=20, color="C0")
 xlabel("PAAOM/AMP")
 ylabel("Power (mW)")
 NaCsPlot.maybe_save("$(prefix)_paaom")
@@ -55,20 +73,27 @@ fit_padpaom = curve_fit(model2, data_padpaom[:, 1], data_padpaom[:, 2],
                         [maximum(data_padpaom[:, 2]), 1.6])
 fit_padpaom2 = curve_fit(model3, data_padpaom2[:, 1], data_padpaom2[:, 2],
                          [maximum(data_padpaom2[:, 2]), 1.0])
+x0, y0 = solve23(0.3, fit_padpaom.param, fit_padpaom2.param)
 figure()
-plot(data_padpaom[:, 1], data_padpaom[:, 2], "C0o")
+plot(data_padpaom[:, 1], data_padpaom[:, 2], "C2o")
 plot(plotamp_padpaom, model2(plotamp_padpaom, fit_padpaom.param), "C0-", label="1st order")
-plot(data_padpaom2[:, 1], data_padpaom2[:, 2], "C1o")
+plot(data_padpaom2[:, 1], data_padpaom2[:, 2], "C3o")
 plot(plotamp_padpaom, model3(plotamp_padpaom, fit_padpaom2.param), "C1-", label="0th order")
+plot(x0, y0, "bs")
+ax = gca()
+ax[:annotate]("x=$(@sprintf("%.5f", x0))\ny=$(@sprintf("%.4f", y0))",
+              (x0, y0 + 0.3), xytext=(0.20, 29),
+              arrowprops=Dict(:arrowstyle=>"fancy", :color=>"b"),
+              color="b")
 title("PADPAOM")
-legend(fontsize="small")
+legend(ncol=2, fontsize="small")
 grid()
 xlim([0, plotmax_padpaom])
-ylim([0, 1.1 * max(maximum(data_padpaom[:, 2]), maximum(data_padpaom2[:, 2]))])
-text(0.45, 22, "\$a\\cdot\\sin^4(b\\cdot AMP)\$", fontsize=18, color="C0")
-text(0.47, 12, params_strs2(fit_padpaom), fontsize=20, color="C0")
-text(0.01, 21, "\$a\\cdot\\cos^4(b\\cdot AMP)\$", fontsize=18, color="C1")
-text(0.03, 11, params_strs2(fit_padpaom2), fontsize=20, color="C1")
+ylim([0, 1.2 * max(maximum(data_padpaom[:, 2]), maximum(data_padpaom2[:, 2]))])
+text(0.01, 21 - 5, "\$a_2\\cdot\\sin^4(b_2\\cdot AMP)\$", fontsize=18, color="C0")
+text(0.012, 11 - 5, params_strs2(fit_padpaom, "_2"), fontsize=20, color="C0")
+text(0.415, 22 - 5, "\$a_3\\cdot\\cos^4(b_3\\cdot AMP)\$", fontsize=18, color="C1")
+text(0.41, 12 - 5, params_strs2(fit_padpaom2, "_3"), fontsize=20, color="C1")
 xlabel("PADPAOM/AMP")
 ylabel("Power (mW)")
 NaCsPlot.maybe_save("$(prefix)_padpaom")
