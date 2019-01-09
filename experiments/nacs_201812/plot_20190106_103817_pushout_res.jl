e6ffc4c6471985316661bb908a7037a7e3e1ee84
Yichao Yu <yyc1992@gmail.com>
2019-01-09 08:11:49 -0500

    [experiments/nacs_201812] pushout resonance scan

---
+++ experiments/nacs_201812/plot_20190106_103817_pushout_res.jl
@@ -0,0 +1,42 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+using LsqFit
+using NaCsCalc.Atomic: all_scatter_D
+
+const inames = ["data_20190106_103817.mat"]
+const datas = [NaCsData.load_striped_mat(joinpath(@__DIR__, "data", iname)) for iname in inames]
+const maxcnts = [typemax(Int)]
+const specs_na = [14 .+ (-10:2:10)]
+const specs_cs = [10 .+ (-10:2:10)]
+select_datas(datas, selector, maxcnts, specs) =
+    [NaCsData.split_data(NaCsData.select_count(data..., selector, maxcnt), spec)
+     for (data, maxcnt, spec) in zip(datas, maxcnts, specs)]
+
+const datas_na = select_datas(datas, NaCsData.select_single((1,), (3,)), maxcnts, specs_na)
+const datas_cs = select_datas(datas, NaCsData.select_single((2,), (4,)), maxcnts, specs_cs)
+
+data_na = datas_na[1]
+data_cs = datas_cs[1]
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20190106_103817_pushout_res")
+
+figure()
+NaCsPlot.plot_survival_data(data_na, fmt="C0.-", label="Na")
+NaCsPlot.plot_survival_data(data_cs, fmt="C1.-", label="Cs")
+legend()
+grid()
+ylim([0, 1])
+title("Pushout")
+xlabel("Detuning (MHz)")
+ylabel("Survival")
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
