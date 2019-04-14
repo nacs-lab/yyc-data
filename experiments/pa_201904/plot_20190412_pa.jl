ee2bf7a2edb7e119b35e901538af24d5c3409a94
Yichao Yu <yyc1992@gmail.com>
2019-04-14 10:11:19 -0400

    [experiments/pa_201904] plot narrower PA lines?

---
+++ experiments/pa_201904/plot_20190412_pa.jl
@@ -0,0 +1,53 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+using LsqFit
+using MAT
+
+function load_data(names, selector)
+    local datas
+    for name in names
+        param, counts = matopen(joinpath(@__DIR__, "data", "data_$name.mat")) do fd
+            sg = read(fd, "ScanGroup")
+            f = sg["base"]["params"]["fWavemeter"]
+            c = read(fd, "SingleAtomLogical") .!= 0
+            return [f for _ in 1:size(c, 3)], c
+        end
+        data = NaCsData.select_count(param, counts, selector)
+        if !@isdefined datas
+            datas = data
+        else
+            datas = [datas; data]
+        end
+    end
+    return datas
+end
+
+const names_files = ["data_20190412_pa_names.mat",
+                     "data_20190413_pa_names.mat",
+                     "data_20190414_pa_names.mat"]
+const datas = [load_data(matopen(fd->read(fd, "names"), joinpath(@__DIR__, "data", names_file)),
+                         NaCsData.select_single((1, 2,), (3, 4,)))
+               for names_file in names_files]
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20190412_pa")
+
+figure()
+NaCsPlot.plot_survival_data(datas[1], fmt="C0.-", label="1ms")
+NaCsPlot.plot_survival_data(datas[2], fmt="C0.-")
+NaCsPlot.plot_survival_data(datas[3], fmt="C1.-", label="10ms")
+legend()
+grid()
+title("PA spectrum")
+xlabel("288XXX GHz")
+ylabel("Two-body survival")
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
