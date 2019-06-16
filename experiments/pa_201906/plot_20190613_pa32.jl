036a15dfe8a9f18b698d2e320051ee1b5a93a1ac
Yichao Yu <yyc1992@gmail.com>
2019-06-15 22:21:09 -0400

    [experiments/pa_201906] repeat 2ms PA

---
+++ experiments/pa_201906/plot_20190613_pa32.jl
@@ -0,0 +1,65 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+using LsqFit
+using MAT
+
+function load_data(names, selector)
+    local datas
+    for name in names
+        param, counts = matopen(joinpath(@__DIR__, "data", "data_$name.mat")) do fd
+            sg = read(fd, "ScanGroup")
+            f = sg["base"]["params"]["fWavemeter"]
+            c = read(fd, "SingleAtomLogical") .!= 0
+            return [f for _ in 1:size(c, 3)], c
+        end
+        data = NaCsData.select_count(param, counts, selector)
+        if !@isdefined datas
+            datas = data
+        else
+            datas = [datas; data]
+        end
+    end
+    return datas
+end
+
+const names_files = ["data_20190613_pa32_names.mat"]
+const datas = [load_data(matopen(fd->read(fd, "names"), joinpath(@__DIR__, "data", names_file)),
+                         NaCsData.select_single((1, 2,), (3, 4,)))
+               for names_file in names_files]
+const datas_na = [load_data(matopen(fd->read(fd, "names"),
+                                    joinpath(@__DIR__, "data", names_file)),
+                            NaCsData.select_single((1, 2,), (3,)))
+                  for names_file in names_files]
+const datas_cs = [load_data(matopen(fd->read(fd, "names"),
+                                    joinpath(@__DIR__, "data", names_file)),
+                            NaCsData.select_single((1, 2,), (4,)))
+                  for names_file in names_files]
+const datas_cs_cs = [load_data(matopen(fd->read(fd, "names"),
+                                       joinpath(@__DIR__, "data", names_file)),
+                               NaCsData.select_single((-1, 2,), (-3, 4,)))
+                     for names_file in names_files]
+const datas_na_na = [load_data(matopen(fd->read(fd, "names"),
+                                       joinpath(@__DIR__, "data", names_file)),
+                               NaCsData.select_single((1, -2,), (3, -4,)))
+                     for names_file in names_files]
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20190613_pa32")
+
+figure()
+NaCsPlot.plot_survival_data(datas[1], fmt="C0.-")
+grid()
+ylim([0.4, 0.7])
+title("PA spectrum")
+xlabel("307XXX GHz")
+ylabel("Survival")
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
