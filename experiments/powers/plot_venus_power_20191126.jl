26275fd2bf0975cff238313761f2475de0255021
Yichao Yu <yyc1992@gmail.com>
2019-11-26 14:51:14 -0500

    [experiments/powers] Power calibration for the new ti-sapph

---
+++ experiments/powers/plot_venus_power_20191126.jl
@@ -0,0 +1,26 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using LsqFit
+import NaCsCalc.Format: Unc, Sci
+
+const iname = joinpath(@__DIR__, "data", "venus_power_20191126.csv")
+const data = readdlm(iname, ',', Float64, skipstart=1)
+
+const prefix = joinpath(@__DIR__, "imgs", "venus_power_20191126")
+
+figure()
+plot(data[:, 1], data[:, 2], "C0o-")
+grid()
+ylim([0, 5])
+xlabel("Wavelength (nm)")
+ylabel("Output Power (W)")
+tight_layout()
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
