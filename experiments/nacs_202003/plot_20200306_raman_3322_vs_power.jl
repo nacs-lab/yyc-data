97b2b611361a33494452f2e9d50477307d91d82d
Yichao Yu <yyc1992@gmail.com>
2020-03-20 00:29:04 -0400

    [experiments/nacs_202003] Power dependency for -36 GHz

---
+++ experiments/nacs_202003/plot_20200306_raman_3322_vs_power.jl
@@ -0,0 +1,88 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+using LsqFit
+
+fit_data(model, x, y, p0; kws...) =
+    fit_data(model, x, y, nothing, p0; kws...)
+
+function fit_data(model, params, ratios, uncs, p0;
+                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
+    use_unc = uncs !== nothing
+    if plotx === nothing
+        lo = minimum(params)
+        hi = maximum(params)
+        span = hi - lo
+        mid = (hi + lo) / 2
+        if plot_lo === nothing
+            plot_lo = mid - span * plot_scale / 2
+            if plot_lo * lo <= 0
+                plot_lo = 0
+            end
+        end
+        if plot_hi === nothing
+            plot_hi = mid + span * plot_scale / 2
+            if plot_hi * hi <= 0
+                plot_hi = 0
+            end
+        end
+        plotx = linspace(plot_lo, plot_hi, 10000)
+    end
+    if use_unc
+        fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
+    else
+        fit = curve_fit(model, params, ratios, p0)
+    end
+    param = fit.param
+    unc = estimate_errors(fit)
+    return (param=param, unc=unc,
+            uncs=Unc.(param, unc, Sci),
+            plotx=plotx, ploty=model.(plotx, (fit.param,)))
+end
+
+function fit_survival(model, data, p0; use_unc=true, kws...)
+    if use_unc
+        params, ratios, uncs = NaCsData.get_values(data)
+        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
+    else
+        params, ratios, uncs = NaCsData.get_values(data, 0.0)
+        return fit_data(model, params, ratios[:, 2], p0; kws...)
+    end
+end
+
+powers = [21, 18, 12]
+freqs = [772.1289, 771.924, 771.323]
+freq_uncs = [0.0054, 0.057, 0.050]
+
+model_pwr1(x, p) = p[1] .* (x)
+model_pwr2(x, p) = p[1] .* (x).^2
+model_pwr(x, p) = p[1] .* x.^p[2]
+model_lin_off(x, p) = p[1] .+ p[2] .* x
+model_sqr_off(x, p) = p[1] .+ p[2] .* x .+ p[3] .* x.^2
+
+fit_freq = fit_data(model_lin_off, powers, freqs, freq_uncs, [770, 0.8])
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20200306_raman_3322_vs_power")
+
+figure()
+errorbar(powers, freqs, freq_uncs, fmt="C0.")
+plot(fit_freq.plotx, fit_freq.ploty, "C0", label="Linear")
+text(15.1, 771.45, "\$f=f_0+a \\cdot P\$", color="C0", fontsize="small")
+text(15.13, 771.26, ("\$f_0=$(fit_freq.uncs[1]) \\mathrm{MHz}\$\n" *
+                    "\$a=$(fit_freq.uncs[2] * 1000) \\mathrm{kHz}\\cdot\\mathrm{mW}^{-1}\$"),
+     color="C0", fontsize="small")
+legend(fontsize="small", loc="upper left", ncol=2)
+grid()
+title("Raman Resonance (288668.35 GHz)")
+xlabel("Tweezer Power (mW)")
+ylabel("Raman Frequency (MHz)")
+NaCsPlot.maybe_save("$(prefix)_f")
+
+NaCsPlot.maybe_show()
