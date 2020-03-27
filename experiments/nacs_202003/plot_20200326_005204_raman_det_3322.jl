a236e76289e562b6f3cc7bb303cfb9878b803af0
Yichao Yu <yyc1992@gmail.com>
2020-03-26 22:49:41 -0400

    [experiments/nacs_202003] Spectrum data

---
+++ experiments/nacs_202003/plot_20200326_005204_raman_det_3322.jl
@@ -0,0 +1,124 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsPlot
+using PyPlot
+using DataStructures
+using LsqFit
+
+const inames = ["data_20200326_005204.mat",
+                "data_20200326_101325.mat"]
+const datas = [NaCsData.load_striped_mat(joinpath(@__DIR__, "data", iname)) for iname in inames]
+const maxcnts = [typemax(Int),
+                 typemax(Int)]
+const specs = [(593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
+                522.7 .+ [-15; -4.5:1.5:4.5; 15], # 12 mW, 0.12 ms
+                447.5 .+ [-10; -3:0.6:3; 5], # 9 mW, 0.16 ms
+                369.5 .+ [-5; -2:0.5:2; 5], # 6 mW, 0.27 ms
+                287.5 .+ [-5; -0.6:0.15:0.6; 5], # 3 mW, 1 ms
+                ),
+               (593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
+                522.7 .+ [-15; -4.5:1.5:4.5; 15], # 12 mW, 0.12 ms
+                447.5 .+ [-10; -3:0.6:3; 5], # 9 mW, 0.16 ms
+                369.5 .+ [-5; -2:0.5:2; 5], # 6 mW, 0.27 ms
+                287.5 .+ [-5; -0.6:0.15:0.6; 5], # 3 mW, 0.9 ms
+                )]
+
+select_datas(datas, selector, maxcnts, specs) =
+    [NaCsData.split_data(NaCsData.select_count(data..., selector, maxcnt), spec)
+     for (data, maxcnt, spec) in zip(datas, maxcnts, specs)]
+
+fit_data(model, x, y, p0; kws...) =
+    fit_data(model, x, y, nothing, p0; kws...)
+
+function fit_data(model, params, ratios, uncs, p0;
+                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
+    use_unc = uncs !== nothing
+    if plotx === nothing
+        lo = minimum(params)
+        hi = maximum(params)
+        span = hi - lo
+        mid = (hi + lo) / 2
+        if plot_lo === nothing
+            plot_lo = mid - span * plot_scale / 2
+            if plot_lo * lo <= 0
+                plot_lo = 0
+            end
+        end
+        if plot_hi === nothing
+            plot_hi = mid + span * plot_scale / 2
+            if plot_hi * hi <= 0
+                plot_hi = 0
+            end
+        end
+        plotx = linspace(plot_lo, plot_hi, 10000)
+    end
+    if use_unc
+        fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
+    else
+        fit = curve_fit(model, params, ratios, p0)
+    end
+    param = fit.param
+    unc = estimate_errors(fit)
+    return (param=param, unc=unc,
+            uncs=Unc.(param, unc, Sci),
+            plotx=plotx, ploty=model.(plotx, (fit.param,)))
+end
+
+function fit_survival(model, data, p0; use_unc=true, kws...)
+    if use_unc
+        params, ratios, uncs = NaCsData.get_values(data)
+        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
+    else
+        params, ratios, uncs = NaCsData.get_values(data, 0.0)
+        return fit_data(model, params, ratios[:, 2], p0; kws...)
+    end
+end
+
+const datas_nacs = select_datas(datas, NaCsData.select_single((1, 2), (3, 4,)), maxcnts, specs)
+
+const prefix = joinpath(@__DIR__, "imgs", "data_20200326_005204_raman_det_3322")
+
+function model_lorentzian(x, p)
+    p[1] .- p[2] ./ (1 .+ ((x .- p[3]) ./ (p[4] / 2)).^2)
+end
+function model_gaussian(x, p)
+    p[1] .- p[2] ./ exp.(((x .- p[3]) ./ p[4]).^2)
+end
+# fit = fit_survival(model_lorentzian, data, [0.6, 0.2, 770.374, 0.02])
+
+powers = [15, 12, 9, 6, 3]
+times_old = [0.09, 0.12, 0.16, 0.27, 1]
+times_new = [0.09, 0.12, 0.16, 0.27, 0.9]
+
+figure(figsize=[12.6, 16.8])
+
+for i in 1:5
+    subplot(3, 2, i)
+    time_old = times_old[i]
+    time_new = times_new[i]
+    if time_old == time_new
+        title_suffix = ", $time_old ms"
+        label_old_suffix = ""
+        label_new_suffix = ""
+    else
+        title_suffix = ""
+        label_old_suffix = ", $time_old ms"
+        label_new_suffix = ", $time_new ms"
+    end
+    NaCsPlot.plot_survival_data(datas_nacs[1][i], fmt="C0.-", label="Old" * label_old_suffix)
+    NaCsPlot.plot_survival_data(datas_nacs[2][i], fmt="C1.-", label="New" * label_new_suffix)
+    title("288560 GHz, $(powers[i]) mW" * title_suffix)
+    legend()
+    grid()
+    xlabel("2-Photon Detuning (770XXX kHz)")
+    ylabel("Two-body survival")
+end
+tight_layout(pad=0.6)
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
