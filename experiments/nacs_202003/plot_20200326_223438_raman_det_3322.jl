c973e10d8ad82b9a90b5399466193cb85bf301c9
Yichao Yu <yyc1992@gmail.com>
2020-03-31 08:08:53 -0400

    [experiments/nacs_202003] Longer time scan for 15 and 6 mW

--- experiments/nacs_202003/plot_20200326_223438_raman_det_3322.jl
+++ experiments/nacs_202003/plot_20200326_223438_raman_det_3322.jl
@@ -12,9 +12,11 @@ using LsqFit
 
 const inames = ["data_20200326_101325.mat",
                 "data_20200326_223438.mat",
-                "data_20200327_004910.mat"]
+                "data_20200327_004910.mat",
+                "data_20200330_033004.mat"]
 const datas = [NaCsData.load_striped_mat(joinpath(@__DIR__, "data", iname)) for iname in inames]
 const maxcnts = [typemax(Int),
+                 typemax(Int),
                  typemax(Int),
                  typemax(Int)]
 const specs = [(593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
@@ -29,6 +31,9 @@ const specs = [(593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
                ([0], # 15 mW, 0 ms
                 593.0 .+ [-30; -7.5:1.5:7.5; 30], # 15 mW, 0.1 ms
                 593.0 .+ [-30; -7.5:1.5:7.5; 30], # 15 mW, 0.14 ms
+                ),
+               (593.0 .+ [-30; -7.5:1.5:7.5; 30], # 15 mW, 0.32 ms
+                369.5 .+ [-10; -2.5:0.5:2.5; 10], # 6 mW, 0.96 ms
                 )
                ]
 
@@ -93,6 +98,7 @@ const data_nacs_00 = datas_nacs[3][1]
 const data_nacs_05 = datas_nacs[1][1]
 const data_nacs_06 = [datas_nacs[2][1]; datas_nacs[3][2]]
 const data_nacs_10 = [datas_nacs[2][2]; datas_nacs[3][3]]
+const data_nacs_28 = datas_nacs[4][1]
 
 const prefix = joinpath(@__DIR__, "imgs", "data_20200326_005204_raman_det_3322")
 
@@ -105,10 +111,12 @@ end
 fit_05 = fit_survival(model_lorentzian, data_nacs_05, [0.35, 0.15, 594, 5])
 fit_06 = fit_survival(model_lorentzian, data_nacs_06, [0.35, 0.15, 594, 5])
 fit_10 = fit_survival(model_lorentzian, data_nacs_10, [0.35, 0.25, 594, 5])
+fit_28 = fit_survival(model_lorentzian, data_nacs_28, [0.35, 0.25, 594, 5])
 
 # @show fit_05.uncs
 # @show fit_06.uncs
 # @show fit_10.uncs
+# @show fit_28.uncs
 
 figure()
 ratio_00, uncs_00 = get_ratio_val(data_nacs_00)
@@ -119,15 +127,20 @@ NaCsPlot.plot_survival_data(data_nacs_06, fmt="C2.", label="0.06 ms")
 plot(fit_06.plotx, fit_06.ploty, "C2")
 NaCsPlot.plot_survival_data(data_nacs_10, fmt="C3.", label="0.10 ms")
 plot(fit_10.plotx, fit_10.ploty, "C3")
-text(557.5, 0.055, ("\$\\Gamma_{0.10}=\\!$(fit_10.uncs[4]) kHz\$\n" *
+NaCsPlot.plot_survival_data(data_nacs_28, fmt="C4.", label="0.28 ms")
+plot(fit_28.plotx, fit_28.ploty, "C4")
+text(557.5, 0.055, ("\$\\Gamma_{0.28}=\\!$(fit_28.uncs[4]) kHz\$\n" *
+                    "\$f_{0.28}=\\!$(770 + fit_28.uncs[3] / 1000) MHz\$"),
+     color="C4", fontsize="x-small", linespacing=0.8)
+text(557.5, 0.090, ("\$\\Gamma_{0.10}=\\!$(fit_10.uncs[4]) kHz\$\n" *
                     "\$f_{0.10}=\\!$(770 + fit_10.uncs[3] / 1000) MHz\$"),
-     color="C3", fontsize="x-small")
-text(557.5, 0.105, ("\$\\Gamma_{0.06}=\\!$(fit_06.uncs[4]) kHz\$\n" *
+     color="C3", fontsize="x-small", linespacing=0.8)
+text(557.5, 0.125, ("\$\\Gamma_{0.06}=\\!$(fit_06.uncs[4]) kHz\$\n" *
                     "\$f_{0.06}=\\!$(770 + fit_06.uncs[3] / 1000) MHz\$"),
-     color="C2", fontsize="x-small")
-text(557.5, 0.155, ("\$\\Gamma_{0.05}=\\!$(fit_05.uncs[4]) kHz\$\n" *
+     color="C2", fontsize="x-small", linespacing=0.8)
+text(557.5, 0.160, ("\$\\Gamma_{0.05}=\\!$(fit_05.uncs[4]) kHz\$\n" *
                     "\$f_{0.05}=\\!$(770 + fit_05.uncs[3] / 1000) MHz\$"),
-     color="C1", fontsize="x-small")
+     color="C1", fontsize="x-small", linespacing=0.8)
 legend(fontsize="x-small", loc="lower right")
 grid()
 xlabel("2-Photon Detuning (770XXX kHz)")
--- experiments/nacs_202003/plot_20200327_234757_raman_det_3322.jl
+++ experiments/nacs_202003/plot_20200327_234757_raman_det_3322.jl
@@ -12,9 +12,11 @@ using LsqFit
 
 const inames = ["data_20200326_101325.mat",
                 "data_20200327_234757.mat",
-                "data_20200328_015629.mat"]
+                "data_20200328_015629.mat",
+                "data_20200330_033004.mat"]
 const datas = [NaCsData.load_striped_mat(joinpath(@__DIR__, "data", iname)) for iname in inames]
 const maxcnts = [typemax(Int),
+                 typemax(Int),
                  typemax(Int),
                  typemax(Int)]
 const specs = [(593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
@@ -28,6 +30,9 @@ const specs = [(593.0 .+ [-20; -6:2:6; 20], # 15 mW, 0.09 ms
                ([0], # 6 mW, 0 ms
                 369.5 .+ [-10; -2.5:0.5:2.5; 10], # 6 mW, 0.48 ms
                 369.5 .+ [-10; -2.5:0.5:2.5; 10], # 6 mW, 0.24 ms
+                ),
+               (593.0 .+ [-30; -7.5:1.5:7.5; 30], # 15 mW, 0.32 ms
+                369.5 .+ [-10; -2.5:0.5:2.5; 10], # 6 mW, 0.96 ms
                 )
                ]
 
@@ -92,6 +97,7 @@ const data_nacs_00 = datas_nacs[3][1]
 const data_nacs_20 = datas_nacs[3][3]
 const data_nacs_23 = datas_nacs[1][4]
 const data_nacs_44 = [datas_nacs[2][1]; datas_nacs[3][2]]
+const data_nacs_92 = datas_nacs[4][2]
 
 const prefix = joinpath(@__DIR__, "imgs", "data_20200327_234757_raman_det_3322")
 
@@ -104,10 +110,12 @@ end
 fit_20 = fit_survival(model_lorentzian, data_nacs_20, [0.35, 0.15, 369.5, 5])
 fit_23 = fit_survival(model_lorentzian, data_nacs_23, [0.35, 0.15, 369.5, 5])
 fit_44 = fit_survival(model_lorentzian, data_nacs_44, [0.35, 0.25, 369.5, 5])
+fit_92 = fit_survival(model_lorentzian, data_nacs_92, [0.35, 0.25, 369.5, 5])
 
 # @show fit_20.uncs
 # @show fit_23.uncs
 # @show fit_44.uncs
+# @show fit_92.uncs
 
 figure()
 ratio_00, uncs_00 = get_ratio_val(data_nacs_00)
@@ -118,15 +126,20 @@ NaCsPlot.plot_survival_data(data_nacs_23, fmt="C2.", label="0.23 ms")
 plot(fit_23.plotx, fit_23.ploty, "C2")
 NaCsPlot.plot_survival_data(data_nacs_44, fmt="C3.", label="0.44 ms")
 plot(fit_44.plotx, fit_44.ploty, "C3")
-text(357.6, 0.017, ("\$\\Gamma_{0.44}=\\!$(fit_44.uncs[4]) kHz\$\n" *
+NaCsPlot.plot_survival_data(data_nacs_92, fmt="C4.", label="0.92 ms")
+plot(fit_92.plotx, fit_92.ploty, "C4")
+text(357.6, 0.017, ("\$\\Gamma_{0.92}=\\!$(fit_92.uncs[4]) kHz\$\n" *
+                    "\$f_{0.92}=\\!$(770 + fit_92.uncs[3] / 1000) MHz\$"),
+     color="C4", fontsize="x-small", linespacing=0.8)
+text(357.6, 0.052, ("\$\\Gamma_{0.44}=\\!$(fit_44.uncs[4]) kHz\$\n" *
                     "\$f_{0.44}=\\!$(770 + fit_44.uncs[3] / 1000) MHz\$"),
-     color="C3", fontsize="x-small")
-text(357.6, 0.063, ("\$\\Gamma_{0.23}=\\!$(fit_23.uncs[4]) kHz\$\n" *
+     color="C3", fontsize="x-small", linespacing=0.8)
+text(357.6, 0.087, ("\$\\Gamma_{0.23}=\\!$(fit_23.uncs[4]) kHz\$\n" *
                     "\$f_{0.23}=\\!$(770 + fit_23.uncs[3] / 1000) MHz\$"),
-     color="C2", fontsize="x-small")
-text(357.6, 0.109, ("\$\\Gamma_{0.20}=\\!$(fit_20.uncs[4]) kHz\$\n" *
+     color="C2", fontsize="x-small", linespacing=0.8)
+text(357.6, 0.122, ("\$\\Gamma_{0.20}=\\!$(fit_20.uncs[4]) kHz\$\n" *
                     "\$f_{0.20}=\\!$(770 + fit_20.uncs[3] / 1000) MHz\$"),
-     color="C1", fontsize="x-small")
+     color="C1", fontsize="x-small", linespacing=0.8)
 legend(fontsize="x-small", loc="lower right")
 grid()
 xlabel("2-Photon Detuning (770XXX kHz)")
