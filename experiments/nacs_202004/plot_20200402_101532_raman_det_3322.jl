3583815b33095ed32a00dd9a9fd1f5ac058223fe
Yichao Yu <yyc1992@gmail.com>
2020-04-27 00:05:25 -0400

    [experiments/nacs_202004] replot

--- experiments/nacs_202004/plot_20200402_101532_raman_det_3322.jl
+++ experiments/nacs_202004/plot_20200402_101532_raman_det_3322.jl
@@ -3,8 +3,10 @@
 push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
 
 import NaCsCalc.Format: Unc, Sci
+using NaCsCalc
 using NaCsCalc.Utils: interactive
 using NaCsData
+using NaCsData.Fitting: fit_data, fit_survival
 using NaCsPlot
 using PyPlot
 using DataStructures
@@ -21,53 +23,6 @@ select_datas(datas, selector, maxcnts, specs) =
     [NaCsData.split_data(NaCsData.select_count(data..., selector, maxcnt), spec)
      for (data, maxcnt, spec) in zip(datas, maxcnts, specs)]
 
-fit_data(model, x, y, p0; kws...) =
-    fit_data(model, x, y, nothing, p0; kws...)
-
-function fit_data(model, params, ratios, uncs, p0;
-                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
-    use_unc = uncs !== nothing
-    if plotx === nothing
-        lo = minimum(params)
-        hi = maximum(params)
-        span = hi - lo
-        mid = (hi + lo) / 2
-        if plot_lo === nothing
-            plot_lo = mid - span * plot_scale / 2
-            if plot_lo * lo <= 0
-                plot_lo = 0
-            end
-        end
-        if plot_hi === nothing
-            plot_hi = mid + span * plot_scale / 2
-            if plot_hi * hi <= 0
-                plot_hi = 0
-            end
-        end
-        plotx = linspace(plot_lo, plot_hi, 10000)
-    end
-    if use_unc
-        fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
-    else
-        fit = curve_fit(model, params, ratios, p0)
-    end
-    param = fit.param
-    unc = estimate_errors(fit)
-    return (param=param, unc=unc,
-            uncs=Unc.(param, unc, Sci),
-            plotx=plotx, ploty=model.(plotx, (fit.param,)))
-end
-
-function fit_survival(model, data, p0; use_unc=true, kws...)
-    if use_unc
-        params, ratios, uncs = NaCsData.get_values(data)
-        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
-    else
-        params, ratios, uncs = NaCsData.get_values(data, 0.0)
-        return fit_data(model, params, ratios[:, 2], p0; kws...)
-    end
-end
-
 const datas_nacs = select_datas(datas, NaCsData.select_single((1, 2), (3, 4,)), maxcnts, specs)
 
 const prefix = joinpath(@__DIR__, "imgs", "data_20200402_101532_raman_det_3322")
@@ -78,7 +33,7 @@ end
 function model_gaussian(x, p)
     p[1] .- p[2] ./ exp.(((x .- p[3]) ./ p[4]).^2)
 end
-fit15 = fit_survival(model_lorentzian, datas_nacs[1][1], [0.4, 0.3, 593, 0.02])
+fit15 = fit_survival(model_lorentzian, datas_nacs[1][1], [0.4, 0.35, 595.7, 0.04])
 fit6 = fit_survival(model_lorentzian, datas_nacs[1][2], [0.4, 0.3, 369.5, 0.02])
 
 figure(figsize=[12.6, 5.6])
