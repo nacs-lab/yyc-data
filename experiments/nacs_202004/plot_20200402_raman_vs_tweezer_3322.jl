c1c7e5284badb324c617f5c41a3f9a771cf34d27
Yichao Yu <yyc1992@gmail.com>
2020-04-27 00:15:21 -0400

    [experiments/nacs_202004] replot

--- experiments/nacs_202004/plot_20200402_raman_vs_tweezer_3322.jl
+++ experiments/nacs_202004/plot_20200402_raman_vs_tweezer_3322.jl
@@ -3,63 +3,18 @@
 push!(LOAD_PATH, joinpath(@__DIR__, "../../lib"))
 
 import NaCsCalc.Format: Unc, Sci
+using NaCsCalc
 using NaCsCalc.Utils: interactive
 using NaCsData
+using NaCsData.Fitting: fit_data, fit_survival
 using NaCsPlot
 using PyPlot
 using DataStructures
 using LsqFit
 
-fit_data(model, x, y, p0; kws...) =
-    fit_data(model, x, y, nothing, p0; kws...)
-
-function fit_data(model, params, ratios, uncs, p0;
-                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
-    use_unc = uncs !== nothing
-    if plotx === nothing
-        lo = minimum(params)
-        hi = maximum(params)
-        span = hi - lo
-        mid = (hi + lo) / 2
-        if plot_lo === nothing
-            plot_lo = mid - span * plot_scale / 2
-            if plot_lo * lo <= 0
-                plot_lo = 0
-            end
-        end
-        if plot_hi === nothing
-            plot_hi = mid + span * plot_scale / 2
-            if plot_hi * hi <= 0
-                plot_hi = 0
-            end
-        end
-        plotx = linspace(plot_lo, plot_hi, 10000)
-    end
-    if use_unc
-        fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
-    else
-        fit = curve_fit(model, params, ratios, p0)
-    end
-    param = fit.param
-    unc = estimate_errors(fit)
-    return (param=param, unc=unc,
-            uncs=Unc.(param, unc, Sci),
-            plotx=plotx, ploty=model.(plotx, (fit.param,)))
-end
-
-function fit_survival(model, data, p0; use_unc=true, kws...)
-    if use_unc
-        params, ratios, uncs = NaCsData.get_values(data)
-        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
-    else
-        params, ratios, uncs = NaCsData.get_values(data, 0.0)
-        return fit_data(model, params, ratios[:, 2], p0; kws...)
-    end
-end
-
 const tweezer = [668.35, 565, 560, 555]
-const freq = [1623, 616.54, 595.73, 577.41]
-const freq_unc = [38, 0.38, 0.86, 0.25]
+const freq = [1623, 616.56, 595.80, 577.44]
+const freq_unc = [30, 0.21, 0.47, 0.13]
 
 const prefix = joinpath(@__DIR__, "imgs", "data_20200402_ramann_vs_tweezer_3322")
 
