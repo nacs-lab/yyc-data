3088f66209454d2b40c2ff32b8e77d8ead92bdfd
Yichao Yu <yyc1992@gmail.com>
2020-04-26 22:43:38 -0400

    [experiments/nacs_202003] replot

--- experiments/nacs_202003/plot_20200315_133410_raman_time_3322.jl
+++ experiments/nacs_202003/plot_20200315_133410_raman_time_3322.jl
@@ -21,53 +21,6 @@ select_datas(datas, selector, maxcnts, specs) =
     [NaCsData.split_data(NaCsData.select_count(data..., selector, maxcnt), spec)
      for (data, maxcnt, spec) in zip(datas, maxcnts, specs)]
 
-fit_data(model, x, y, p0; kws...) =
-    fit_data(model, x, y, nothing, p0; kws...)
-
-function fit_data(model, params, ratios, uncs, p0;
-                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
-    use_unc = uncs !== nothing
-    if plotx === nothing
-        lo = minimum(params)
-        hi = maximum(params)
-        span = hi - lo
-        mid = (hi + lo) / 2
-        if plot_lo === nothing
-            plot_lo = mid - span * plot_scale / 2
-            if plot_lo * lo <= 0
-                plot_lo = 0
-            end
-        end
-        if plot_hi === nothing
-            plot_hi = mid + span * plot_scale / 2
-            if plot_hi * hi <= 0
-                plot_hi = 0
-            end
-        end
-        plotx = linspace(plot_lo, plot_hi, 10000)
-    end
-    if use_unc
-        fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
-    else
-        fit = curve_fit(model, params, ratios, p0)
-    end
-    param = fit.param
-    unc = estimate_errors(fit)
-    return (param=param, unc=unc,
-            uncs=Unc.(param, unc, Sci),
-            plotx=plotx, ploty=model.(plotx, (fit.param,)))
-end
-
-function fit_survival(model, data, p0; use_unc=true, kws...)
-    if use_unc
-        params, ratios, uncs = NaCsData.get_values(data)
-        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
-    else
-        params, ratios, uncs = NaCsData.get_values(data, 0.0)
-        return fit_data(model, params, ratios[:, 2], p0; kws...)
-    end
-end
-
 const datas_nacs = select_datas(datas, NaCsData.select_single((1, 2), (3, 4,)), maxcnts, specs)
 
 const data_12 = datas_nacs[1][1]
