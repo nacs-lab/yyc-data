9e45921d2a048e49dfdc5aa1549cb04138ce9cec
Yichao Yu <yyc1992@gmail.com>
2020-04-30 16:37:48 -0400

    [lib/NaCsData] support non-number X variables

--- lib/NaCsData/src/fitting.jl
+++ lib/NaCsData/src/fitting.jl
@@ -13,15 +13,28 @@ import ..get_values, ..SortedData
 
 export fit_data
 
+function curve_fit(model, params::AbstractArray{T}, args...; kws...) where T<:Number
+    return LsqFit.curve_fit(model, params, args...; kws...)
+end
+
+function curve_fit(origin_model, origin_xs, args...; kws...)
+    xs = 1:length(origin_xs)
+    function model(x, p)
+        x == xs && return origin_model(origin_xs, p)
+        return origin_model(origin_xs[x], p)
+    end
+    return LsqFit.curve_fit(model, xs, args...; kws...)
+end
+
 fit_data(model, x, y, p0; kws...) =
     fit_data(model, x, y, nothing, p0; kws...)
 
-function fit_data(model, params, ratios, uncs, p0;
+function fit_data(model, xs, ratios, uncs, p0;
                   plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1)
     use_unc = uncs !== nothing
     if plotx === nothing
-        lo = minimum(params)
-        hi = maximum(params)
+        lo = minimum(xs)
+        hi = maximum(xs)
         span = hi - lo
         mid = (hi + lo) / 2
         if plot_lo === nothing
@@ -40,27 +53,27 @@ function fit_data(model, params, ratios, uncs, p0;
     end
     if use_unc
         @static if VERSION >= v"1.0"
-            fit = curve_fit(model, params, ratios, uncs.^-2, p0)
+            fit = curve_fit(model, xs, ratios, uncs.^-2, p0)
         else
-            fit = curve_fit(model, params, ratios, uncs.^-(2/3), p0)
+            fit = curve_fit(model, xs, ratios, uncs.^-(2/3), p0)
         end
     else
-        fit = curve_fit(model, params, ratios, p0)
+        fit = curve_fit(model, xs, ratios, p0)
     end
     param = fit.param
     unc = sqrt.(diag(estimate_covar(fit)))
     return (param=param, unc=unc,
             uncs=Unc.(param, unc, Sci),
-            plotx=plotx, ploty=plotx === false ? false : model.(plotx, (fit.param,)))
+            plotx=plotx, ploty=plotx === false ? false : model(plotx, param))
 end
 
 function fit_survival(model, data::SortedData, p0; use_unc=true, kws...)
     if use_unc
-        params, ratios, uncs = get_values(data)
-        return fit_data(model, params, ratios[:, 2], uncs[:, 2], p0; kws...)
+        xs, ratios, uncs = get_values(data)
+        return fit_data(model, xs, ratios[:, 2], uncs[:, 2], p0; kws...)
     else
-        params, ratios, uncs = get_values(data, 0.0)
-        return fit_data(model, params, ratios[:, 2], p0; kws...)
+        xs, ratios, uncs = get_values(data, 0.0)
+        return fit_data(model, xs, ratios[:, 2], p0; kws...)
     end
 end
 end
