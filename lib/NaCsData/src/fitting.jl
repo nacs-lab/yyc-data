fb9240a5122ac02e8022163dc526b5f2408f035c
Yichao Yu <yyc1992@gmail.com>
2020-05-09 22:36:35 -0400

    [lib/NaCsData] get_plot_range

--- lib/NaCsData/src/fitting.jl
+++ lib/NaCsData/src/fitting.jl
@@ -24,36 +24,43 @@ function curve_fit(origin_model, origin_xs, args...; kws...)
     return LsqFit.curve_fit(model, xs, args...; kws...)
 end
 
+function get_plot_range(xs; plotx=nothing, plot_lo=nothing,
+                        plot_hi=nothing, plot_scale=1.1, plot_npts=10000)
+    if plotx !== nothing
+        return plotx
+    end
+    lo = minimum(xs)
+    hi = maximum(xs)
+    span = hi - lo
+    mid = (hi + lo) / 2
+    if plot_lo === nothing
+        plot_lo = mid - span * plot_scale / 2
+        if plot_lo * lo <= 0
+            plot_lo = 0
+        end
+    end
+    if plot_hi === nothing
+        plot_hi = mid + span * plot_scale / 2
+        if plot_hi * hi <= 0
+            plot_hi = 0
+        end
+    end
+    plotx = linspace(plot_lo, plot_hi, plot_npts)
+end
+
 fit_data(model, x, y, p0; kws...) =
     fit_data(model, x, y, nothing, p0; kws...)
 
-function fit_data(model, xs, ratios, uncs, p0;
-                  plotx=nothing, plot_lo=nothing, plot_hi=nothing, plot_scale=1.1, kws...)
+function fit_data(model, xs, ratios, uncs, p0; plotx=nothing, plot_lo=nothing,
+                  plot_hi=nothing, plot_scale=1.1, plot_npts=10000, kws...)
     use_unc = uncs !== nothing
-    if plotx === nothing
-        lo = minimum(xs)
-        hi = maximum(xs)
-        span = hi - lo
-        mid = (hi + lo) / 2
-        if plot_lo === nothing
-            plot_lo = mid - span * plot_scale / 2
-            if plot_lo * lo <= 0
-                plot_lo = 0
-            end
-        end
-        if plot_hi === nothing
-            plot_hi = mid + span * plot_scale / 2
-            if plot_hi * hi <= 0
-                plot_hi = 0
-            end
-        end
-        plotx = linspace(plot_lo, plot_hi, 10000)
-    end
     if use_unc
         fit = curve_fit(model, xs, ratios, uncs.^-2, p0; kws...)
     else
         fit = curve_fit(model, xs, ratios, p0; kws...)
     end
+    plotx = get_plot_range(xs; plotx=plotx, plot_lo=plot_lo,
+                           plot_hi=plot_hi, plot_scale=plot_scale, plot_npts=plot_npts)
     param = fit.param
     unc = sqrt.(diag(estimate_covar(fit)))
     return (param=param, unc=unc,
