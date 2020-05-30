a0a7dfe1cd9627ad3f22812ac6a9d1411fb832c3
Yichao Yu <yyc1992@gmail.com>
2020-05-30 14:05:46 -0400

    [presentations/damop-2020] thicker ratio line

--- presentations/damop-2020/plot_raman.jl
+++ presentations/damop-2020/plot_raman.jl
@@ -36,30 +36,35 @@ grid()
 ax = gca()
 ax2 = ax.twinx()
 l3 = ax2.plot(data[:, 1] .- 288625.081, abs.(data[:, 2] ./ data[:, 4]), "C2",
-              label="\$\\frac{\\Omega_{Raman}}{\\Gamma_{scatter}}\$")
+              label="\$\\frac{\\Omega_{Raman}}{\\Gamma_{scatter}}\$", linewidth=4)
 ls = [l1; l2; l3]
 legend(ls, [l.get_label() for l in ls], fontsize="small",
        loc="lower right", bbox_to_anchor=(1, 0.05))
 xlim([-45, 45])
 ylim([0, 60])
 ax2.tick_params(axis="y", labelcolor="C2")
+setp(ax2.get_yticklabels(), fontweight="bold")
 NaCsPlot.maybe_save("$(prefix)_v0")
 
 figure()
-l1 = plot(data[:, 1] .- 351271.53, abs.(data[:, 2] ./ 2π / 1000), "C0", label="\$\\Omega_{Raman}\$")
-l2 = plot(data[:, 1] .- 351271.53, abs.(data[:, 4] ./ 2π / 1000), "C1", label="\$\\Gamma_{scatter}\$")
+l1 = plot(data[:, 1] .- 351271.53, abs.(data[:, 2] ./ 2π / 1000), "C0",
+          label="\$\\Omega_{Raman}\$")
+l2 = plot(data[:, 1] .- 351271.53, abs.(data[:, 4] ./ 2π / 1000), "C1",
+          label="\$\\Gamma_{scatter}\$")
 ylabel("\$2\\pi\\cdot \\mathrm{kHz}\$")
 xlabel("One-Photon Detuning (GHz)")
 ylim([0, 800])
 grid()
 ax = gca()
 ax2 = ax.twinx()
-l3 = ax2.plot(data[:, 1] .- 351271.53, abs.(data[:, 2] ./ data[:, 4]), "C2", label="\$\\frac{\\Omega_{Raman}}{\\Gamma_{scatter}}\$")
+l3 = ax2.plot(data[:, 1] .- 351271.53, abs.(data[:, 2] ./ data[:, 4]), "C2",
+              label="\$\\frac{\\Omega_{Raman}}{\\Gamma_{scatter}}\$", linewidth=4)
 ls = [l1; l2; l3]
 legend(ls, [l.get_label() for l in ls], fontsize="small", loc="upper right")
 xlim([-12.5, 12.5])
 ylim([0, 2])
 ax2.tick_params(axis="y", labelcolor="C2")
+setp(ax2.get_yticklabels(), fontweight="bold")
 NaCsPlot.maybe_save("$(prefix)_vh")
 
 NaCsPlot.maybe_show()
