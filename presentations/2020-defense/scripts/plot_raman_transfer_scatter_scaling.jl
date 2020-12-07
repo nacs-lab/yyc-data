67b720fb14e3e71be7cda525b309dd6088734c58
Yichao Yu <yyc1992@gmail.com>
2020-12-07 02:26:45 -0500

    [presentations/2020-defense] ASE and two-photon

---
+++ presentations/2020-defense/scripts/plot_raman_transfer_scatter_scaling.jl
@@ -0,0 +1,134 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsData.Fitting: fit_data, fit_survival
+using NaCsPlot
+using PyPlot
+using PyCall
+using DataStructures
+using LsqFit
+using MAT
+
+# 1: 288503 GHz
+# 2: 288560 GHz
+
+const pwrs = [15, 6, 3]
+const Γ_atom1 = [32.5, 2.77, 0.573]
+const unc_Γ_atom1 = [3.1, 0.27, 0.078]
+const Γ_atom2 = [24.5, 2.40, 0.572]
+const unc_Γ_atom2 = [2.3, 0.20, 0.063]
+const Γ_mol1 = [0.700, 0.227, 0.159]
+const unc_Γ_mol1 = [0.045, 0.020, 0.023]
+const Γ_mol2 = [1.30, 0.395, 0.170]
+const unc_Γ_mol2 = [0.10, 0.041, 0.010]
+
+const Γ_mol_diff = Γ_mol2 .- Γ_mol1
+const unc_Γ_mol_diff = sqrt.(unc_Γ_mol1.^2 .+ unc_Γ_mol2.^2)
+
+function gen_model_power(pwr)
+    return (x, p)->x.^pwr .* p[1]
+end
+
+model_lin0(x, p) = x .* p[1]
+model_lin1(x, p) = x .* p[1] .+ p[2]
+model_quad0(x, p) = x.^2 .* p[1]
+
+fit1_Γ_atom1 = fit_data(gen_model_power(1.58), pwrs, Γ_atom1, unc_Γ_atom1, [0.2], plot_lo=0)
+fit1_Γ_atom2 = fit_data(gen_model_power(1.58), pwrs, Γ_atom2, unc_Γ_atom2, [0.2], plot_lo=0)
+fit2_Γ_atom1 = fit_data(gen_model_power(2.58), pwrs, Γ_atom1, unc_Γ_atom1, [0.03], plot_lo=0)
+fit2_Γ_atom2 = fit_data(gen_model_power(2.58), pwrs, Γ_atom2, unc_Γ_atom2, [0.03], plot_lo=0)
+
+fit1_Γ_mol1 = fit_data(model_lin0, pwrs, Γ_mol1, unc_Γ_mol1, [0.05], plot_lo=0)
+fit1_Γ_mol2 = fit_data(model_lin0, pwrs, Γ_mol2, unc_Γ_mol2, [0.05], plot_lo=0)
+fit2_Γ_mol1 = fit_data(model_quad0, pwrs, Γ_mol1, unc_Γ_mol1, [0.01], plot_lo=0)
+fit2_Γ_mol2 = fit_data(model_quad0, pwrs, Γ_mol2, unc_Γ_mol2, [0.01], plot_lo=0)
+
+fit1_Γ_mol_diff = fit_data(model_lin0, pwrs, Γ_mol_diff, unc_Γ_mol_diff, [0.01], plot_lo=0)
+fit2_Γ_mol_diff = fit_data(model_quad0, pwrs, Γ_mol_diff, unc_Γ_mol_diff, [0.01], plot_lo=0)
+
+@show fit2_Γ_atom1.uncs # 2.93(17)\times10^{-2}
+@show fit2_Γ_atom2.uncs # 2.45(13)\times10^{-2}
+@show fit1_Γ_mol1.uncs # 4.35(21)\times10^{-2}
+@show fit1_Γ_mol2.uncs # 6.32(27)\times10^{-2}
+
+const prefix = joinpath(@__DIR__, "../figures/raman_transfer_scatter_scaling")
+
+# figure()
+# ax = gca()
+# errorbar([], [], [], fmt="C0o-", label="288503 GHz")
+# errorbar([], [], [], fmt="C1s-", label="288560 GHz")
+# errorbar(pwrs, Γ_atom1, unc_Γ_atom1, fmt="C0o")
+# plot(fit1_Γ_atom1.plotx, fit1_Γ_atom1.ploty, "C0--")
+# plot(fit2_Γ_atom1.plotx, fit2_Γ_atom1.ploty, "C0")
+# errorbar(pwrs, Γ_atom2, unc_Γ_atom2, fmt="C1s")
+# plot(fit1_Γ_atom2.plotx, fit1_Γ_atom2.ploty, "C1--")
+# plot(fit2_Γ_atom2.plotx, fit2_Γ_atom2.ploty, "C1")
+# xlim([2.4, 17])
+# ylim([0.4, 40])
+# legend(loc="lower right", fontsize="x-small")
+# xscale("log")
+# yscale("log")
+# grid()
+# ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# ax.set_xticks(3:17, minor=true)
+# xticks([3, 6, 12], ["3", "6", "12"])
+# yticks([1, 3, 10, 30], ["1", "", "10", ""])
+# xlabel("Tweezer Power (mW)")
+# ylabel("\$\\Gamma_{\\mathrm{a}}~(\\mathrm{2\\pi\\times Hz})\$")
+# NaCsPlot.maybe_save("$(prefix)_atom")
+
+# figure()
+# ax = gca()
+# errorbar([], [], [], fmt="C1s-", label="288560 GHz")
+# errorbar([], [], [], fmt="C0o-", label="288503 GHz")
+# errorbar(pwrs, Γ_mol1, unc_Γ_mol1, fmt="C0o")
+# # plot(fit2_Γ_mol1.plotx, fit2_Γ_mol1.ploty, "C0--")
+# plot(fit1_Γ_mol1.plotx, fit1_Γ_mol1.ploty, "C0")
+# errorbar(pwrs, Γ_mol2, unc_Γ_mol2, fmt="C1s")
+# # plot(fit2_Γ_mol2.plotx, fit2_Γ_mol2.ploty, "C1--")
+# plot(fit1_Γ_mol2.plotx, fit1_Γ_mol2.ploty, "C1")
+# xlim([2.4, 17])
+# ylim([0.1, 1.6])
+# legend(loc="lower right", fontsize="x-small")
+# xscale("log")
+# yscale("log")
+# grid()
+# ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+# ax.set_xticks(3:17, minor=true)
+# xticks([3, 6, 12], ["3", "6", "12"])
+# yticks([0.2, 0.4, 0.8, 1.6], ["0.2", "0.4", "0.8", "1.6"])
+# xlabel("Tweezer Power (mW)")
+# ylabel("\$\\Gamma_{\\mathrm{m}}~(\\mathrm{2\\pi\\times kHz})\$")
+# NaCsPlot.maybe_save("$(prefix)_mol")
+
+figure()
+ax = gca()
+errorbar(pwrs, Γ_mol_diff, unc_Γ_mol_diff, fmt="C0o")
+plot(fit1_Γ_mol_diff.plotx, fit1_Γ_mol_diff.ploty, "C0--")
+plot(fit2_Γ_mol_diff.plotx, fit2_Γ_mol_diff.ploty, "C0")
+xlim([2.4, 17])
+ylim([0.008, 0.8])
+xscale("log")
+yscale("log")
+grid()
+text(3.8, 0.02,
+     ("\$\\Delta\\Gamma_{\\mathrm{m}}=P^2\\cdot2\\pi\\times$(fit2_Γ_mol_diff.uncs[1] .* 1000)" *
+      "~\\mathrm{Hz/mW^2}\$"), fontsize="small", color="C0")
+ax.xaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
+ax.set_xticks(3:17, minor=true)
+xticks([3, 6, 12], ["3", "6", "12"])
+yticks([0.01, 0.03, 0.1, 0.3], ["0.01", "", "0.1", ""])
+xlabel("Tweezer Power (mW)")
+ylabel("\$\\Delta\\Gamma_{\\mathrm{m}}~(\\mathrm{2\\pi\\times kHz})\$")
+gca().yaxis.set_label_coords(-0.09, 0.55)
+NaCsPlot.maybe_save("$(prefix)_mol_diff")
+
+NaCsPlot.maybe_show()
---
+++ presentations/2020-defense/scripts/plot_raman_transfer_spectrum_prefilter.jl
@@ -0,0 +1,96 @@
+#!/usr/bin/julia
+
+push!(LOAD_PATH, joinpath(@__DIR__, "../../../lib"))
+
+import NaCsCalc.Format: Unc, Sci
+using NaCsCalc
+using NaCsCalc.Utils: interactive
+using NaCsData
+using NaCsData.Fitting: fit_data, fit_survival
+using NaCsPlot
+using PyPlot
+# using DataStructures
+# using LsqFit
+using LibArchive
+using Statistics
+
+using DelimitedFiles
+
+function read_csv_compressed(fname, args...; kwargs...)
+    LibArchive.Reader(fname) do reader
+        LibArchive.support_format_raw(reader)
+        LibArchive.support_filter_all(reader)
+        LibArchive.next_header(reader)
+        readdlm(reader, args...; kwargs...)
+    end
+end
+
+const data = read_csv_compressed(
+    joinpath(@__DIR__, "../../thesis/data/Timebase1038_20200702_094907/data.csv.zst"),
+    ',', Float64)
+
+const params = (
+    bw = 2,
+    xthresh = 0.0001,
+    yzero = 0.02,
+    # xcutoff1 = 0.2705,
+    # xcutoff2 = 0.301,
+    lambda0 = 1009, # nm
+    lambda1 = 1109, # nm
+)
+
+function process_data(data, params)
+    curxs = [data[1, 1]]
+    ymin = ymax = data[1, 2]
+    xs = Float64[]
+    ys = Float64[]
+    uncs = Float64[]
+    for i in 2:size(data, 1)
+        x = data[i, 1]
+        y = data[i, 2]
+        if x - curxs[end] <= params.xthresh
+            push!(curxs, x)
+            if y < ymin
+                ymin = y
+            elseif y > ymax
+                ymax = y
+            end
+            continue
+        end
+        if length(curxs) > 1
+            push!(xs, mean(curxs) * (params.lambda1 - params.lambda0) + params.lambda0)
+            if ymin < params.yzero
+                ymin = -0.0001
+            end
+            push!(ys, (ymin + ymax) / 2 / params.bw)
+            push!(uncs, (ymax - ymin) / 2 / params.bw)
+        end
+        resize!(curxs, 1)
+        curxs[1] = x
+        ymin = ymax = y
+    end
+    if length(curxs) > 1
+        push!(xs, mean(curxs) * (params.lambda1 - params.lambda0) + params.lambda0)
+        push!(ys, (ymin + ymax) / 2 / params.bw)
+        push!(uncs, (ymax - ymin) / 2 / params.bw)
+    end
+    return (xs=xs, ys=ys, uncs=uncs)
+end
+
+const prefix = joinpath(@__DIR__, "../figures/raman_transfer_spectrum_prefilter")
+
+const processed = process_data(data, params)
+
+figure()
+ax = gca()
+mask = matplotlib.patches.Rectangle((1036.1, 0), 2.8, 2.4, facecolor="C0", alpha=0.7)
+ax.add_patch(mask)
+errorbar(processed.xs, processed.ys, processed.uncs, linestyle="None")
+grid()
+xlim([params.lambda0, params.lambda1])
+ylim([0, 2.4])
+xlabel("Wavelength (nm)")
+ylabel("Spectral Density (nW/nm)")
+NaCsPlot.maybe_save("$(prefix)")
+
+NaCsPlot.maybe_show()
