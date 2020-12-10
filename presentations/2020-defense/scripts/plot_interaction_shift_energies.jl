41a04fb3af2c19e4b1d31e3ef603f50831bab05a
Yichao Yu <yyc1992@gmail.com>
2020-12-10 15:46:40 -0500

    [presentations/2020-defense] Interaction Shift

--- presentations/2020-defense/scripts/plot_interaction_shift_energies.jl
+++ presentations/2020-defense/scripts/plot_interaction_shift_energies.jl
@@ -44,23 +44,15 @@ figure(figsize=[5.6, 4.9])
 plot([-0.5, 0.5], [-0.5 * perturb_slope, 0.5 * perturb_slope],
      ls="dotted", color="gray")
 plot(data[:, 1], data[:, 2], "C0") # 0, 0
-text(-0.065, -16.4, "\$|m_{\\mathrm{rel},z},m_{\\mathrm{COM},z}\\rangle\\!=\\!|0,0\\rangle\$",
+text(0.17, 4.7,
+     "\$|m_{\\mathrm{rel},z},\\!m_{\\mathrm{COM},z}\\rangle\$\n\$=\\!|0,\\!0\\rangle\$",
      color="C0", fontsize=14)
-# plot(data[:, 1], data[:, 3], "C1--") # 1, 0
-# text(-0.200, -10.0, "\$|0,1\\rangle\$", color="C1", fontsize=14)
-# plot(data[:, 1], data[:, 4], "C2--") # 0, 1
-# text(-0.470, 15.0, "\$|1,0\\rangle\$", color="C2", fontsize=14)
 plot(data[:, 1], data[:, 5], "C3") # 2, 0
-text(-0.350, 2.1, "\$|0,2\\rangle\$", color="C3", fontsize=14)
+text(-0.480, -7.1, "\$|0,\\!2\\rangle\$", color="C3", fontsize=14)
 plot(data[:, 1], data[:, 6], "C4") # 1, 1
-text(-0.276, 27.2, "\$|2,0\\rangle\$", color="C4", fontsize=14)
+text(-0.490, 23.0, "\$|2,\\!0\\rangle\$", color="C4", fontsize=14)
 plot(data[:, 1], data[:, 7], "C5") # 0, 2
-text(-0.490, 39.6, "\$|1,1\\rangle\$", color="C5", fontsize=14)
-# plot(data[:, 1], data[:, 8], "C6--") # 3, 0
-# text(-0.370, 37.0, "\$|0,3\\rangle\$", color="C6", fontsize=14)
-# plot(data[:, 1], data[:, 9], "C7--") # 2, 1
-# plot(data[:, 1], data[:, 10], "C8--") # 1, 2
-# plot(data[:, 1], data[:, 11], "C9--") # 0, 3
+text(-0.490, 39.6, "\$|1,\\!1\\rangle\$", color="C5", fontsize=14)
 grid()
 xlim([-0.5, 0.5])
 ylim([-50, 50])
@@ -80,13 +72,15 @@ yfinal2 = interpolate_data(xfinal, 6)
 axvline(xinit, color="green", ls="dotted")
 axvline(xfinal, color="red", ls="dotted")
 plot(data[:, 1], data[:, 2], "C0") # 0, 0
-text(0.2, 12.0, "\$|0,0\\rangle\$", color="C0", fontsize=14)
+text(0.17, 4.7,
+     "\$|m_{\\mathrm{rel},z},\\!m_{\\mathrm{COM},z}\\rangle\$\n\$=\\!|0,\\!0\\rangle\$",
+     color="C0", fontsize=14)
 plot(data[:, 1], data[:, 5], "C3") # 2, 0
-text(-0.480, -7.1, "\$|0,2\\rangle\$", color="C3", fontsize=14)
+text(-0.480, -7.1, "\$|0,\\!2\\rangle\$", color="C3", fontsize=14)
 plot(data[:, 1], data[:, 6], "C4") # 1, 1
-text(-0.490, 23.0, "\$|2,0\\rangle\$", color="C4", fontsize=14)
+text(-0.490, 23.0, "\$|2,\\!0\\rangle\$", color="C4", fontsize=14)
 plot(data[:, 1], data[:, 7], "C5") # 0, 2
-text(-0.490, 39.6, "\$|1,1\\rangle\$", color="C5", fontsize=14)
+text(-0.490, 39.6, "\$|1,\\!1\\rangle\$", color="C5", fontsize=14)
 annotate("", xy=(xfinal, yfinal1), xytext=(xinit, yinit), arrowprops=ptrprops)
 annotate("", xy=(xfinal, yfinal2), xytext=(xinit, yinit), arrowprops=ptrprops)
 text(xinit, -26, "Initial Spin State\nNa(2,2) Cs(4,4)",
@@ -109,13 +103,15 @@ yfinal2 = interpolate_data(xfinal, 6)
 axvline(xinit, color="green", ls="dotted")
 axvline(xfinal, color="red", ls="dotted")
 plot(data[:, 1], data[:, 2], "C0") # 0, 0
-text(0.2, 12.0, "\$|0,0\\rangle\$", color="C0", fontsize=14)
+text(0.17, 4.7,
+     "\$|m_{\\mathrm{rel},z},\\!m_{\\mathrm{COM},z}\\rangle\$\n\$=\\!|0,\\!0\\rangle\$",
+     color="C0", fontsize=14)
 plot(data[:, 1], data[:, 5], "C3") # 2, 0
-text(-0.480, -7.1, "\$|0,2\\rangle\$", color="C3", fontsize=14)
+text(-0.480, -7.1, "\$|0,\\!2\\rangle\$", color="C3", fontsize=14)
 plot(data[:, 1], data[:, 6], "C4") # 1, 1
-text(-0.490, 23.0, "\$|2,0\\rangle\$", color="C4", fontsize=14)
+text(-0.490, 23.0, "\$|2,\\!0\\rangle\$", color="C4", fontsize=14)
 plot(data[:, 1], data[:, 7], "C5") # 0, 2
-text(-0.490, 39.6, "\$|1,1\\rangle\$", color="C5", fontsize=14)
+text(-0.490, 39.6, "\$|1,\\!1\\rangle\$", color="C5", fontsize=14)
 annotate("", xy=(xfinal, yfinal1), xytext=(xinit, yinit), arrowprops=ptrprops)
 annotate("", xy=(xfinal, yfinal2), xytext=(xinit, yinit), arrowprops=ptrprops)
 text(xinit, -6, "Initial Spin State\n      Na(2,2) Cs(3,3)",
